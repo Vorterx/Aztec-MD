263bc36e726ec059bc7cc296b7f50e181996ce3d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-30 12:32:44 +0200

    vorterx

--- plugins/_facebook.js
+++ plugins/_facebook.js
@@ -10,18 +10,17 @@ module.exports = {
     await xReact('❌');
     return m.reply('Please provide a Facebook video URL');
   }
-
   try {
-    let response = await axios.get(`https://api.botcahx.live/api/dowloader/fbdown3?url=${text}&apikey=29y8XIYL`);
-
-    const videoUrl = response.data.url;
-    const caption = `[*FB DOWNLOAD*]\n` +
-      `😀 Title: ${response.data.title}\n` +
-      `😀 Quality: ${response.data.selectedNumber === '1' ? '720p (HD)' : '360p (SD)'}\n` +
-      `🙂 Views: ${response.data.views}\n\n` +
+    await xReact('📤');
+    let res = await axios.get(`https://api.botcahx.live/api/dowloader/fbdown3?url=${text}&apikey=29y8XIYL`);
+    const oUrl = res.data.url;
+    const txt = `[*FB DOWNLOAD*]\n` +
+      `😀 Title: ${res.data.title}\n` +
+      `😀 Quality: ${res.data.selectedNumber === '1' ? '720p (HD)' : '360p (SD)'}\n` +
+      `🙂 Views: ${res.data.views}\n\n` +
       `└───────────◉`;
-    const message = { video: { url: videoUrl }, caption: caption };
-    vorterx.sendMessage(m.from, message);
+    const mSg = { video: { url: oUrl }, caption: txt };
+    vorterx.sendMessage(m.from, mSg);
   } catch (error) {
     console.error('Error downloading Facebook video:', error);
     m.reply('An error occurred while processing the video');
