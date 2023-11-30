0e71d2e0907360cfbcd5cf2a7a9c109a59353d68
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-30 20:43:27 +0200

    vorterx

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
-//-------------------------------------------------------------------------
+//----------------------------------------
 //           COPYRIGHT MIT
 //            2023 @DIEGOSONTECH
-//-------------------------------------------------------------------------
+//----------------------------------------
 //                     |
 //                     |
 //                     |
-//                     -----------------------------------------------------
-
+//                     -------------------
 const fs = require('fs');
 require('dotenv').config();
 const chalk = require('chalk');
@@ -14,38 +13,16 @@ const chalk = require('chalk');
 let config = {
   botName: process.env.BOTNAME || 'AZTEC MD',
   prefix: process.env.PREFIX || '.',
-  name: process.env.OWNER_NAME || '', //Add your name here as its required
+  name: process.env.OWNER_NAME || '', 
   owner_number: process.env.OWNER_NUMBER || '27686881509',
   session_Id: process.env.SESSION_ID || 'add something',
-  level_up: process.env.LEVEL_UP || '', // Set true or false or enable/disable
+  level_up: process.env.LEVEL_UP || '',
   menu: process.env.MENU || '', // 2 is the default menu Aztec, 0 is Suhail MD menu
   thumb: process.env.THUMB || 'https://imageupload.io/69vJBZbn4iPqWTZ',
-  auto_react: process.env.AUTO_REACT || 'off', // else {on} to react
-  auto_read_status: process.env.AUTO_READ_STATUS || 'true', //else false
-  worktype: process.env.WORKTYPE || "", //public or private 
-  mods: process.env.MODS ? process.env.MODS.split(',') : [],
+  auto_react: process.env.AUTO_REACT || 'off', 
+  auto_read_status: process.env.AUTO_READ_STATUS || 'true',
+  worktype: process.env.WORKTYPE || '', 
+  mods: process.env.MODS ? process.env.MODS.split(',').map(mod => mod.replace('@net.whatsapp', '')) : [],
   LANG: process.env.LANG || 'VOR_TERX',
 };
-
-config.HEROKU = {
-  API_KEY: process.env.HEROKU_API_KEY || '',
-  APP_NAME: process.env.HEROKU_APP_NAME || '',
-};
-
 module.exports = config;
-
-const configFile = require.resolve(__filename);
-watchAndReloadConfig(configFile);
-
-function watchAndReloadConfig(filePath) {
-  fs.watchFile(filePath, () => {
-  console.log(chalk.redBright(`Configuration file '${filePath}' updated`));
-  delete require.cache[filePath];
-  config = require(filePath);
-  });
-}
-
-process.on('SIGINT', () => {
-console.log(chalk.yellowBright('📴 Gracefully shutting down...'));
-process.exit();
-});
