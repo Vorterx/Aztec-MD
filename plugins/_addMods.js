82c2f11812c3e69e0fb8068fbf2459ba7cd1b406
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-30 20:45:33 +0200

    vorterx

--- plugins/_addMods.js
+++ plugins/_addMods.js
@@ -1,26 +1,28 @@
- module.exports = {
+const config = require('../config');
+
+module.exports = {
   name: '(admod|setsudo)',
   category: 'Owner',
-   description: 'To add the user as a mod to the bot',
+  description: 'To add the user as a mod to the bot',
   async xstart(vorterx, m, { text, args, quoted, mentionByTag, xReact }) {
 
-    if (!process.env.MODS) {
+    if (!config.mods) {
       await xReact('❌');
-      return m.reply(`\`\`\`This command is for my ownr only\`\`\``);
+      return m.reply(`\`\`\`This command is for my owner only\`\`\``);
     }
     const sender = m.sender;
-    const tag = quoted ? quoted.text : '';     
-    const isMod = process.env.MODS.includes(sender);
+    const tag = quoted ? quoted.text : '';
+    const isMod = config.mods.includes(sender);
 
-    if (isMod) { 
+    if (isMod) {
       await xReact('✔️');
-      const mods = quoted ? quoted.sender : mentionByTag[0];
+      const mods = quoted ? quoted.sender.replace('@net.whatsapp', '') : mentionByTag[0];
       if (mods) {
-        process.env.MODS += `,${mods}`;        
-        vorterx.sendMessage(m.from, { text: `*User  ${mods} has been promoted to moderator*`});
-      } else {        
+        config.mods.push(mods);
+        vorterx.sendMessage(m.from, { text: `*User ${mods} has been promoted to moderator*` });
+      } else {
         m.reply(`\`\`\`Please reply with the number you want to add to mods\`\`\``);
-      }      
+      }
     }
   }
 };
