af972379bb8aca13e855d2cd34f57ece2899d899
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-30 21:19:47 +0200

    vorterx

--- plugins/_mods.js
+++ plugins/_mods.js
@@ -1,22 +1,35 @@
 module.exports = {
-  name: '(mods|sudos)',
-  description: 'To check mods admins to the bot',
-  category: 'Owner',
-  async xstart(vorterx, m, { text, args, quoted, xReact }) {
-  
-    await xReact('✔️');
-    let azteci = '*👤 VORTERX MODS 👤*\n\n';
-    const mods = process.env.MODS || '';
+    name: '(mods|sudos)',
+    description: 'To check mods admins to the bot',
+    category: 'Owner',
+    async xstart(vorterx, m, {
+        text,
+        args,
+        quoted,
+        xReact
+    }) {
 
-    if (mods) {
-      const sudo = mods.split(',');
-      for (let i = 0; i < sudo.length; i++) {
-        azteci += `*#${i + 1} 〄* ${sudo[i]}\n`;
-      }
-    } else {
-      azteci += '`No mods are set for now`';
+        await xReact('✔️');
+        let azteci = '*👤 VORTERX MODS 👤*\n\n';
+        const mods = process.env.MODS || '';
+
+        if (mods) {
+            const sudo = mods.split(',');
+            for (let i = 0; i < sudo.length; i++) {
+                azteci += `*#${i + 1} 〄* @${sudo[i]}\n`;
+            }
+        } else {
+            azteci += '`No mods are set for now`';
+        }
+        const img = 'https://i.ibb.co/2dvDgBd/464318-5149318-823730-thumbnail.png';
+        await vorterx.sendMessage(m.from, {
+            image: {
+                url: img,
+                caption: azteci
+            },
+            mentions: sudo.map((x) => x + '@s.whatsapp.net')
+        }, {
+            quoted: m
+        });
     }
-    const img = 'https://i.ibb.co/2dvDgBd/464318-5149318-823730-thumbnail.png';
-    await vorterx.sendMessage(m.from, { image: { url: img, caption: azteci } }, { quoted: m });
-  }
 };
