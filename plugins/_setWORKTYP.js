24807b2722ba3a83ba8a121d9aa0c51687640d12
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-11-30 16:41:51 +0200

    vorterx

--- plugins/_setWORKTYP.js
+++ plugins/_setWORKTYP.js
@@ -4,28 +4,25 @@ module.exports = {
   name: '(set|mod|use)',
   description: 'To set private mode or public using cmd',
   category: 'Owner',
-  async xstart(vorterx, m, { text, args }) {
+  async xstart(vorterx, m, { text, args, xReact }) {
    
     if (process.env.MODS !== text) {
       await xReact('❌');
       return m.reply('This command is for my owner only');
     }
-
     if (args.length < 1) {
       await xReact('❌');
       return vorterx.sendMessage(m.from, { text: 'Please specify the mode (private or public).' });
     }
-
-    const modeValue = args[0].toLowerCase();
-
-    if (modeValue === 'private') {
+    const worktype = args[0].toLowerCase();
+    if (worktype === 'private') {
       await xReact('✔️');
       mode.set('private');
-      m.reply('Mode set to private. Only mods and the bot owner can use the bot now.');
-    } else if (modeValue === 'public') {
+      m.reply('Bot set to private. Only mods and the bot owner can use the bot now.');
+    } else if (worktype === 'public') {
       await xReact('✔️');
       mode.set('public');
-      m.reply('Mode set to public. Anyone can use the bot now.');
+      m.reply('Bot set to public. Anyone can use the bot now.');
     } else {
       m.reply('Invalid mode. Please specify either "private" or "public".');
     }
