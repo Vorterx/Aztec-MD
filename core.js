7e8b1a1c11614e51125e46cd09d800d011991d35
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-03 10:20:27 +0200

    vorterx

--- core.js
+++ core.js
@@ -13,10 +13,12 @@ const MessageHandler = require('./lib/message/vorterx.js');
 let cc = config.session_Id.replace(/Vorterx;;;/g, "");
 
 const app = express();
-const PORT = 3000; 
+const PORT = 3000;
+
 async function startAztec() {
   const store = makeInMemoryStore({ logger: P().child({ level: 'silent', stream: 'store' }) });
-async function MakeSession() {
+
+  async function MakeSession() {
     if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
       if (cc.length < 30) {
         const axios = require('axios');
@@ -25,13 +27,19 @@ async function MakeSession() {
       } else {
         var c = atob(cc);
         await fs.writeFileSync(__dirname + '/auth_info_baileys/creds.json', c, "utf8");
-      }}}
-      await MakeSession();
-      async function main() {
+      }
+    }
+  }
+
+  await MakeSession();
+
+  async function main() {
     if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
- }
-const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/');
-const vorterx = makeWASocket({
+    }
+  }
+
+  const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/');
+  const vorterx = makeWASocket({
     logger: P({ level: "silent" }),
     printQRInTerminal: false,
     browser: Browsers.macOS("Desktop"),
@@ -50,7 +58,8 @@ const vorterx = makeWASocket({
     for (const file of cmdfile) {
       const command = require(`./plugins/${file}`);
       vorterx.cmd.set(command.name, command);
-    }}
+    }
+  }
 
   readcommands();
 
@@ -81,7 +90,7 @@ const vorterx = makeWASocket({
     }
 
     if (connection === "open") {
-      console.log('WhatsApp chatbot has connected✔️');
+     console.log('WhatsApp chatbot has connected✔️');
       const version = require(__dirname + "/package.json").version;
       const BotName = require(__dirname + "/config.js").botName;
       const Mods = require(__dirname + "/config.js").mods;
@@ -91,7 +100,9 @@ const vorterx = makeWASocket({
 
     if (update.qr) {
       vorterx.QR = qr.imageSync(update.qr);
-    }});
+    }
+  });
+
   app.get("/", (req, res) => {
     res.end(vorterx.QR);
   });
@@ -100,13 +111,17 @@ const vorterx = makeWASocket({
   vorterx.ev.on('contacts.update', async (update) => await contact.saveContacts(update, vorterx));
 }
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
- });
+async function startServer() {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
-startAztec();
 
-        
+async function main() {
+  await startAztec();
+  await startServer();
+}
+
+main().catch((error) => {
+  console.error("An error occurred:", error);
+});
