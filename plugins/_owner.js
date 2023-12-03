71fd879142906d930fdab0810aff94721c52538d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-03 11:32:57 +0200

    vorterx

--- plugins/_owner.js
+++ plugins/_owner.js
@@ -4,27 +4,28 @@ module.exports = {
   name: "owner",
   description: "Get owner information",
   category: "user",
-  async xstart(vorterx, m) {
-  //  await xReact("✔️");
+  async xstart(vorterx, m, { xReact }) {
 
-    const ownerName = config.mods;
-    const userName = config.OWNER_NAME;
+    await xReact("✔️");
+
+    const userNumber = config.mods;
+    const ownerName = config.OWNER_NAME;
     const logo = "https://i.ibb.co/v47d4BL/IMG-20230429-WA0021.jpg";
-    const sourceUrl = `https://wa.me/+${ownerName}?text=Hii+bro,I+am+a+big+fan+`;
+    const sourceUrl = `https://wa.me/+${userNumber}?text=Hii+bro,I+am+a+big+fan+`;
 
     const buttonMessage = {
       contacts: {
         displayName: ownerName,
-        contacts: [{ vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${ownerName}:+${ownerName}\nEND:VCARD` }],
+        contacts: [
+          { vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${userNumber}:+${userNumber}\nEND:VCARD` }
+        ],
       },
       contextInfo: {
-        stanzaId: m.key.id,
-        participant: m.key.remoteJID,
-        quotedMessage: m.message,
         externalAdReply: {
           title: "Aztec MD.",
           body: "Powered by Aztec",
           renderLargerThumbnail: true,
+          thumbnailUrl: logo,
           thumbnail: logo,
           mediaType: 1,
           mediaUrl: "",
