5c6ea5672d35031b009be2d98ebb29583b9efa14
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-04 12:14:35 +0200

    vorterx

--- plugins/_setWORKTYP.js
+++ plugins/_setWORKTYP.js
@@ -1,31 +1,31 @@
-const { mode } = require('../lib/message/vorterx.js');
+const { mode } = require('../lib/client.js');
 const config = require('../config');
 
 module.exports = {
   name: '(set|mod|use)',
   description: 'To set private mode, public mode, or group work mode using cmd',
   category: 'Owner',
-  async xstart(vorterx, m, { text, args, xReact }) {
+  async client(vorterx, m, { text, args, connect }) {
   
     if (config.mods !== text) {
-      await xReact('❌');
+      await connect('❌');
       return m.reply('This command is for my owner only');
     }
     if (args.length < 1) {
-      await xReact('❌');
+      await connect('❌');
       return vorterx.sendMessage(m.from, { text: 'Please specify the mode (private, public, or group_work).' });
     }
     const worktype = args[0].toLowerCase();
     if (worktype === 'private') {
-      await xReact('✔️');
+      await connect('✔️');
       mode.set('private');
       m.reply('Bot set to private. Only mods and the bot owner can use the bot now.');
     } else if (worktype === 'public') {
-      await xReact('✔️');
+      await connect('✔️');
       mode.set('public');
       m.reply('Bot set to public. Anyone can use the bot now.');
     } else if (worktype === 'group_work') {
-      await xReact('✔️');
+      await connect('✔️');
       mode.set('group_work');
       m.reply('Bot set to group work. The bot will only work in group chats.');
     } else {
