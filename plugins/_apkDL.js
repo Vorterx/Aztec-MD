580927f23030e5e8ef19ce7c77df816f1bffc3c6
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-06 00:01:39 +0200

    vorterx

--- plugins/_apkDL.js
+++ plugins/_apkDL.js
@@ -1,3 +1,4 @@
+//
 const config = require('../config.js');
 const { getJson } = require('../lib/_scrapers.js');
 
@@ -7,7 +8,6 @@ module.exports = {
   description: 'Download applications from Aptoid',
   category: 'Downloads',
   async client(vorterx, m, { text, args, connect, quoted }) {
-   
     if (!text) {
       await connect('❌');
       return m.reply('*Please provide the name of the application.*');
@@ -28,16 +28,16 @@ module.exports = {
       }
 
       await connect('📤');
-     return m.reply(`*Downloading ${app.name}...*`);
-    await vorterx.sendMessage(m.from, {
-  document: {
-    url: downloadResult.result.link,
-    caption: `*Developer*: ${downloadResult.result.dev}`,
-    fileName: app.name + '.apk',
-  },
-  quoted: m
-});
-      
+      m.reply(`*Downloading ${app.name}...*`);
+
+      await vorterx.sendMessage(m.from, {
+        document: {
+          url: downloadResult.result.link,
+          caption: `*Downloading ${app.name}*\n\n*Developer*: ${downloadResult.result.dev}`,
+          fileName: app.name + '.apk',
+          quoted: m
+        }
+      });
 
     } catch (error) {
       console.error(error);
