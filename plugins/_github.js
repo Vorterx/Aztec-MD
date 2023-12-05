ec4f1e2eee49fb8fef4df04bb093ed3241bf69cd
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-05 18:05:05 +0200

    vorterx

--- plugins/_github.js
+++ plugins/_github.js
@@ -4,20 +4,19 @@ module.exports = {
   name: 'github',
   alias: ['git'],
   category: 'Search',
-  async client(vorterx,m,{ text,args, connect}) {
-
-    if(!text) {
+  async client(vorterx, m, { text, args, connect }) {
+    if (!text) {
       await connect('❌');
       return m.reply(`*Please provide a git user name e.g github DiegosonTech*`);
-        }
+    }
 
-        await connect('📊');
-        try {
-          const response = await axios.get(`https://api.github.com/users/${text}`);
-          const { login, name, bio, followers, public_repos, following, blog, avatar_url } = response.data;
+    await connect('📊');
+    try {
+      const response = await axios.get(`https://api.github.com/users/${text}`);
+      const { login, name, bio, followers, public_repos, following, blog, avatar_url } = response.data;
 
-          const rezText8 = `
-*乂 USER GUTHUB INFORMTN 乂*
+      const rezText8 = `
+*乂 USER GITHUB INFORMATION 乂*
 _👤 Username:_ *${login}*
 _👤 Name:_ *${name || 'N/A'}*
 _👩‍💻 Bio:_ *${bio || 'N/A'}*
@@ -27,14 +26,14 @@ _👥 Following:_ *${following}*
 _📌 Website:_ ${blog || 'N/A'}
 `;
 
-          const userRepos = await axios.get(`https://api.github.com/users/${text}/repos`);
-          const repoNames = userRepos.data.map(repo => repo.name);
-          const repoList = repoNames.join('\n');
+      const userRepos = await axios.get(`https://api.github.com/users/${text}/repos?per_page=2`);
+      const repoNames = userRepos.data.map(repo => repo.name);
+      const repoList = repoNames.join('\n');
 
-          vorterx.sendMessage(m.from, { image: { url: avatar_url, mimetype: 'image/jpeg' }, caption: rezText8 + '\n\n*📚 Repositories:*\n' + repoList }, { quoted: m });
-              } catch (error) {
-                console.error(error);
-             m.reply('An error occurred');
-        }
-}
-}
+      vorterx.sendMessage(m.from, { image: { url: avatar_url, mimetype: 'image/jpeg' }, caption: rezText8 + '\n\n*📚 Repositories:*\n' + repoList }, { quoted: m });
+    } catch (error) {
+      console.error(error);
+      m.reply('An error occurred');
+    }
+  }
+};
