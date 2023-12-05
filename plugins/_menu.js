2f7e4d59b71cf9e23a82ce0f64b6599f0bf5003b
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-05 19:06:06 +0200

    vorterx

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -31,7 +31,7 @@ module.exports = {
   async client(vorterx, m, { args, connect }) {
    
     await connect('Ⓜ️');
-    let chat_v = "https://repository-images.githubusercontent.com/292765152/b5b54c80-ef19-11ea-9998-10a88f042830";
+    const chat_v = "https://repository-images.githubusercontent.com/292765152/b5b54c80-ef19-11ea-9998-10a88f042830";
     let getCommands = '';
 
     for (const category in commandsByCategory) {
@@ -40,12 +40,11 @@ module.exports = {
       getCommands += '\n\n└─────────◉\n\n';
     }
 
-    let up_up, up_mid, up_btm;
-    up_up = `┏━━⟪ *${process.env.BOTNAME}* ⟫━━⦿`;
-    up_mid = `┃ ✗`;
-    up_btm = `┗━━━━━━━━━━━━━━⦿`;
+    const up_up = `┏━━⟪ *${process.env.BOTNAME}* ⟫━━⦿`;
+    const up_mid = `┃ ✗`;
+    const up_btm = `┗━━━━━━━━━━━━━━⦿`;
 
-    let amarok = `${up_up}
+    const amarok = `${up_up}
 ${up_mid} User: ${m.pushname}
 ${up_mid} Botname: ${process.env.BOTNAME}
 ${up_mid} Prefix: ${process.env.PREFIX}
@@ -54,22 +53,26 @@ ${up_mid} Time: ${new Date().toLocaleTimeString()}
 ${up_mid} Date: ${new Date().toLocaleDateString()}
 ${up_btm}\n\n${getCommands}\n\n\n*©WHATSAPP CHATBOT*`;
 
-     let chatBot = {
-        image: chat_v,
-        caption: amarok,
-        headerType: 2,
-        contextInfo: {
-            externalAdReply: {
-            title: 'vorterx bot',
-            body: 'ʙᴇsᴛ ᴛᴏ ᴜsᴇ',
-            mediaType: 2,
-            thumbnail: chat_v,
-            sourceUrl: 'wa.me/27686881509',
-            mediaUrl: '',
-            },
+    const chatBot = {
+      image: {
+        url: chat_v
+      },
+      caption: amarok,
+      headerType: 2,
+      contextInfo: {
+        externalAdReply: {
+          title: 'vorterx bot',
+          body: 'ʙᴇsᴛ ᴛᴏ ᴜsᴇ',
+          mediaType: 2,
+          thumbnail: {
+            url: chat_v
+          },
+          sourceUrl: 'wa.me/27686881509',
+          mediaUrl: '',
         },
+      },
     };
 
-await vorterx.sendMessage(m.from, chatBot,  { quoted: m})
+    await vorterx.sendMessage(m.from, chatBot, { quoted: m });
   }
 };
