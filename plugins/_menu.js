2b62318251a8580b221f3947c7e065ca63824b17
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-05 16:23:47 +0200

    vorterx

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -1,6 +1,23 @@
-const config = require('../config.js');
 const { tiny } = require("@viper-x/fancytext");
 const fs = require("fs");
+const path = require("path");
+
+const pluginDir = path.join(__dirname, "plugins");
+const pluginFiles = fs.readdirSync(pluginDir);
+const commandsByCategory = {};
+
+for (const file of pluginFiles) {
+  if (!file.endsWith('.js') || file === 'menu.js') {
+    continue;
+  }
+    const plugin = require(path.join(pluginDir, file));
+  const category = plugin.category;
+  
+  if (!commandsByCategory[category]) {
+    commandsByCategory[category] = [];
+  }
+  commandsByCategory[category].push(...plugin.commands);
+}
 
 module.exports = {
   name: 'menu',
@@ -9,51 +26,20 @@ module.exports = {
   description: 'Reveals menu categories commands',
 
   async client(vorterx, m, { args, connect }) {
-
+    
     await connect('Ⓜ️');
-
-    const getCommands = `
-┌──『 General 』──❖
-|  
-|
-|
-└─────────◉
-┌──『 Chatgpt 』──❖
-                        
-|
-└─────────◉
-┌──『 Downloads 』──❖
-|  
-|
-|
-└─────────◉
-┌──『 Mics 』──❖
-|  
-|
-|
-└─────────◉
-┌──『 Extra 』──❖
-|  
-|
-|
-└─────────◉
-
-┌──『 Group 』──❖
-|  
-|
-|
-└─────────◉
-┌──『 Owner 』──❖
-|  
-|
-|
-└─────────◉\n\n*WHATSAPP CHATBOT*`;
+    let getCommands = '';
+    for (const category in commandsByCategory) {
+      getCommands += `┌──『 ${category} 』──❖\n`;
+      getCommands += commandsByCategory[category].map(cmd => `| ${cmd}`).join('\n');
+      getCommands += '\n└─────────◉\n';
+    }
     
     let up_up, up_mid, up_btm;
     up_up = `┏━━⟪ *${tiny(process.env.BOTNAME)}* ⟫━━⦿`;
     up_mid = `┃ ✗`;
     up_btm = `┗━━━━━━━━━━━━━━⦿`;
-    
+
     let amarok = `${up_up}
 ${up_mid} User: ${tiny(m.pushname)}
 ${up_mid} Botname: ${tiny(process.env.BOTNAME)}
@@ -64,5 +50,5 @@ ${up_mid} Date: ${tiny(new Date().toLocaleDateString())}
 ${up_btm}\n\n${getCommands}`;
 
     await vorterx.sendMessage(m.from, { image: { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8IoKEDdsbryDr8GQr6gqFjgQh0APPLZsmnLuK-2_GnA&s" }, caption: tiny(amarok) }, { quoted: m });
-    }
-}
+  }
+};
