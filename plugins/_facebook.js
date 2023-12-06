fe8c3dac1e3b81dfd39c65213bb5384115da0188
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-06 14:31:20 +0200

    vorterx

--- plugins/_facebook.js
+++ plugins/_facebook.js
@@ -1,12 +1,12 @@
 const fg = require('api-dylux');
-const Regex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
 
 module.exports = {
   name: 'fb',
   description: 'To download Facebook video',
   category: 'Downloads',
   async client(vorterx, m, { text, connect, args }) {
-    if (!text || !Regex.test(text)) {
+    if (!text || !urlRegex.test(text)) {
       await connect('❌');
       return m.reply('Please provide a valid Facebook video URL');
     }
@@ -15,14 +15,14 @@ module.exports = {
       await connect('📤');
       m.reply('Downloading your video, please wait...⏳');
       const res = await fg.fbdl(text);
-      const oUrl = res.url && res.url.toString();
+      const oUrl = res.url ? res.url.toString() : null;
       const txt = `[*FB DOWNLOAD*]\n` +
         `😀 Title: ${res.title}\n` +
         `😀 Quality: ${res.selectedNumber === '1' ? '720p (HD)' : '360p (SD)'}\n` +
         `🙂 Views: ${res.views}\n\n` +
         `└───────────◉`;
       const mSg = { video: { url: oUrl }, caption: txt };
-      vorterx.sendMessage(m.from, mSg);
+      await vorterx.sendMessage(m.from, mSg);
     } catch (error) {
       console.error('Error downloading Facebook video:', error);
       m.reply('An error occurred while processing the video');
