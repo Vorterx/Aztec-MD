c07721f78a756a3dcb6d40caf8c732764bd8cd39
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-06 19:06:17 +0200

    vorterx

--- plugins/_facebook.js
+++ plugins/_facebook.js
@@ -1,29 +1,26 @@
+//
 const fg = require('api-dylux');
 
 module.exports = {
   name: 'fb',
-  category: 'Downloads',
-  async client(vorterx, m, { text, args, quoted, connect }) {
-    const url = args[0];
-
-    // Check if the URL matches the required pattern
-    const urlPattern = /^https?:\/\/fb\.watch\/\d+/i;
-    if (!url || !urlPattern.test(url)) {
-      return m.reply('Please provide a valid Facebook video URL starting with "https://fb.watch".');
+  description: 'Downloads and sends Facebook videos',
+  category: ':Downloads',
+  async client(vorterx, m, { args, connect }) {
+    if (!args.length) {
+      await connect('❌');
+      return m.reply('Please provide at least one valid URL. I am a glitch bot, nigga.');
     }
 
     try {
-      console.log('URL:', url);
-      const downloadUrl = await fg.fbvideo(url);
-      console.log('Download URL:', downloadUrl);
-      await vorterx.sendMessage(m.from, { video: { url: downloadUrl } });
-      // fs.writeFile('video.mp4', downloadedVideo, (err) => {
-      //   if (err) console.log(err);
-      //   console.log('Video saved successfully.');
-      // });
+      await connect('✅');
+      for (const videoURL of args) {
+        const res = await fg.fbdl(videoURL);
+        await vorterx.sendMessage(m.from, { video: { url: res }, quoted: m });
+      }
     } catch (error) {
-      console.log(error);
-      m.reply(`An error occurred while downloading the Facebook video: ${error.message}`);
+      console.error(error);
+      await connect('❌');
+      return m.reply('Failed to download and send the videos.');
     }
-  }
+  },
 };
