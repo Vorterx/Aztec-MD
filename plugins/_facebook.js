f37c06052aa3df35a9393fc495753cd651ca33ae
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-06 13:41:30 +0200

    vorterx

--- plugins/_facebook.js
+++ plugins/_facebook.js
@@ -1,29 +1,31 @@
-const axios = require('axios');
+const fg = require('api-dylux');
+const Regex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
 
 module.exports = {
-  mame: 'fb',
+  name: 'fb',
   description: 'To download Facebook video',
   category: 'Downloads',
-  async client(vorterx,m,{text, connect, args}) {
+  async client(vorterx, m, { text, connect, args }) {
+    if (!text || !Regex.test(text)) {
+      await connect('❌');
+      return m.reply('Please provide a valid Facebook video URL');
+    }
 
-  if (!text) {
-    await connect('❌');
-    return m.reply('Please provide a Facebook video URL');
+    try {
+      await connect('📤');
+      m.reply('Downloading your video, please wait...⏳');
+      const res = await fg.fbdl(text);
+      const oUrl = res.url;
+      const txt = `[*FB DOWNLOAD*]\n` +
+        `😀 Title: ${res.title}\n` +
+        `😀 Quality: ${res.selectedNumber === '1' ? '720p (HD)' : '360p (SD)'}\n` +
+        `🙂 Views: ${res.views}\n\n` +
+        `└───────────◉`;
+      const mSg = { video: { url: oUrl }, caption: txt };
+      vorterx.sendMessage(m.from, mSg);
+    } catch (error) {
+      console.error('Error downloading Facebook video:', error);
+      m.reply('An error occurred while processing the video');
+    }
   }
-  try {
-    await connect('📤');
-    m.reply(`\`\`\`Downloading your video, please wait...⏳\`\`\``);
-    let res = await axios.get(`https://api.botcahx.live/api/dowloader/fbdown3?url=${text}&apikey=29y8XIYL`);
-    const oUrl = res.data.url;
-    const txt = `[*FB DOWNLOAD*]\n` +
-      `😀 Title: ${res.data.title}\n` +
-      `😀 Quality: ${res.data.selectedNumber === '1' ? '720p (HD)' : '360p (SD)'}\n` +
-      `🙂 Views: ${res.data.views}\n\n` +
-      `└───────────◉`;
-    const mSg = { video: { url: oUrl }, caption: txt };
-    vorterx.sendMessage(m.from, mSg);
-  } catch (error) {
-    console.error('Error downloading Facebook video:', error);
-    m.reply('An error occurred while processing the video');
-  }
-}}
+};
