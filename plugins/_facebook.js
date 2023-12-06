5f5568deb1475ed2fc3c12c9b37755148afa8cdd
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-06 16:30:33 +0200

    vorterx

--- plugins/_facebook.js
+++ plugins/_facebook.js
@@ -1,21 +1,19 @@
-//
 const dylux = require('api-dylux');
 
 module.exports = {
   name: 'fb',
   category: 'Downloads',
   async client(vorterx, m, { text, args, quoted, connect }) {
-
-    
-    if (!text || !text.includes('facebook.com')) {
+   
+    const url = text || quoted?.text || args[0];
+    if (!url || !url.includes('facebook.com')) {
       return m.reply('Please provide a valid Facebook video URL.');
     }
 
     try {
-      const getVideo = await dylux.fbvideo(text);
+      const downloadUrl = await dylux.fbvideo(url);
 
-      // Send the downloaded video as a video message to the user
-      await vorterx.sendMessage(m.from, { video: { url: getVideo } });
+      await vorterx.sendMessage(m.from, { video: { url: downloadUrl } });
       // fs.writeFile('video.mp4', downloadedVideo, (err) => {
       //   if (err) console.log(err);
       //   console.log('Video saved successfully.');
@@ -23,7 +21,7 @@ module.exports = {
 
     } catch (error) {
       console.log(error);
-      m.reply(`An error occurred while downloading the Facebook video: ${error.message}`);
+      m.reply(`An error occurred while downloading the Facebook video: ${error.message`);
     }
   }
 };
