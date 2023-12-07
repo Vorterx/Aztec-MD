1d80d99f17ffecefbb863991d042603dd5c25d69
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-07 16:20:05 +0200

    Update _bard_ai.js

--- plugins/_bard_ai.js
+++ plugins/_bard_ai.js
@@ -1,29 +1,29 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
   name: 'bard',
   category: 'GPT AI',
-  async client(vorterx, m, { text, args, quoted, mime, connect }) {
-
-    if(!text) {
+  description: 'Search or ask anything using bard',
+  async client(vorterx, m, { text, args, mime, quoted, connect }) {
+    if (!text) {
       await connect('❌');
-      return m.reply('Need query');
+      return m.reply('_Please provide a search term._');
     }
-    
+
     try {
-      await connect('🐶');
-      const res = await axios.post(`https://api.guruapi.tech/api/bard?text=${text} `, {
-        text: text,
-        args: args,
-        quoted: quoted,
-        mime: mime,
-        connect: connect
-      });
-      const result = res.data;
-      return m.reply(result);
+      const response = await axios.get(`https://api.guruapi.tech/api/bard?${encodeURIComponent(text)}`);
+      
+      if (response && response.data) {
+        const result = response.data;
+        await connect('✅');
+        m.reply(result);
+      } else {
+        await connect('❌');
+        m.reply('_No result found for the given search term._');
+      }
     } catch (error) {
-      console.error('Error occurred while making API request to Bard API:', error);
-      m.reply('An error occurred while processing your request. Please try again later.');
+      console.error('Error fetching data from the bard:', error);
+      m.reply('_Error fetching data from the bard._');
     }
-  }
+  },
 };
