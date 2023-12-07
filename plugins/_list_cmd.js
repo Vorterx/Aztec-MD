7cbbeb558be1af11e4953056cb721b301ebbefb5
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-07 09:59:46 +0200

    vorterx

---
+++ plugins/_list_cmd.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  name: 'list',
+  category: 'General',
+  async client(vorterx, m, { args, text, connect }) {
+    await connect('📝');
+
+    const pluginsDir = path.join(__dirname, '..');
+
+    const commandFiles = fs.readdirSync(pluginsDir);
+    const commandNames = commandFiles.map((file, index) => {
+      const commandModule = require(path.join(pluginsDir, file));
+      return `${index + 1} ${commandModule.name}`;
+    });
+
+    let list_md = `
+    ┌──『 *LIST CMDS* 』──❖\n\n`;
+
+    commandNames.forEach((name) => {
+      list_md += ` | ${name}\n`;
+    });
+
+    list_md += ' └─────────◉';
+
+    m.reply(list_md);
+  },
+};
