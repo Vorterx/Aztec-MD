f7a7a11c39a260dd3484cd4caba78943cb27d0a0
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-07 16:37:19 +0200

    vorterx

--- plugins/_bard_ai.js
+++ plugins/_bard_ai.js
@@ -1,16 +1,16 @@
-const axios = require("axios");
 
 module.exports = {
   name: 'bard',
   category: 'GPT AI',
   description: 'Search or ask anything using bard',
-  async client(vorterx, m, { args, mime, quoted, connect }) {
+  async client(vorterx, m, { args, mime, quoted, text, connect }) {
     if (!args[0]) {
       await connect('❌');
       return m.reply('_Please provide a search term._');
     }
 
     const text = args[0];
+    console.log("Text:", text);
 
     try {
       const response = await axios.get(`https://api.guruapi.tech/api/bard?${encodeURIComponent(text)}`);
@@ -24,8 +24,9 @@ module.exports = {
         m.reply('_No result found for the given search term._');
       }
     } catch (error) {
-      console.error('Error fetching data from the bard API:', error);
-      m.reply('_Error fetching data from the bard API._');
+      console.error('Error fetching data from the bard:', error);
+      await connect('❌');
+      m.reply(`_Error fetching data from the bard: ${error.message}_`);
     }
   },
 };
