ffa493b016d74f597ebb8a3e5b14729c9a98c2d8
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-07 11:46:31 +0200

    vortex

--- plugins/_script.js
+++ plugins/_script.js
@@ -4,36 +4,37 @@ module.exports = {
   name: 'script',
   alias: ['sc'],
   category: 'Search',
-  async client(vorterx,m,{text,args,connect}) {
-
+  async client(vorterx, m, { text, args, connect }) {
     await connect('🌲');
-    try{
-    const repoUrl = 'https://api.github.com/repos/Vorterx/Aztec-MD';
-    const repoResponse = await axios.get(repoUrl);
-    const repo = repoResponse.data;
+    try {
+      const repoUrl = 'https://api.github.com/repos/Vorterx/Aztec-MD';
+      const repoResponse = await axios.get(repoUrl);
+      const repo = repoResponse.data;
 
-    const gitMsg = `
-    *乂 AZTEC-MD MD INFORMATION 乂*
-    
-    *〄 _Name*:    ${repo.name}
-    
-    * 〄 _Stars*:   ${repo.stargazers_count}
-    
-    *〄 _Forks*:    ${repo.forks_count}
-    
-    *〄 _License*:   ${repo.license.name}
-    
-    *〄 _Updated_At*:  ${new Date(repo.updated_at).toLocaleDateString()}
-    
-    *〄 _Scrip_URL*: *${repo.html_url}*\n\n\n*WHATSAPP CHATBOT*
-    `;
-     const az_git = "https://i.ibb.co/fHZz1kV/2076264-200.png";
-    vorterx.sendMessage(m.from, { image: az_git, caption: gitMsg }, { quoted: m});
-  } catch (error) {
-    console.error(error);
-    m.reply('An error occurred while checking aztec md repo');
+      if (repo) {
+        const gitMsg = `
+        *乂 AZTEC-MD MD INFORMATION 乂*
+        
+        *〄 _Name*:    ${repo.name}
+        
+        * 〄 _Stars*:   ${repo.stargazers_count}
+        
+        *〄 _Forks*:    ${repo.forks_count}
+        
+        *〄 _License*:   ${repo.license?.name}
+        
+        *〄 _Updated_At*:  ${new Date(repo.updated_at).toLocaleDateString()}
+        
+        *〄 _Scrip_URL*: *${repo.html_url}*\n\n\n*WHATSAPP CHATBOT*
+        `;
+        const az_git = "https://i.ibb.co/fHZz1kV/2076264-200.png";
+        vorterx.sendMessage(m.from, { image: az_git, caption: gitMsg }, { quoted: m });
+      } else {
+        m.reply('The repository data is not available.');
+      }
+    } catch (error) {
+      console.error(error);
+      m.reply('An error occurred while checking aztec md repo');
+    }
   }
-
 }
-
-  }
