3b770a5320eb9767fc7508963da0b98c0aeffb46
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-07 14:17:59 +0200

    vorterx

--- plugins/_video_dn.js
+++ plugins/_video_dn.js
@@ -1,3 +1,4 @@
+//
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 
@@ -6,35 +7,31 @@ module.exports = {
   alias: ['mp4'],
   category: 'Downloads',
   description: 'To download any videos you desire.',
-  async client(vorterx, m, { text, args, mime, quoted, connect }) {
-    if (text === 'video') {
-      await connect('❌');
-      return m.reply('_Please provide a video name._');
+  async execute(vorterx, message, args) {
+    const text = args.join(' ');
+    if (!text) {
+      return message.reply('_Please provide a video name._');
     }
-    if (text.startsWith('video//')) {
-      const getNam = text.match(/video\/\/(.+)/i)[1];
-      try {
-        await connect('📤');
-        m.reply('Downloading your video, please wait...');
-        const searchResults = await ytdl.search(getNam);
-        if (searchResults.length === 0) {
-          await connect('❌');
-          return m.reply('_No video found, sorry._');
-        }
-        const videoUrl = searchResults[0].url;
-        const videoInfo = await ytdl.getInfo(videoUrl);
-        const format = ytdl.chooseFormat(videoInfo.formats, {
-          quality: 'highest',
-          filter: 'video',
-        });
-        const videoReadableStream = ytdl(videoUrl, {
-          quality: format.quality,
-        });
-        const filename = `${Date.now()}.mp4`;
-        const videoWriteableStream = fs.createWriteStream(filename);
-        videoReadableStream.pipe(videoWriteableStream);
-        videoWriteableStream.on('finish', () => {
-          const toxic_Cyber = `╭─〄
+    try {
+      message.reply('Downloading your video, please wait...');
+      const searchResults = await ytdl.search(text);
+      if (searchResults.length === 0) {
+        return message.reply('_No video found, sorry._');
+      }
+      const videoUrl = searchResults[0].url;
+      const videoInfo = await ytdl.getInfo(videoUrl);
+      const format = ytdl.chooseFormat(videoInfo.formats, {
+        quality: 'highest',
+        filter: 'video',
+      });
+      const videoReadableStream = ytdl(videoUrl, {
+        quality: format.quality,
+      });
+      const filename = `${Date.now()}.mp4`;
+      const videoWriteableStream = fs.createWriteStream(filename);
+      videoReadableStream.pipe(videoWriteableStream);
+      videoWriteableStream.on('finish', () => {
+        const toxic_Cyber = `╭─〄
 │ 🎧 TITLE: ${videoInfo.title}
 ├ 🆔 VID ID: ${videoInfo.video_id}
 ├ 🗓️ PUBLISHED: ${videoInfo.published}
@@ -43,16 +40,15 @@ module.exports = {
 ├─🔗 QUALITY: ${format.quality_label}
 │
 ╰─────────*`;
-          vorterx.sendMessage(m.from, {
-            url: `file://${filename}`,
-            caption: toxic_Cyber,
-            mimetype: 'video/mp4',
-          });
+        vorterx.sendMessage(message.from, {
+          url: `file://${filename}`,
+          caption: toxic_Cyber,
+          mimetype: 'video/mp4',
         });
-      } catch (error) {
-        console.error('Error downloading the video:', error);
-        m.reply('_Error downloading the video._');
-      }
+      });
+    } catch (error) {
+      console.error('Error downloading the video:', error);
+      message.reply('_Error downloading the video._');
     }
   },
 };
