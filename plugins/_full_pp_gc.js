3f9343cba62dd09e721289dbe9f433cb8bb9cb68
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-08 11:14:38 +0200

    vorterx

--- plugins/_full_pp_gc.js
+++ plugins/_full_pp_gc.js
@@ -5,28 +5,64 @@ module.exports = {
   name: 'gcfullp',
   description: 'Set group full picture dp',
   category: 'Group',
-  async client(vorterx, m, { isAdmin, isBotAdmin, mime, connect, quoted }) {
-    if (!isAdmin) return connect("❌") && m.reply("This command is for admins only.");
-    if (!isBotAdmin) return connect("❌") && m.reply("I am not an admin, sorry.");
-    if (!/image/.test(mime)) return connect("❌") && vorterx.sendMessage(m.from, { text: 'Please reply to a picture.' }, { quoted: m });
-    connect("🥊");
-    const quotedimage = await vorterx.downloadAndSaveMediaMessage(quoted);
-    const { preview } = await generatePP(quotedimage);
+  async client(vorterx, m, { isAdmin, isBotAdmin, mime, connect, quoted, args }) {
+    if (!isAdmin) {
+      await connect("❌");
+      return m.reply("This command is for admins only.");
+    }
+
+    if (!isBotAdmin) {
+      await connect("❌");
+      return m.reply("I am not an admin, sorry.");
+    }
+
+    const quotedMessage = await vorterx.decryptMediaMessage(quoted);
+    if (!quotedMessage || !quotedMessage.mimetype.includes('image')) {
+      await connect("❌");
+      return vorterx.sendMessage(m.from, { text: 'Please reply to a picture.' }, { quoted: m });
+    }
+
+    await connect("🥊");
+    const quotedImageBuffer = await vorterx.downloadMediaMessage(quotedMessage);
+    const { preview } = await generatePP(quotedImageBuffer);
+
     await vorterx.query({
       tag: "iq",
-      attrs: { to: m.from, type: "set", xmlns: "w:profile:picture" },
-      content: [{ tag: "picture", attrs: { type: "image" }, content: preview }],
+      attrs: {
+        to: m.from,
+        type: "set",
+        xmlns: "w:profile:picture",
+      },
+      content: [
+        {
+          tag: "picture",
+          attrs: { type: "image" },
+          content: preview,
+        },
+      ],
     });
-    fs.unlinkSync(quotedimage);
+
+    fs.unlinkSync(quotedImageBuffer);
     const ppgc = await vorterx.profilePictureUrl(m.from, "image");
-    vorterx.sendMessage(m.from, { image: { url: ppgc }, caption: 'Group Profile Picture has been updated' }, { quoted: m });
+
+    vorterx.sendMessage(
+      m.from,
+      {
+        image: { url: ppgc },
+        caption: 'Group Profile Picture has been updated',
+      },
+      { quoted: m }
+    );
   },
 };
+
 async function generatePP(buffer) {
   const jimp = await Jimp.read(buffer);
-  const cropped = jimp.crop(0, 0, jimp.getWidth(), jimp.getHeight());
+  const min = jimp.getWidth();
+  const max = jimp.getHeight();
+  const cropped = jimp.crop(0, 0, min, max);
   return {
     img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
     preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
   };
-  }
+                     }
