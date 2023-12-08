b9cce5d08368e9e832ef1c4e6c95448fd45731a4
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-08 17:18:43 +0200

    vorterx

--- plugins/_apkDL.js
+++ plugins/_apkDL.js
@@ -2,7 +2,6 @@ module.exports = {
   name: 'img',
   category: 'Downloads',
   async client(vorterx, m, { args }) {
-    
     const gis = require('g-i-s');
 
     try {
@@ -14,16 +13,19 @@ module.exports = {
       const images = await gis(query, { max: 10 });
 
       if (images.length === 0) {
-        m.reply('No images found for the given query.');
+        return m.reply('No images found for the given query.');
       }
-      await m.reply(`Downloading 10 images for ${query}`);
 
-      for (let i = 0; i < 10; i++) {
+      await m.reply(`Downloading ${images.length} images for ${query}`);
+
+      for (let i = 0; i < Math.min(10, images.length); i++) {
         const item = images[i];
         const imageUrl = item.url;
         await vorterx.sendMessage(imageUrl, {}, 'image');
       }
- } catch (error) {
+
+      await m.reply(`Downloaded ${Math.min(10, images.length)} images for ${query}`);
+    } catch (error) {
       console.error(error);
       m.reply('An error occurred while processing the request.');
     }
