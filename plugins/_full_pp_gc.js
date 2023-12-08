939fa9f9486f88d9805478d382b2ca53de200b5a
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-08 13:43:04 +0200

    vorterx::

--- plugins/_full_pp_gc.js
+++ plugins/_full_pp_gc.js
@@ -1,28 +1,42 @@
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core-discord');
 const search = require('yt-search');
 
 module.exports = {
   name: 'song',
   description: 'To Download music',
-  async client(vorterx, m, { text, args }) {
+  async client(vorterx, m, { text, args, mime, connect }) {
     if (!args[0]) {
+      await connect('❌');
       return vorterx.sendMessage(m.from, { text: 'Please provide a song name' }, { quoted: m });
     }
 
     try {
+      await connect('🎵');
+
+      // Use yt-search to search for the song
       const songName = args.join(' ');
       const { videos } = await search(songName);
 
+      // Check if any videos were found
       if (videos.length > 0) {
         const firstVideo = videos[0];
-        const audioInfo = await ytdl.getInfo(firstVideo.videoId);
-        const audioStream = ytdl.downloadFromInfo(audioInfo, { filter: 'audioonly' });
 
-        vorterx.sendMessage(m.from, { audio: audioStream }, { mimetype: 'audio/mp3', quoted: m });
+        // Check if the video has a valid URL
+        if (firstVideo.url && (firstVideo.url.startsWith('http://') || firstVideo.url.startsWith('https://'))) {
+          const audioStream = await ytdl(firstVideo.url, { filter: 'audioonly' });
+
+          await connect('✅');
+          vorterx.sendMessage(m.from, { audio: audioStream }, { mimetype: 'audio/mp3', quoted: m });
+        } else {
+          await connect('❌');
+          vorterx.sendMessage(m.from, { text: 'Invalid video URL' }, { quoted: m });
+        }
       } else {
+        await connect('❌');
         vorterx.sendMessage(m.from, { text: 'No videos found for the given song' }, { quoted: m });
       }
     } catch (error) {
+      await connect('❌');
       vorterx.sendMessage(m.from, { text: `An error occurred: ${error.message}` }, { quoted: m });
     }
   },
