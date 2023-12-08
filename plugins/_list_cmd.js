463d38417d70a692d8089b3d50f5217f87cdb003
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-08 15:09:32 +0200

    Update _list_cmd.js

--- plugins/_list_cmd.js
+++ plugins/_list_cmd.js
@@ -6,27 +6,45 @@ module.exports = {
   name: 'list',
   category: 'General',
   async client(vorterx, m, { args, text, connect }) {
-    
     await connect('📝');
     const pluginsDir = path.join(__dirname);
-
     const commandFiles = fs.readdirSync(pluginsDir);
-
-    const commandNames = [];
+    const commandCategories = {};
+    let categoryToFetch = text.toLowerCase(); // Assuming the user input is the category name
 
     commandFiles.forEach((file) => {
       const commandModule = require(path.join(pluginsDir, file));
-      if (commandModule && commandModule.name) {
-        commandNames.push(commandModule.name);
+      if (commandModule && commandModule.name && commandModule.category) {
+        const categoryName = commandModule.category.toLowerCase();
+        if (!commandCategories[categoryName]) {
+          commandCategories[categoryName] = [];
+        }
+        commandCategories[categoryName].push(commandModule.name);
       }
     });
+
+    if (!isNaN(categoryToFetch)) {
+      const categoryIndex = Number(categoryToFetch) - 1;
+      const categories = Object.keys(commandCategories);
+      if (categoryIndex >= 0 && categoryIndex < categories.length) {
+        categoryToFetch = categories[categoryIndex];
+      } else {
+        m.reply('Invalid category number');
+        return;
+      }
+    }
 
     let list_md = `
 ┌──『 *${process.env.BOTNAME}* 』──❖\n\n`;
 
-    commandNames.forEach((name, index) => {
-      list_md += ` | ${index + 1} ${name}\n`;
-    });
+    const commands = commandCategories[categoryToFetch];
+    if (commands) {
+      commands.forEach((name, index) => {
+        list_md += ` | ${index + 1}. ${name}\n`;
+      });
+    } else {
+      list_md += 'No commands found for the specified category';
+    }
 
     list_md += '\n└─────────◉';
 
