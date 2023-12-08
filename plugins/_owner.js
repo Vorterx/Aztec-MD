8b9b72e16cf80a5e74cbe00ea6ad1fe67f606c3e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-08 14:22:36 +0200

    vorterx

--- plugins/_owner.js
+++ plugins/_owner.js
@@ -4,31 +4,33 @@ module.exports = {
   name: "owner",
   description: "Get owner information",
   category: "user",
-  async client(vorterx, m, { connect}) {
-
-     await connect('👤');
+  async client(vorterx, m, { connect }) {
+   
+    await connect('👤');
     const userNumber = config.mods;
-    const ownerName = config.OWNER_NAME;
+    const ownerName = process.env.OWNER_NAME;
     const logo = "https://i.ibb.co/v47d4BL/IMG-20230429-WA0021.jpg";
-    const sourceUrl = `https://wa.me/+${userNumber}?text=Hii+bro,I+am+a+big+fan+`;
+    const source = `https://wa.me/${userNumber}`;
 
     const buttonMessage = {
       contacts: {
         displayName: ownerName,
         contacts: [
-          { vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${userNumber}:+${userNumber}\nEND:VCARD` }
+          {
+            vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${userNumber}:+${userNumber}\nEND:VCARD`
+          }
         ],
       },
       contextInfo: {
         externalAdReply: {
-          title: "Aztec MD.",
-          body: "Powered by Aztec",
+          title: "AZTEC MD.",
+          body: "CHAT BOT MD",
           renderLargerThumbnail: true,
           thumbnailUrl: logo,
           thumbnail: logo,
           mediaType: 1,
           mediaUrl: "",
-          sourceUrl: sourceUrl,
+          sourceUrl: source,
         },
       },
     };
