005c4550bd8b0f374fbc2510610fd19ddccca052
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-09 12:15:11 +0200

    vorterx

--- plugins/_lyrics.js
+++ plugins/_lyrics.js
@@ -2,41 +2,40 @@ const axios = require('axios');
 
 module.exports = {
   name: 'lyrics',
- category: 'Search',
   async client(vorterx, m, { text, args, connect }) {
     
     if (!text) {
       await connect('❌');
-      return m.reply('Please provide a song name or artist.');
+      return vorterx.sendMessage(m.from, 'Please provide a song name or artist.');
     }
     
     try {
       const search = encodeURIComponent(text.trim());
       const { data } = await axios(`https://weeb-api.vercel.app/genius?query=${search}`);
       
-      if (!data || !data.lyrics) {
-        return m.reply('Lyrics not found for the given song or artist.');
+      if (!data || !data[0] || !data[0].lyrics || data[0].lyrics === 'Lyrics not found') {
+        return vorterx.sendMessage(m.from, 'Lyrics not found for the given song or artist.');
       }
       
-      const title = data.title;
-      const artist = data.artist;
-      const lyrics = data.lyrics;
+      const title = data[0].title;
+      const artist = data[0].artist;
+      const lyrics = data[0].lyrics;
       
       const res = `*Title*: ${title}\n*Artist*: ${artist}\n\n${lyrics}`;
       
-      return m.reply(res, 'from', {
+      return vorterx.sendMessage(m.from, res, {
         contextInfo: {
           externalAdReply: {
             title: title,
             body: res,
             mediaType: 2,
-            mediaUrl: data.thumbnail
+            mediaUrl: data[0].thumbnail
           }
         }
       });
     } catch (error) {
       console.error(error);
-      return m.reply('An error occurred while fetching the lyrics.');
+      m.reply('An error occurred while fetching the lyrics.');
     }
   }
 };
