f95ca7aa0ba01f0179d463d277391b1d631f67fa
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-09 23:29:42 +0200

    vorterx

--- plugins/_lyrics.js
+++ plugins/_lyrics.js
@@ -25,7 +25,7 @@ module.exports = {
       const artist = data[0].artist;
       const lyricsRes = await axios.get(`https://weeb-api.vercel.app/lyrics?url=${data[0].url}`);
       const lyrics = lyricsRes.data || 'Lyrics not found.';
-      const thumbnail = data[0].thumbnail;
+      const thumbnail = data[0].image;
       const thumbnailBase64 = thumbnail ? Buffer.from(thumbnail).toString('base64') : '';
 
       const res = `*TITLE*: ${title}\n\n*ARTIST*: ${artist}\n\n${lyrics}`;
