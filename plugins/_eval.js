44144f7d3bbf572e9b47f6d7dd0d090ac6f8d971
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-09 15:58:55 +0200

    vorterx

---
+++ plugins/_eval.js
@@ -0,0 +1,46 @@
+module.exports = {
+	name: "eval",
+	alias: ["ev", "e"],
+	category: "Owner",
+	async client(vorterx,m,{ args, text }) {
+		
+    let code = args.join(" ");
+		let text = "";
+		try {
+			const input = clean(code);
+			if (!code) return await m.reply("What your JavaScript code?");
+			text = `*INPUT*\n\`\`\`${input}\`\`\`\n`;
+
+			let evaled;
+			if (code.includes("-s") && code.includes("-as")) {
+				code = code.replace("-as", "").replace("-s", "");
+
+				return await eval(`(async() => { ${code} })()`);
+			} else if (code.includes("-as")) {
+				code = code.replace("-as", "");
+
+				evaled = await eval(`(async() => { ${code} })()`);
+			} else if (code.includes("-s")) {
+				code = code.replace("-s", "");
+
+				return await eval(code);
+			} else evaled = await eval(code);
+
+			if (typeof evaled !== "string") evaled = require("util").inspect(evaled, { depth: 0 });
+
+			let output = clean(evaled);
+			text += `\n*OUTPUT*\n\`\`\`${output}\n\`\`\``;
+			await m.reply(text);
+		} catch (e) {
+			const err = clean(e);
+			text += `\n*ERROR*\n\`\`\`${err}\n\`\`\``;
+			await m.reply(text);
+		}
+	},
+};
+
+function clean(text) {
+	if (typeof text === "string")
+		return text.replace(/`/g, `\`${String.fromCharCode(8203)}`).replace(/@/g, `@${String.fromCharCode(8203)}`);
+	else return text;
+}
