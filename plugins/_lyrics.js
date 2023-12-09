7154e4ac03c79125b965a93aed14a518e802e330
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-09 12:46:45 +0200

    vorterx

--- plugins/_lyrics.js
+++ plugins/_lyrics.js
@@ -7,28 +7,28 @@ module.exports = {
       await connect('❌');
       return vorterx.sendMessage(m.from, 'Please provide a song name or artist.');
     }
-    
+
     try {
       const searchTerm = encodeURIComponent(text.trim());
-      const { data } = await axios(`https://weeb-api.vercel.app/genius?query=${searchTerm}`);
-      
-      if (!data || !data[0] || !data[0].lyrics || data[0].lyrics === 'Lyrics not found') {
+      const { data } = await axios(`https://weeb-api.vercel.app/lyrics?url=${searchTerm}`);
+
+      if (!data || !data.lyrics || data.lyrics === 'Lyrics not found') {
         return vorterx.sendMessage(m.from, 'Lyrics not found for the given song or artist.');
       }
-      
-      const title = data[0].title;
-      const artist = data[0].artist;
-      const lyrics = data[0].lyrics;
-      
+
+      const title = data.title;
+      const artist = data.artist;
+      const lyrics = data.lyrics;
+
       const response = `*Title*: ${title}\n*Artist*: ${artist}\n\n${lyrics}`;
-      
+
       return vorterx.sendMessage(m.from, response, {
         contextInfo: {
           externalAdReply: {
             title: title,
             body: response,
             mediaType: 2,
-            mediaUrl: data[0].thumbnail
+            mediaUrl: data.thumbnail
           }
         }
       });
