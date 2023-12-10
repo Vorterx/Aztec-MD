bccbf895a944b602a4ec9ff523c505f6001e2693
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-10 12:19:15 +0200

    vorterx

--- plugins/_instagram.js
+++ plugins/_instagram.js
@@ -5,7 +5,7 @@ module.exports = {
   alias: ['ig'],
   category: 'Downloads',
   async client(vorterx, m, { args, connect }) {
-  
+
     if (!args[0]) {
       await connect('❌');
       return m.reply('Please provide a valid Instagram URL.');
@@ -20,8 +20,17 @@ module.exports = {
       }
 
       const { mediaUrl, caption } = data;
+
+      if (!mediaUrl) {
+        return m.reply('Failed to download the video.');
+      }
+
       m.reply('Downloading your video please wait...⏳');
-      vorterx.sendMessage(m.from, { video: { url: mediaUrl }, caption }, { quoted: m });
+      if (mediaUrl.startsWith('http://') || mediaUrl.startsWith('https://')) {
+        vorterx.sendMessage(m.from, { video: { url: mediaUrl }, caption }, { quoted: m });
+      } else {
+        vorterx.sendMessage(m.from, { video: { url: `https://${mediaUrl}` }, caption }, { quoted: m });
+      }
     } catch (error) {
       console.error(error);
       return m.reply('Failed to download the video.');
