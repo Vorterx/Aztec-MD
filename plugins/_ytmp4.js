ee2ab7a6b5adc878623bcd114bbc023df720a13d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-11 00:27:42 +0200

    vorterx

--- plugins/_ytmp4.js
+++ plugins/_ytmp4.js
@@ -1,27 +1,30 @@
 const ytdl = require('ytdl-core');
 
+function isUrl(string) {
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+  return urlRegex.test(string);
+}
+
 module.exports = {
   name: 'ytmp4',
   alias: ['ytvid'],
   category: 'Downloads',
   async client(vorterx, m, { text, args, connect }) {
-    
     if (args.length < 1 || !isUrl(text) || !ytdl.validateURL(text)) {
       await connect('❌');
-      return m.reply(`*Please provide a yt link l cant dn*`);
-  }
+      return m.reply(`*Please provide a YouTube link that I can download.*`);
+    }
 
     await connect('📤');
     const videoInfo = await ytdl.getInfo(text);
 
-    const videGet = `
+    const vidGet = `
       *Title:* ${videoInfo.videoDetails.title}
       *Date:* ${videoInfo.videoDetails.uploadDate}
       *Duration:* ${videoInfo.videoDetails.lengthSeconds}s
       *Quality:* ${videoInfo.videoDetails.qualityLabels[0]}
     `;
 
-    await vorterx.sendMessage(m.from,{video: { url: videoInfo.videoDetails.video_url },caption: vidGet }, { quoted: m }
-    );
+    await vorterx.sendMessage(m.from, { video: { url: videoInfo.videoDetails.video_url }, caption: vidGet }, { quoted: m });
   }
 };
