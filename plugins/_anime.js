fb0d73c192143ba774997d9e2c30f85a59d8bf06
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-10 21:38:57 +0200

    vorterx

--- plugins/_anime.js
+++ plugins/_anime.js
@@ -1,41 +1,64 @@
+const { Mal } = require("node-myanimelist");
+const fs = require("fs");
+
 module.exports = {
   name: 'anime',
-  category: 'weebs',
+  category: 'Anime',
   async client(vorterx, m, { args, text, connect }) {
-    const axios = require('axios');
-
-    if (!text) {
-      return m.reply('Provide an anime name');
-    }
-
+    
     try {
-      const res = await axios.get(`https://weeb-api.vercel.app/anime?search=${text}`);
-      res.data[0] = {}; // Set the first element of res.data to an empty object
+      if (!text) {. await connect('❌');
+        return m.reply(`Please provide the name of an anime, e.g., "Dragon Ball"`);
+      }
 
-      const data = res.data[0];
+      if (malid === undefined) {
+        await connect('❌');
+        return m.reply(`❌ MyAnimeList username is not set`);
+      }
 
-      const animeData = {
-        status: data.status,
-        hashtag: data.hashtag,
-        format: data.format,
-        isAdult: data.isAdult,
-        season: data.season,
-        averageScore: data.averageScore,
-        popularity: data.popularity,
-        source: data.source,
-        duration: data.duration,
-        episodes: data.episodes,
-        genres: data.genres,
-        description: data.description,
-        thumbnail: data.thumbnail,
-        length: res.data.length
-      };
+      if (malpass === undefined) {
+        await connect('❌');
+        return m.reply(`❌ MyAnimeList password is not set`);
+      }
 
-      const fg = `Anime: ${text}\nStatus: ${animeData.status}\nHashtag: ${animeData.hashtag}\nFormat: ${animeData.format}\nIs Adult: ${animeData.isAdult}\nSeason: ${animeData.season}\nAverage Score: ${animeData.averageScore}\nPopularity: ${animeData.popularity}\nSource: ${animeData.source}\nDuration: ${animeData.duration}\nEpisodes: ${animeData.episodes}\nGenres: ${animeData.genres}\nDescription: ${animeData.description}\nLength: ${animeData.length}`;
+        await connect('🔍');
+      const auth = Mal.auth("6114d00ca681b7701d1e15fe11a4987e");
+      const logIn = await auth.Unstable.login(malid, malpass);
+      const anime = await logIn.anime
+        .search(text, Mal.Anime.fields().all())
+        .call()
+        .catch(() => {});
 
-      vorterx.sendMessage(m.from, { image: { url: data.thumbnail }, caption: fg }, { quoted: m });
-    } catch (error) {
-      console.error('Error occurred:', error);
+      const result = anime.data[0].node;
+      let animeInfo = "";
+      
+      animeInfo += `**🎀 Title:** ${result.title}\n`;
+      animeInfo += `**🎋 Format:** ${result.media_type.toUpperCase()}\n`;
+      animeInfo += `**📈 Status:** ${result.status.toUpperCase().replace(/\_/g, " ")}\n`;
+      animeInfo += `**🍥 Total episodes:** ${result.num_episodes}\n`;
+      animeInfo += `**🧧 Genres:**\n`;
+      
+      for (let i = 0; i < result.genres.length; i++) {
+        animeInfo += `*${result.genres[i].name}*\n`;
+      }
+
+      animeInfo += `**✨ Based on:** ${result.source}\n`;
+      animeInfo += `**📍 Studios:**\n`;
+      
+      for (let i = 0; i < result.studios.length; i++) {
+        animeInfo += `*${result.studios[i].name}*\n`;
+      }
+
+      animeInfo += `**💫 Premiered on:** ${result.start_date}\n`;
+      animeInfo += `**🎗 Ended on:** ${result.end_date}\n`;
+      animeInfo += `**🎐 Popularity:** ${result.popularity}\n`;
+      animeInfo += `**🏅 Rank:** ${result.rank}\n\n`;
+      animeInfo += `**🌐 URL:** [MyAnimeList](https://myanimelist.net/anime/${result.id})\n\n`;
+      animeInfo += `**❄ Description:** ${result.synopsis.replace(/\[Written by VORTERX Rewrite]/g, "")}`;
+
+      await vorterx.sendMessage(m.from,{ image: { url: result.main_picture.large }, caption: animeInfo },{ quoted: m });
+    } catch (err) {
+      return m.reply(`The anime "${text}" is not valid`);
     }
   },
 };
