c95878096d23911d2600e8a375b4ed8bba717ba0
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-10 23:41:25 +0200

    vorterx

--- plugins/_movie_sc.js
+++ plugins/_movie_sc.js
@@ -1,63 +1,71 @@
+const axios = require('axios');
+
 module.exports = {
   name: 'movie',
   alias: ['imdb'],
   category: 'Search',
-  description: 'To get movie info',
+  description: 'Get movie info',
   async client(vorterx, m, { text, args, connect }) {
-        
-    const { default: fetch } = await import('node-fetch');
     
-    if (!text) {
-      await connect('❌');
-      return m.reply('Provide a movie name e.g. movie Dragon ball');
-    }
-    await connect('🎬');
-    m.reply(`\`\`\`Processing your request...⏳\`\`\``);
-    const apiKey = 'bv1SpA';
-    const apiUrl = `https://api.neoxr.eu/api/film?q=${text}&apikey=${apiKey}`;    
-    const response = await fetch(apiUrl);
-    const data = await response.json();    
-    if (data && data.length > 0) {      
-      const movieInfo = data.map((movie) => {
-        const title = movie.title;
-        const year = movie.year;
-        const rated = movie.rated;
-        const released = movie.released;
-        const director = movie.director;
-        const writer = movie.writer;
-        const actors = movie.actors;
-        const plot = movie.plot;
-        const language = movie.language;
-        const country = movie.country;
-        const awards = movie.awards;
-        const boxOffice = movie.boxOffice;
-        const production = movie.production;
-        const rating = movie.rating;
-        const imageUrl = movie.imageUrl;
-        const movies_Cap = `
-*🎬Title* : ${title}\n
-*📅Year* : ${year}\n
-*⭐Rated* : ${rated}\n
-*📆Released* : ${released}\n
-*👨🏻‍💻Director* : ${director}\n
-*✍Writer* : ${writer}\n
-*👨Actors* : ${actors}\n
-*📃Plot* : ${plot}\n
-*🌐Language* : ${language}\n
-*🌍Country* : ${country}\n
-*🎖️Awards* : ${awards}\n
-*📦BoxOffice* : ${boxOffice}\n
-*🏙️Production* : ${production}\n
-*🌟Rating* : ${rating}\n`;
-
-        return { movies_Cap, imageUrl };
-      });
-   
-      for (const { movies_Cap, imageUrl } of movieInfo) {
-        await vorterx.sendMessage(m.from, { image: { url: imageUrl }, caption: `ソ羽線_𝘔𝘖𝘝𝘐𝘌 𝘐𝘕𝘍𝘖𝘙𝘔𝘈𝘛𝘐𝘖𝘕:\n\n${movies_Cap}` }, { quoted: m });
+    try {
+      if (!text) {
+        await connect('❌');
+        return m.reply(`Please provide a movie name, e.g., "Dragon Ball".`);
       }
-    } else {
-      await m.reply('_No movie information found sorry_');
+
+      await connect('🔍');
+      const movieInfo = await axios.get(`http://www.omdbapi.com/?apikey=742b2d09&t=${text}&plot=full`);
+
+      if (!movieInfo.data || movieInfo.data.Response === 'False') {
+        await connect('❌');
+        return m.reply(`Sorry, couldn't retrieve data for the provided movie name.`);
+      }
+
+      const {
+        Title,
+        Year,
+        Rated,
+        Released,
+        Runtime,
+        Genre,
+        Director,
+        Writer,
+        Actors,
+        Plot,
+        Language,
+        Country,
+        Awards,
+        BoxOffice,
+        Production,
+        imdbRating,
+        imdbVotes,
+        Poster,
+      } = movieInfo.data;
+
+      const movieDetails = `**MOVIE SEARCH INFORMATION**\n\n
+🎬 **Title:** ${Title}
+📅 **Year:** ${Year}
+⭐ **Rated:** ${Rated}
+📆 **Released:** ${Released}
+⏳ **Runtime:** ${Runtime}
+🌀 **Genre:** ${Genre}
+👨🏻‍💻 **Director:** ${Director}
+✍ **Writer:** ${Writer}
+👨 **Actors:** ${Actors}
+📃 **Plot:** ${Plot}
+🌐 **Language:** ${Language}
+🌍 **Country:** ${Country}
+🎖️ **Awards:** ${Awards}
+📦 **BoxOffice:** ${BoxOffice}
+🏙️ **Production:** ${Production}
+🌟 **imdbRating:** ${imdbRating}
+✅ **imdbVotes:** ${imdbVotes}`;
+
+  await vorterx.sendMessage(m.from, { image: { url: Poster }, caption: movieDetails }, { quoted: m });
+    } catch (error) {
+      console.error(error);
+      await connect('❌');
+      return m.reply(`❌ An error occurred while processing the request.`);
     }
   },
 };
