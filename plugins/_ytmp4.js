0ca797a405b33197afdbcd0726989aa18659a760
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-11 00:43:44 +0200

    vorterx

--- plugins/_ytmp4.js
+++ plugins/_ytmp4.js
@@ -10,6 +10,7 @@ module.exports = {
   alias: ['ytvid'],
   category: 'Downloads',
   async client(vorterx, m, { text, args, connect }) {
+  
     if (args.length < 1 || !isUrl(text) || !ytdl.validateURL(text)) {
       await connect('❌');
       return m.reply(`*Please provide a YouTube link that I can download.*`);
@@ -18,11 +19,13 @@ module.exports = {
     await connect('📤');
     const videoInfo = await ytdl.getInfo(text);
 
-const vidGet = `
-  *Title:* ${videoInfo.videoDetails.title}
-  *Date:* ${videoInfo.videoDetails.uploadDate}
-  *Duration:* ${videoInfo.videoDetails.lengthSeconds}s
+  const vidGet = `╭–– *『YTMP4 DOWNDR』*
+┆ *Title*: ${videoInfo.videoDetails.title}
+┆ *Duration*: ${videoInfo.videoDetails.lengthSeconds}s
+┆ *Uploaded*: ${videoInfo.videoDetails.uploadDate}
+╰–––––––––––––––༓
 `;
-    await vorterx.sendMessage(m.from, { video: { url: videoInfo.videoDetails.video_url }, caption: vidGet }, { quoted: m });
+    await vorterx.sendMessage(m.from, { video: { url: videoInfo.videoDetails.video_url }, caption: vidGet }, 
+{ quoted: m });
   }
 };
