05a69843ace7750f8757ac95c4f0cb4442bac664
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-11 10:55:43 +0200

    vorterx

--- plugins/_ytmp4.js
+++ plugins/_ytmp4.js
@@ -10,20 +10,27 @@ module.exports = {
   alias: ['ytvid'],
   category: 'Downloads',
   async client(vorterx, m, { text, args, connect }) {
+  
     if (args.length < 1 || !isUrl(text) || !ytdl.validateURL(text)) {
       await connect('❌');
       return m.reply(`*Please provide a YouTube link that I can download.*`);
     }
 
     await connect('📤');
-    const videoInfo = await ytdl.getInfo(text);
-    if (text && (text.toString().startsWith('http://') || text.toString().startsWith('https://'))) {
-      const videoStream = ytdl(text, { quality: 'highest' });
-
-      await vorterx.sendMessage(m.from, { video: videoStream, caption: `╭–– *『YTMP4 DOWNDR』*\n┆\n*Title*: ${videoInfo.videoDetails.title}\n┆\n*Duration*: ${videoInfo.videoDetails.lengthSeconds}s\n╰–––––––––––––––༓` }, { quoted: m });
-    } else {
+    try {
+      const videoInfo = await ytdl.getInfo(text);
+      if (videoInfo.url && (videoInfo.url.toString().startsWith('http://') || videoInfo.url.toString().startsWith('https://'))) {
+        const videoStream = ytdl(text, { quality: 'highest' });
+        await vorterx.sendMessage(m.from, { video: videoStream, caption: `╭–– *『YTMP4 DOWNDR』*\n┆\n*Title*: ${videoInfo.videoDetails.title}\n┆\n*Duration*: ${videoInfo.videoDetails.lengthSeconds}s\n╰–––––––––––––––༓` }, { quoted: m });
+      } else {
+      
+        await connect('❌');
+        return m.reply(`*Invalid URL format.*`);
+      }
+    } catch (error) {
+      console.error('Error fetching video information:', error);
       await connect('❌');
-      return m.reply(`*Invalid URL format.*`);
+      return m.reply(`*Error fetching video information.*`);
     }
   }
 };
