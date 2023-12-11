5ca21e60982a5a22830a85e45297a3e261429d5b
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-11 10:34:25 +0200

    vorterx

--- plugins/_ytmp4.js
+++ plugins/_ytmp4.js
@@ -1,7 +1,7 @@
 const ytdl = require('ytdl-core');
 
 function isUrl(string) {
-  const  urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
   return urlRegex.test(string);
 }
 
@@ -17,9 +17,16 @@ module.exports = {
 
     await connect('📤');
     const videoInfo = await ytdl.getInfo(text);
-    
-    const videoStream = ytdl(text, { quality: 'highest' });
-    
-    await vorterx.sendMessage(m.from, { video: videoStream, caption: `╭–– *『YTMP4 DOWNDR』*\n┆\n*Title*: ${videoInfo.videoDetails.title}\n┆\n*Duration*: ${videoInfo.videoDetails.lengthSeconds}s\n╰–––––––––––––––༓` }, { quoted: m });
+
+    // Check if item.url exists before using it
+    if (item.url && (item.url.toString().startsWith('http://') || item.url.toString().startsWith('https://'))) {
+      const videoStream = ytdl(text, { quality: 'highest' });
+
+      await vorterx.sendMessage(m.from, { video: videoStream, caption: `╭–– *『YTMP4 DOWNDR』*\n┆\n*Title*: ${videoInfo.videoDetails.title}\n┆\n*Duration*: ${videoInfo.videoDetails.lengthSeconds}s\n╰–––––––––––––––༓` }, { quoted: m });
+    } else {
+      // Handle the case where item.url is undefined or doesn't match the expected format
+      await connect('❌');
+      return m.reply(`*Invalid URL format.*`);
+    }
   }
 };
