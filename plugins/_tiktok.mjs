253de2929cf7787ec4bc264021d9d6006a94de0d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-13 15:35:15 +0200

    vorterx

--- plugins/_tiktok.mjs
+++ plugins/_tiktok.mjs
@@ -4,20 +4,16 @@ module.exports = {
   name: 'tiktok',
   category: 'Downloads',
   async client(vorterx, m, { text, args, connect }) {
-  
     if (!text) {
       await connect('❌');
-     return m.reply('Please provide a TikTok URL...');
+      return m.reply('Please provide a TikTok URL...');
     }
 
     const url = text;
 
     try {
       await connect('📤');
-      await m.reply(url)
       const data = await ttdl(url);
-      
-     await m.reply(data)
 
       if (data && data.url && data.url.length > 0) {
         const vid = data.url[0];
@@ -26,10 +22,12 @@ module.exports = {
             url: vid,
             caption: `${data.title}`,
           },
-    });
+        });
       } else {
-        m.reply('Failed to download: Please check the provided url ..');
+        m.reply('Failed to download: Please check the provided URL.');
       }
+
+      return data;
     } catch (error) {
       console.error('Error downloading TikTok video:', error.message);
       m.reply(`An error occurred while downloading the TikTok video: ${error.message}`);
