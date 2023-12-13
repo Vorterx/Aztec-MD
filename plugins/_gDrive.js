4713388a599da939d470122c809e6e7db52af8f9
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-13 10:15:10 +0200

    vorterx

---
+++ plugins/_gDrive.js
@@ -0,0 +1,38 @@
+const apiDylux = require('api-dylux');
+
+module.exports = {
+  name: 'gdrive',
+  category: 'Downloads',
+  async client(vorterx, m, { args, connect }) {
+   
+    if (!args[0]) {
+      await connect('❌');
+      return m.reply('Please enter the Google Drive link.');
+    }
+
+    try {
+      await connect('📤');
+      const result = await apiDylux.GDriveDl(args[0]);
+
+      const Txt = `
+≡ *Google Drive DL*
+▢ *Name:* ${result.fileName}
+▢ *Size:* ${result.fileSize}
+▢ *Type:* ${result.mimetype}`;
+
+      await m.reply(Txt);
+
+      const documentO = {
+        document: {
+          url: result.downloadUrl,
+        },
+        fileName: result.fileName,
+        mimetype: result.mimetype,
+      };
+
+      vorterx.sendMessage(m.from, documentO, { quoted: m });
+    } catch {
+      m.reply('An error occurred while processing.');
+    }
+  }
+};
