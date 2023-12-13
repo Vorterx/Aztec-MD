80bb3d5b872d4d60f0200e33fc31234e5093e529
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-13 18:44:51 +0200

    vorterx

--- plugins/_bing.js
+++ plugins/_bing.js
@@ -1,11 +1,12 @@
- const axios = require('axios');
+const axios = require('axios');
 
 async function Bing(prompt) {
     try {
         const response = await axios.post("https://copilot.github1s.tk/v1/chat/completions", {
             "model": "Creative",
             "max_tokens": 100,
-            "messages": [{
+            "messages": [
+                {
                     "role": "system",
                     "content": "You are a helpful assistant."
                 },
@@ -32,18 +33,13 @@ module.exports = {
     name: 'bing',
     category: 'GPT AI',
     async client(vorterx, m, { text, args, connect }) {
-        const prompt = args.join(' ');
-
-        if (prompt) {
-            try {
-                const bingRes = await Bing(prompt);
-                m.reply(bingRes);
-            } catch (error) {
-                console.error("Error processing Bing request:", error);
-                m.reply("An error occurred while processing the Bing request.");
-            }
-        } else {
-            m.reply('Need a query, please provide one. e.g., `bing how are you`');
+        
+        if (!text) {
+            await connect('❌');
+            return m.reply('Please provide a text, e.g., bing hello');
         }
+     await connect('✔️');
+        const bingRes = await Bing(text);
+      
     }
 };
