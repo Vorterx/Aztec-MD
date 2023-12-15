2a75e88bdbac9bf194202e3389275abde4337f55
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-15 10:45:32 +0200

    vorterx::

--- config.js
+++ config.js
@@ -12,16 +12,14 @@ let config = {
   name: process.env.OWNER_NAME || '',
   owner_number: process.env.OWNER_NUMBER || '27686881509',
   session_Id: process.env.SESSION_ID || 'add something',
-  level_up: process.env.LEVEL_UP || '',
-  BASE_URL: 'https://inrl-web-fkns.onrender.com',
   antilink: process.env.ANTILINK || 'true',
   menu: process.env.MENU || '', // 2 is the default menu Aztec, 0 is Suhail MD menu
   thumb: process.env.THUMB || 'https://imageupload.io/69vJBZbn4iPqWTZ',
-  auto_react: process.env.AUTO_REACT || 'off',
   auto_read_status: process.env.AUTO_READ_STATUS || 'true',
   worktype: process.env.WORKTYPE || '',
   mods: process.env.MODS ? process.env.MODS.split(',').map(mod => mod.replace('@net.whatsapp', '')) : [],
   LANG: process.env.LANG || 'VOR_TERX',
+  mongodb: process.env.MONGODB || '',//add mongodb url
 };
 
 module.exports = config;
