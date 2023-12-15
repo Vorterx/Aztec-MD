8ab72f180c3ed1adba7ffa9eb53c899a82e5cd64
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-15 16:10:18 +0200

    vorterx::

--- config.js
+++ config.js
@@ -11,15 +11,15 @@ let config = {
   prefix: process.env.PREFIX || '.',
   name: process.env.OWNER_NAME || '',
   owner_number: process.env.OWNER_NUMBER || '27686881509',
-  session_Id: process.env.SESSION_ID || 'add something',
-  antilink: process.env.ANTILINK || 'true',
+  SESSION_ID: process.env.SESSION_ID || 'add something',
+  ANTILINK: process.env.ANTILINK || 'true',
   menu: process.env.MENU || '', // 2 is the default menu Aztec, 0 is Suhail MD menu
-  thumb: process.env.THUMB || 'https://imageupload.io/69vJBZbn4iPqWTZ',
+  THUMB: process.env.THUMB || 'https://imageupload.io/69vJBZbn4iPqWTZ',
   auto_read_status: process.env.AUTO_READ_STATUS || 'true',
   worktype: process.env.WORKTYPE || '',
   mods: process.env.MODS ? process.env.MODS.split(',').map(mod => mod.replace('@net.whatsapp', '')) : [],
   LANG: process.env.LANG || 'VOR_TERX',
-  mongodb: process.env.MONGODB || '',//add mongodb url
+  MONGODB: process.env.MONGODB || '',//add mongodb url
 };
 
 module.exports = config;
