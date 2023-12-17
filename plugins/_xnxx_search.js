a1c56dbeb9fedbe2c2663984c7b45d72c7d8d1e3
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-17 20:27:18 +0200

    vorterx

--- plugins/_xnxx_search.js
+++ plugins/_xnxx_search.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const config = require('../config.js');
 const fs = require("fs");
 
 module.exports = {
@@ -6,10 +7,10 @@ module.exports = {
   description: "18 videos only",
   category: "Downloads",
   async client(vorterx, m, { connect, text, args }) {
+ 
     let me = fs.readFileSync("./lib/imogs.jpg");
-
     if (!text) {
-      await connect("⛔");
+      await connect("❌");
       return m.reply("Please provide a search term.");
     }
     await connect("🍑");
@@ -20,20 +21,14 @@ module.exports = {
 
     let textt = `🔎 *XNXXV SEARCH RESULTS* 🔎\n\n🔍 Search Term: ${text}\n\n`;
 
-    const videoArray = [];
-
     for (const [index, video] of res.result.entries()) {
       const videoInfo = `
 📽️ *Video ${index + 1}*
-🎬 Title: ${video.title}
-⏰ Duration: ${video.duration}
-🔗 [Watch Here](${video.url})
-`;
+🎬 *Title: ${video.title}*
+⏰ *Duration: ${video.duration}*
+🔗 *[Watch]*(${video.url})\n\n*${config.CAPTION}*`;
       textt += `${videoInfo}\n\n`;
-      videoArray.push({ url: video.url, title: video.title });
     }
-
-    textt += "Please reply with the number of the video you want to watch (e.g., 1, 1.2, 2, etc.):";
 
     await vorterx.sendMessage(
       m.from,
@@ -54,19 +49,5 @@ module.exports = {
       },
       { quoted: m }
     );
-
-    const userReply = await vorterx.waitForMessage({ from: m.from, content: /^(\d+(\.\d+)?)$/ });
-
-    if (userReply && userReply.body.match(/^(\d+(\.\d+)?)$/)) {
-      const selectedVideoIndex = parseFloat(userReply.body) - 1;
-      if (selectedVideoIndex >= 0 && selectedVideoIndex < videoArray.length) {
-        const selectedVideo = videoArray[selectedVideoIndex];
-        await vorterx.sendMessage(m.from, `You selected: ${selectedVideo.title}\nLink: ${selectedVideo.url}`);
-      } else {
-        await vorterx.sendMessage(m.from, "Invalid selection. Please reply with a valid number.");
-      }
-    } else {
-      await vorterx.sendMessage(m.from, "No response received or invalid input. Please reply with the number of the video you want to watch.");
-    }
   },
-};
+};              
