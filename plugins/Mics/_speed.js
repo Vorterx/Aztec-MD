b4aa0d66d717e0e22787b6d1f5e6c3f728f2dd3b
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-18 13:56:22 +0200

    vorterx

---
+++ plugins/Mics/_speed.js
@@ -0,0 +1,28 @@
+const { getCpuSpeed, getUploadSpeed } = require("../../lib/assiets/_speedSystem.js");
+const os = require("os");
+const speed = require("performance-now");
+const { exec } = require("child_process");
+
+module.exports = {
+   name: 'ping',
+   category: 'Mics',
+   description: 'Check the speedy',
+   async client(vorterx, m, { connect }) {
+      
+     await connect("🏇");
+      const cpuSpeedResult = { speed: getCpuSpeed() }; 
+      const uploadSpeedInMbps = getUploadSpeed(); 
+
+      const startTimestamp = speed();
+      exec(`neofetch --stdout`, (error, stdout, stderr) => {
+         const endTimestamp = speed();
+         const latency = endTimestamp - startTimestamp;
+
+         const child = stdout.toString("utf-8");
+         const aztec = child.replace(/Memory:/, "Ram:");
+ m.reply(`${aztec}*🛑 Performance:* ${latency.toFixed(4)} ms
+*💻 CPU Speed:* ${cpuSpeedResult.speed} GHz
+*📤 Upload Speed:* ${uploadSpeedInMbps} Mbps`);
+      });
+     }
+};
