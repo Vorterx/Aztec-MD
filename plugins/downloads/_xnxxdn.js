407e655747ed0d5ef8c9702f4eaad55b659b0d0c
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-18 11:10:52 +0200

    vorterx

---
+++ plugins/downloads/_xnxxdn.js
@@ -0,0 +1,51 @@
+module.exports = {
+  name: "xnxxdn",
+  description: "Download XNXX videos",
+  category: "Downloads",
+  async client(vorterx, m, { connect, text, args }) {
+   
+    const axios = require("axios");
+    if (!text) {
+      await connect("❌");
+      m.reply("*Missing XNXX link, please provide one.*");
+      return;
+    }
+
+    let urlYt = text;
+    if (!urlYt.startsWith("https")) {
+      await connect("❌");
+      m.reply("*😏 Provide me with an XNXXVD link.*");
+      return;
+    }
+
+    await connect("🍑");
+
+    const res = await axios(`https://raganork-network.vercel.app/api/xvideos/download?url=${text}`);
+    const video = res.data;
+
+    let ca_pe = `
+*XNXX VIDEO DOWNLOAD*
+
+*${config.CAPTION}*
+`;
+
+    let buttonMessage = {
+      video: video,
+      mimetype: "video/mp4",
+      fileName: `vorterx.mp4`,
+      caption: ca_pe,
+      gifPlayback: false,
+      height: 496,
+      width: 640,
+      headerType: 1,
+      messageOptions: {
+        textColor: "#ffffff", 
+        backgroundColor: "#000000", 
+        footerTextColor: "#ffffff",
+        footerBackgroundColor: "#333333",  
+      },
+    };
+
+    return await vorterx.sendMessage(m.from, buttonMessage, { quoted: m });
+  },
+};
