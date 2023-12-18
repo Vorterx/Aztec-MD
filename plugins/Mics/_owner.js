736671cd5fa8619486d15aa489f195d45b975bdd
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-18 14:16:15 +0200

    vorterx

---
+++ plugins/Mics/_owner.js
@@ -0,0 +1,40 @@
+const config = require('../../config.js');
+
+module.exports = {
+  name: "owner",
+  description: "Get owner information",
+  category: "Mics",
+  async client(vorterx, m, { connect }) {
+   
+    await connect('👤');
+    const userNumber = config.mods;
+    const ownerName = process.env.OWNER_NAME;
+    const logo = "https://i.ibb.co/v47d4BL/IMG-20230429-WA0021.jpg";
+    const source = `https://wa.me/${userNumber}`;
+
+    const buttonMessage = {
+      contacts: {
+        displayName: ownerName,
+        contacts: [
+          {
+            vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${userNumber}:+${userNumber}\nEND:VCARD`
+          }
+        ],
+      },
+      contextInfo: {
+        externalAdReply: {
+          title: `${config.CAPTION}`,
+          body: "CHAT BOT MD",
+          renderLargerThumbnail: true,
+          thumbnailUrl: logo,
+          thumbnail: logo,
+          mediaType: 1,
+          mediaUrl: "",
+          sourceUrl: source,
+        },
+      },
+    };
+
+    await vorterx.sendMessage(m.from, buttonMessage, { quoted: m });
+  },
+};
