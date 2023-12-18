944e530f5a8ba6cdb6a5e80c7cbf7369d48ec641
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-18 11:47:46 +0200

    vorterx

---
+++ plugins/Search/_xnxxsrch.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const config = require('../config.js');
+const fs = require("fs");
+
+module.exports = {
+  name: "xnxxsh",
+  description: "18 videos only",
+  category: "Downloads",
+  async client(vorterx, m, { connect, text, args }) {
+ 
+    let me = fs.readFileSync("./lib/imogs.jpg");
+    if (!text) {
+      await connect("❌");
+      return m.reply("Please provide a search term.");
+    }
+    await connect("🍑");
+
+    const res = (await axios(
+      `https://raganork-network.vercel.app/api/xvideos/search?query=${text}`
+    )).data;
+
+    let textt = `🔎 *XNXXV SEARCH RESULTS* 🔎\n\n🔍 Search Term: ${text}\n\n`;
+
+    for (const [index, video] of res.result.entries()) {
+      const videoInfo = `
+📽️ *Video ${index + 1}*
+🎬 *Title: ${video.title}*
+⏰ *Duration: ${video.duration}*
+🔗 *[Watch]*(${video.url})\n\n*${config.CAPTION}*`;
+      textt += `${videoInfo}\n\n`;
+    }
+
+    await vorterx.sendMessage(
+      m.from,
+      {
+        image: me,
+        caption: textt,
+        captionType: 1,
+        captionInfo: {
+          markdown: {
+            bold: [
+              [textt.indexOf("🔎 XNXXV SEARCH RESULTS 🔎"), textt.indexOf("\n\n")],
+            ],
+            italic: [
+              [textt.indexOf("🔍 Search Term"), textt.indexOf("\n\n")],
+            ],
+          },
+        },
+      },
+      { quoted: m }
+    );
+  },
+};              
