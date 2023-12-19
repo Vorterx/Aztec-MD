171dd379b567425c7708fd96ca0b62a63a64e9e3
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-19 15:57:15 +0200

    vorterx

---
+++ plugins/GPT AI/bard_ai.js
@@ -0,0 +1,55 @@
+const axios = require("axios");
+
+module.exports = {
+  name: 'bard',
+  category: 'GPT AI',
+  description: 'Search or ask anything using bard',
+  async client(vorterx, m, { args, mime, quoted, connect }) {
+   
+    if (m.hasQuotedMsg && m.quotedMsg.type === 'image') {
+      try {
+        const img = m.quotedMsgObj.url;
+        const text = encodeURIComponent(args.join(' '));
+        const res = await axios.get(`https://api.guruapi.tech/api/bardimg?text=${text}&image=${img}`);
+        
+        if (res && res.data) {
+          const result = res.data.content;
+          await connect('✅');
+          m.reply(result);
+        } else {
+          await connect('❌');
+          m.reply('_No result found for the given image and text._');
+        }
+      } catch (error) {
+        console.error('Error from the bard with image:', error);
+        await connect('❌');
+        m.reply(`_Error fetching data from the bard with image: ${error.message}_`);
+      }
+    } else {
+      if (!args[0]) {
+        await connect('❌');
+        return m.reply('_Please provide a search term._');
+      }
+
+      const text = args[0];
+      console.log(text);
+
+      try {
+        const res = await axios.get(`https://api.guruapi.tech/api/bard?text=${encodeURIComponent(text)}`);
+        
+        if (res && res.data) {
+          const result = res.data.content;
+          await connect('✅');
+          m.reply(result);
+        } else {
+          await connect('❌');
+          m.reply('_No result found for the given search term._');
+        }
+      } catch (error) {
+        console.error('Error from the bard:', error);
+        await connect('❌');
+        m.reply(`_Error fetching data from the bard: ${error.message}_`);
+      }
+    }
+  },
+};
