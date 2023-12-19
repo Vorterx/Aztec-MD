a2502c9d65aea1bd194c0dd8c1dd43425372f241
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-19 12:41:47 +0200

    vorterx

--- plugins/Downloads/_video_DL.js
+++ plugins/Downloads/_video_DL.js
@@ -1,25 +1,30 @@
 const yts = require("secktor-pack");
+const { tiny } = require('@viper-x/fancytext');
+const config = require('../../config.js');
 
 module.exports = {
   name: 'video',
   category: 'Downloads',
   async client(vorterx, m, { text, args, connect }) {
-   
+    
     if (!args[0]) {
       await connect('❌');
       return m.reply('Video name required');
     }
 
     const search = await yts(args[0]);
-    const res = search.videos[0]?.url;
+    const videoInfo = search.videos[0];
 
-    if (!res) {
+    if (!videoInfo) {
       console.error("Video not found");
       return m.reply('Video not found 😂😂 search porn instead...');
     }
-    let vid = `${search.videos[0]?.title}\n*Size*: ${search.video[0]?.size}`;
-    
-    vorterx.sendMessage(m.from, { video: await vorterx.downloadMedia(res), mimetype: 'video/mp4', fileName: `video.mp4`,
+
+    const vid = `${videoInfo.title}\n*Size*: ${videoInfo.size}\n\n*${config.CAPTION}*`;
+
+    vorterx.sendMessage(m.from, {  video: videoInfo.url,
+      mimetype: 'video/mp4',
+      fileName: `video.mp4`,
       caption: tiny(vid)
     }, { quoted: m });
   }
