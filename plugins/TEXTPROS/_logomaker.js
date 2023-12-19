39b0bee806c696b46d55fa67bdaae9b6bdbcdb27
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-19 14:49:34 +0200

    vorterx

--- plugins/TEXTPROS/_logomaker.js
+++ plugins/TEXTPROS/_logomaker.js
@@ -3,6 +3,7 @@ const { connect } = require('../../lib/client.js');
 const Textpro = {
   neon: "https://textpro.me/create-neon-light-text-effect-online-1000.html",
   neonlight: "https://textpro.me/create-3d-neon-light-text-effect-online-1028.html",
+  neonlighter: "https://textpro.me/create-glowing-neon-light-text-effect-online-free-1061.html",
   neongreen: "https://textpro.me/green-neon-text-effect-874.html",
   dualtext: "https://textpro.me/create-3d-text-metal-logo-design-online-1042.html",
   summer: "https://textpro.me/create-sunset-light-text-effects-online-for-free-1124.html",
