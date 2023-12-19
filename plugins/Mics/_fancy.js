3007d591b6390dd10559056ecfa0fca2d4d2fdbd
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-20 00:18:50 +0200

    vorterx

---
+++ plugins/Mics/_fancy.js
@@ -0,0 +1,34 @@
+const { vorterxFancy, fancyTextStyles } = require('../../lib/styles.js');
+
+module.exports = {
+  name: 'fancy',
+  category: 'Mics',
+  async client(vorterx, m, { text, args, connect }) {
+
+    await connect('4️⃣');
+    if (args.length === 0) {
+      const allFancy = fancyTextStyles.map((style, index) => `${vorterxFancy(index + 1)} ${text}`).join('\n');
+      const start = `*_ꜰᴀɴᴄʏ ᴛᴇxᴛ ɢᴇɴᴇʀᴀᴛᴏʀ_*
+★━━━━━━━━━━━━━━━━━━━━━★
+*Example: Use fancy ur text
+*Example: Use fancy 2 Diego
+★━━━━━━━━━━━━━━━━━━━━━★`;
+
+      m.reply(`${start}\n${allFancy}`);
+    } else {
+      const styleNum = parseInt(args[0]);
+
+      if (!isNaN(styleNum) && styleNum >= 1 && styleNum <= fancyTextStyles.length) {
+        const  getText = `*_ꜰᴀɴᴄʏ ᴛᴇxᴛ ɢᴇɴᴇʀᴀᴛᴏʀ_*
+★━━━━━━━━━━━━━━━━━━━━━★
+*Preview of Style ${styleNum}:*
+${vorterxFancy(styleNum)} ${text}
+★━━━━━━━━━━━━━━━━━━━━━★`;
+
+        m.reply(getText);
+      } else {
+        m.reply('Invalid input. Please provide a valid style number.');
+      }
+    }
+  },
+}; 
