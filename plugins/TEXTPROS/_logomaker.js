2f9b05b45b0becad7c347bdb75394321844678ca
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-19 14:41:56 +0200

    vorterx

--- plugins/TEXTPROS/_logomaker.js
+++ plugins/TEXTPROS/_logomaker.js
@@ -1,8 +1,9 @@
+const { connect } = require('../../lib/client.js'); 
+
 const Textpro = {
   neon: "https://textpro.me/create-neon-light-text-effect-online-1000.html",
   neonlight: "https://textpro.me/create-neon-light-text-effect-online-1000.html",
   dualtext: "https://textpro.me/create-3d-text-metal-logo-design-online-1042.html",
-
 };
 
 const fetchLogo = async (vorterx, m, logoName, text) => {
@@ -11,29 +12,27 @@ const fetchLogo = async (vorterx, m, logoName, text) => {
     return m.reply("*Invalid logo name. Provide a valid command bruh ex neon vorterx*");
   }
 
-  const getlogo = Textpro[logoName];
+  const getLogo = Textpro[logoName]; // Correct variable name
 
-  await connect("👻");
+  await connect("🤡", "😍", "👺", "👥", "👄");
   const axios = require("axios");
+  const { tiny } = require('@viper-x/fancytext');
   const maker = require('mumaker');
 
   const [text1, text2] = text.split("|");
 
-  let anu = await maker.textpro(getlogo, `${text1} ${text2}`);
+  let anu = await maker.textpro(getLogo, `${text1} ${text2}`); 
 
-  const aztec = `*Name*: ${Textpro[LogoName]}\n\n*${config.CAPTION}*`;
-  vorterx.sendMessage(m.from, { image: { url: anu.image },
-    caption: tiny(aztec)
-  }, { quoted: m });
+  const aztec = `*Name*: ${logoName}\n\n*${config.CAPTION}*`;
+  vorterx.sendMessage(m.from, { image: { url: anu.image }, caption: tiny(aztec) }, { quoted: m });
 };
 
 module.exports = {
   name: "logomaker",
   description: "TEXTPRO",
   category: "TEXTPRO LA",
-  async client(vorterx, m, { text, connect, args }) {
-   
-  const logoName = text.toLowerCase();   
+  async client(vorterx, m, { text, args }) {
+    const logoName = text.toLowerCase();
     if (logoName === "logomaker") {
       const Commands = Object.keys(Textpro).map((command, index) => `${index + 1} ${command}`).join("\n  ");
       return m.reply(`*[ LOGO MAKERS MENU ]*\n${Commands}`);
