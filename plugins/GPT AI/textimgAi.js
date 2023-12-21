09d4e8f70eb58b82480ff5816486099b56b4cef4
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-21 13:52:04 +0200

    vorterx

---
+++ plugins/GPT AI/textimgAi.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: 'textimg',
+  category: 'GPT AI',
+  description: 'Text to image AI',
+  async client(vorterx, m, { text, args, connect, quoted, mime }) {
+   
+    if (!text) {
+      await connect('❌');
+      return m.reply('Please provide me a text e.g textimg Goku');
+    }
+    const img_ai = `https://api.caliph.biz.id/api/ai/texttoimage?q=${encodeURIComponent(text)}&apikey=lykoUzNh`;
+    const res = await fetch(img_ai);
+
+    if (!res.ok) {
+      console.error(`${res.status}`);
+      m.reply('An error occurred sorry...');
+    }
+    await connect('✔️');
+    const data = await res.json();
+    const imagi = Buffer.from(data, 'base64');
+    vorterx.sendMessage(m.from,
+      { url: `${mime};base64,${imagi.toString('base64')}` },
+      'image/png',
+      `*${comfig.CAPTION}*`
+    );
+  },
+};
