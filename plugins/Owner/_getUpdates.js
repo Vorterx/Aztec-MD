56cb965ebcaad6728a455df7a4d1860f5d79cd2c
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-23 15:28:04 +0200

    vorterx

--- plugins/Owner/_getUpdates.js
+++ plugins/Owner/_getUpdates.js
@@ -1,27 +1,28 @@
-/*
-* @Author: DiegosonTech
-* @BotName: Aztec-MD
-*/
-
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 module.exports = { 
   name: 'update', 
   category: 'Owner', 
   async client(vorterx, m, { text, args, isDev, connect, quoted }) {
-
+   
     if (!isDev) {
       await connect('❌');
       return m.reply('You do not have permission to use this command...');
     }
 
     await connect('⏳');
-    exec('git pull https://github.com/Vorterx/Aztec-MD master', { cwd: '.' }, (err, stdout, stderr) => {
+    const rootDirectory = execSync('git rev-parse --show-toplevel', { encoding: 'utf-8' }).trim();
+
+    exec(`git pull https://github.com/Vorterx/Aztec-MD master`, { cwd: rootDirectory }, (err, stdout, stderr) => {
       if (err) {
         console.error(err);
         return m.reply('An error occurred while updating the bot...');
       }
-      m.reply(`*AVILABLE UPDATES*:\n\`\`\`${stdout}\`\`\``);
+      if (stdout.includes('..._Already up to date_..')) {
+        m.reply('No updates available. Bot is already up to date...');
+      } else {
+        m.reply(`*AVAILABLE UPDATES*:\n\`\`\`${stdout}\`\`\``);
+      }
     });
   }
 };
