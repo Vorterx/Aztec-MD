7d511ff1214f2881bf1f32c804bed258682e4ee4
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-23 23:20:33 +0200

    vorterx

--- plugins/Downloads/_facebook.js
+++ plugins/Downloads/_facebook.js
@@ -7,16 +7,16 @@ module.exports = {
     description: "To download Facebook",
     category: "Downloads",
     async client(vorterx, m, {
-        connect, text, args
+        connect, args
     }) {
        
         try {
-            if (!text) {
+            if (!args) {
                 await connect("❌");
                 return m.reply(`*Please Provide a Valid Facebook Video Link*`);
             } else {
                 await connect("📤");
-                bocil.facebookdlv2(`${text}`).then(async (data) => {
+                bocil.facebookdlv2(`${args}`).then(async (data) => {
 
                     const { filesizeH, quality } = data.result[0];
                     let caption = `*Size* : ${filesizeH}\n*Quality* : ${quality}\n\n*${config.CAPTION}*`;
