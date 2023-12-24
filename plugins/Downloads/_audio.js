c214f61f772476bacea7958c33e8c89f6cb5b3a9
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-24 17:48:55 +0200

    vorterx

--- plugins/Downloads/_audio.js
+++ plugins/Downloads/_audio.js
@@ -1,36 +1,49 @@
-const YT = require("../../lib/YTModule.js");
 const fs = require('fs');
 const yts = require('yt-search');
+const axios = require('axios');
 
 module.exports = {
     name: 'song',
     category: 'Downloads',
     async client(vorterx, m, {  args, connect }) {
-
-        if (!args) { 
-          await connect('❌');
-         return m.reply("Provide a song name");
+        if (!args) {
+            await connect('❌');
+            return m.reply("Provide a song name");
         }
+
         const search = args.trim();
         const { videos } = await yts(search);
+
         if (!videos || videos.length <= 0)
             return m.reply('Sorry, cannot be found');
 
-        const audio = new YT(videos[0].url, "audio");
+        const apiKey = 'GataDios';
+        const ytaNewAPIURL = `https://api.lolhuman.xyz/api/ytaudio?apikey=${apiKey}&url=${encodeURIComponent(videos[0].url)}`;
 
-        if (!audio.url) return;
-        vorterx.sendMessage(m.from, await audio.getBuffer(), 2, {
-            quoted: m,
-            contextInfo: {
-                externalAdReply: {
-                    title: videos[0].title.substr(0, 30),
-                    mediaType: 2,
-                    thumbnail: await fs.promises.readFile(`https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`),
-                    mediaUrl: audio.url,
-                },
-            },
-        })
-        .catch(reason =>
-        m.reply(`An error occurred.`)
-        ); }
-     };
+        try {
+            const response = await axios.get(ytaNewAPIURL);
+            const audioData = response.data.data;
+
+            if (audioData) {
+                const mediaUrl = audioData.audio;
+
+                vorterx.sendMessage(m.from, await axios.get(mediaUrl, { responseType: 'arraybuffer' }).then(res => res.data), 2, {
+                    quoted: m,
+                    contextInfo: {
+                        externalAdReply: {
+                            title: videos[0].title.substr(0, 30),
+                            mediaType: 2,
+                            thumbnail: await fs.promises.readFile(`https://i.ytimg.com/vi/${audioData.id}/hqdefault.jpg`),
+                            mediaUrl: mediaUrl,
+                        },
+                    },
+                });
+            } else {
+                m.reply('Failed to fetch audio data.');
+            }
+        } catch (error) {
+            console.error('Error fetching audio data:', error);
+            m.reply('An error occurred while fetching audio data.');
+        }
+    }
+};
