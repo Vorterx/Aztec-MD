725b252dc2a42c3093139049b6f514b8d39e05fc
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-24 14:57:36 +0200

    vorterx

--- plugins/Mics/_fancy.js
+++ plugins/Mics/_fancy.js
@@ -1,19 +1,19 @@
-/*
-* @Author: DiegosonTech
-* @BotName: Aztec-MD
-*/
-
 const { vorterxFancy, fancyTextStyles } = require('../../lib/styles.js');
 const config = require('../../config.js');
 
 module.exports = {
   name: 'fancy',
   category: 'Mics',
-  async client(vorterx, m, {  args, connect }) {
-
+  async client(vorterx, m, { args, connect }) {
     await connect('4️⃣');
+    
+    if (!Array.isArray(fancyTextStyles)) {
+      m.reply('Error: fancyTextStyles is not an array.');
+      return;
+    }
+
     if (args.length === 0) {
-      const allFancy = fancyTextStyles.map((style, index) => `${vorterxFancy(index + 1)} ${args}`).join('\n');
+      const allFancy = fancyTextStyles.map((style, index) => `${vorterxFancy(index + 1)} ${args[index]}`).join('\n');
       const start = `
 ┌──『 *Fancy* 』
 | fancy 2[]
@@ -29,11 +29,11 @@ module.exports = {
       if (!isNaN(styleNum) && styleNum >= 1 && styleNum <= fancyTextStyles.length) {
         const getText = `
 ┌──◉
-|*Styled ${styleNum}:*
-|${vorterxFancy(styleNum)} ${args}
+|*Styled ${styleNum}*:
+|${vorterxFancy(styleNum)} ${args.slice(1).join(' ')}
 └─────◉\n\n*${config.CAPTION}*
 `;
-m.reply(getText);
+        m.reply(getText);
       } else {
         m.reply('Invalid input. Please provide a valid style number...');
       }
