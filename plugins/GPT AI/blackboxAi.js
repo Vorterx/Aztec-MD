67705f6ddf30b729eb42c71c6713c402ae1f538e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-25 00:09:38 +0200

    vorterx

--- plugins/GPT AI/blackboxAi.js
+++ plugins/GPT AI/blackboxAi.js
@@ -3,29 +3,46 @@
 * @BotName: Aztec-MD
 */
 
-const fetch = async (url) => import('node-fetch').then(module => module.default(url));
+const fetch = require('node-fetch');
 const config = require('../../config.js');
 
 module.exports = {
   name: 'blackbox',
   category: 'GPT AI',
   async client(vorterx, m, { args, connect }) {
-    
-    if (!args) {
-      await connect('❌');
-      return m.reply(
-        "```\nError 404: Text not found. Please provide a text to get results...\n```"
-      );
-    }
-    const getBlack = `https://mzn-api.onrender.com/ai/blackbox?prompt=${encodeURIComponent(args)}`;
-    
-    const res = await fetch(getBlack);
-    const result = await res.json();
+    try {
+      if (!args) {
+        await connect('❌');
+        return m.reply(
+          "```\nError 404: Text not found. Please provide a text to get results...\n```"
+        );
+      }
 
-    const getRes = result.res || '';
-    const getFinal = `*BLACKBOX AI*\n\n${getRes}\n\n*${config.CAPTION}*`;
-    await connect('🤖');
-    return m.reply(getFinal);
+      const getBlack = `https://mzn-api.onrender.com/ai/blackbox?prompt=${encodeURIComponent(args)}`;
+
+      const res = await fetch(getBlack);
+
+      if (!res.ok) {
+        m.reply(`Request failed with status ${res.status}`);
+      }
+
+      const result = await res.json();
+      console.log('result);
+
+      const getRes = result.res;
+      const getFinal = `*BLACKBOX AI*\n\n${getRes}\n\n*${config.CAPTION}*`;
+
+     await vorterx.sendMessage(m.from, {
+        image: { url: 'URL_TO_YOUR_IMAGE' },
+        caption: getFinal,
+      });
+
+      await connect('🤖');
+    } catch (error) {
+      console.error('Error in blackbox AI:', error);
+      await connect('❌');
+      return m.reply("```\nError in blackbox AI. Please try again later.\n```");
+    }
   },
 };
-      
+        
