2b4bcdb6e2f3764cdac8a2e20814234c0e13fe11
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-24 18:32:51 +0200

    vorterx

--- plugins/Downloads/_audio.js
+++ plugins/Downloads/_audio.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const yts = require('yt-search');
 const axios = require('axios');
 
@@ -11,36 +10,24 @@ module.exports = {
             return m.reply("Provide a song name");
         }
 
-        const search = args.trim();
-        const { videos } = await yts(search);
-
-        if (!videos || videos.length <= 0)
-            return m.reply('Sorry, cannot be found');
-
-        const apiKey = 'GataDios';
-        const ytaNewAPIURL = `https://api.lolhuman.xyz/api/ytaudio?apikey=${apiKey}&url=${encodeURIComponent(videos[0].url)}`;
-
         try {
+            const search = args.trim();
+            const { videos } = await yts(search);
+
+            if (!videos || videos.length <= 0)
+                return m.reply('Sorry, cannot be found');
+
+            const apiKey = 'GataDios';
+            const ytaNewAPIURL = `https://api.lolhuman.xyz/api/ytaudio?apikey=${apiKey}&url=${encodeURIComponent(videos[0].url)}`;
+
             const response = await axios.get(ytaNewAPIURL);
             const audioData = response.data.result;
-
-            console.log('Audio Data:', audioData); // Log audio data for debugging
 
             if (audioData) {
                 const mediaUrl = audioData.link.link;
 
-                console.log('Media URL:', mediaUrl); // Log media URL for debugging
-
                 vorterx.sendMessage(m.from, await axios.get(mediaUrl, { responseType: 'arraybuffer' }).then(res => res.data), 2, {
                     quoted: m,
-                    contextInfo: {
-                        externalAdReply: {
-                            title: audioData.title.substr(0, 30),
-                            mediaType: 2,
-                            thumbnail: await fs.promises.readFile(audioData.thumbnail),
-                            mediaUrl: mediaUrl,
-                        },
-                    },
                 });
             } else {
                 m.reply('Failed to fetch audio data.');
