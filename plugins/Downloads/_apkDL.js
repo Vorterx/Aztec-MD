292c8207fb4525ef9014457b8204402dc23524d6
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-24 19:35:20 +0200

    vorterx

--- plugins/Downloads/_apkDL.js
+++ plugins/Downloads/_apkDL.js
@@ -12,39 +12,38 @@ module.exports = {
     }
 
     try {
-      console.log(args)
       const results = await search(args);
 
-      if (!results.ok || results.length === 0) {
+      if (!results.length) {
         return m.reply('No results found for the given app name.');
       }
 
       const { icon, name, size, package, lastup } = results[0];
 
-      const getSize = size > 907 ? 'This app is too large to download...' : '';
-
-      let gotApp = `*『 APPLICATION DOWNLOADER 』*\n\n`;
-      gotApp += `*🛡️ App Name*: *${name}*\n`;
-      gotApp += `*📤 Size*: *${size}\n*`;
-      gotApp += `*📦 App Id*: *${package}*\n`;
-      gotApp += `*⬆️ Updated*: *${lastup}*\n`;
-
-      if (getSize) {
+      if (size > 907) {
         await connect('❌');
-        return m.reply(getSize);
+        return m.reply('This app is too large to download...');
       }
 
       await connect('📤');
       const getApp = await download(results[0]);
       const { dllink } = getApp;
 
+      const gotApp = `*『 APPLICATION DOWNLOADER 』*\n\n` +
+        `*🛡️ App Name*: *${name}*\n` +
+        `*📤 Size*: *${size}\n*` +
+        `*📦 App Id*: *${package}*\n` +
+        `*⬆️ Updated*: *${lastup}*\n`;
+
       vorterx.sendMessage(m.from, {
-        image: { url: icon }, caption: tiny(gotApp),
+        image: { url: icon },
+        caption: tiny(gotApp),
         document: { url: dllink, mimetype: 'application/vnd.android.package-archive', fileName: `${name}.apk` }
       });
     } catch (error) {
       console.error('Error:', error);
+      await connect('❌');
       return m.reply('An unexpected error occurred, sorry...');
     }
   },
-};        
+};
