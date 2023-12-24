ddcdf069927a726a8d2241acccc60fd8649b111e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-24 02:15:31 +0200

    vorterx

--- plugins/Search/_dare.js
+++ plugins/Search/_dare.js
@@ -4,14 +4,15 @@
 */
 
 const { getDare } = require('../../lib/getTruth_Dare.js');
+const config = require('../../config.js');
 
 module.exports = {
   name: 'dare',
-  category: 'Funn',
+  category: 'Fun',
   async client(vorterx, m, { args, connect }) {
    
     await connect('🤡');
     const dare = getDare();
-    vorterx.sendMessage(m.from, { text:` *Dare*: ${dare}`});
+    vorterx.sendMessage(m.from, { text:` *Dare*: ${dare}\n\n*${config.CAPTION}*`});
   },
 };
