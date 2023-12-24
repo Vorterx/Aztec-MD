e5f9e4f80cd96c4d65047eb9e18a263928cb6aa8
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-24 19:15:04 +0200

    vorterx

--- plugins/Downloads/_apkDL.js
+++ plugins/Downloads/_apkDL.js
@@ -1,42 +1,40 @@
 const { search, download } = require('aptoide-scraper');
 const { tiny } = require('@viper-x/fancytext');
 
-
 module.exports = {
   name: 'getpack',
   alias: ['apk', 'app'],
   category: 'Downloads',
   async client(vorterx, m, { args, connect }) {
-
     if (!args) {
       await connect('❌');
-      return m.reply('Please provide an app name e.g apk Acode Editor...');
+      return m.reply('Please provide an app name, e.g., apk Acode Editor...');
     }
 
     try {
       const results = await search(args);
 
-      if (!results.ok) {
-        return m.reply('An error occurred, sorry');
+      if (!results.ok || results.length === 0) {
+        return m.reply('No results found for the given app name.');
       }
 
-      const { icon, name, size, package, lastup} = results;
+      const { icon, name, size, package, lastup } = results[0];
 
       const getSize = size > 907 ? 'This app is too large to download...' : '';
 
       let gotApp = `*『 APPLICATION DOWNLOADER 』*\n\n`;
-      gotApp += `*🛡️ App NaMe*: *${name}*\n`;
+      gotApp += `*🛡️ App Name*: *${name}*\n`;
       gotApp += `*📤 Size*: *${size}\n*`;
       gotApp += `*📦 App Id*: *${package}*\n`;
       gotApp += `*⬆️ Updated*: *${lastup}*\n`;
 
       if (getSize) {
         await connect('❌');
-         return m.reply(getSize);
+        return m.reply(getSize);
       }
 
       await connect('📤');
-      const getApp = await download(results);
+      const getApp = await download(results[0]);
       const { dllink } = getApp;
 
       vorterx.sendMessage(m.from, {
@@ -45,7 +43,7 @@ module.exports = {
       });
     } catch (error) {
       console.error('Error:', error);
-      return m.reply('An unexpected error occurred sorry...');
+      return m.reply('An unexpected error occurred, sorry...');
     }
   },
-};  
+};        
