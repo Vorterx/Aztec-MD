abeea9f4bc02f2e4ead6e3cd64986d59ee6d3a33
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-25 23:03:56 +0200

    vorterx

--- plugins/Downloads/_audio.js
+++ plugins/Downloads/_audio.js
@@ -1,5 +1,7 @@
 const yts = require('yt-search');
 const ytdl = require('ytdl-core');
+const fs = require('fs').promises;
+const path = require('path');
 
 module.exports = {
   name: 'song',
@@ -12,6 +14,7 @@ module.exports = {
     }
 
     try {
+      await connect('✔️');
       m.reply('🔄 Searching for the song...');
 
       const searchResults = await yts(args);
@@ -23,15 +26,29 @@ module.exports = {
       }
 
       const videoURL = firstResult.url;
+      const videoInfo = await ytdl.getInfo(videoURL);
+
       const audioStream = ytdl(videoURL, { quality: 'highestaudio' });
 
-       vorterx.sendMessage(m.from, { audio: audioStream }, { quoted: m });
+       const fileName = `${videoInfo.videoDetails.title}.mp3`;
+       const filePath = path.join(__dirname, 'downloads', fileName);
 
+      const fileExists = await fs.access(filePath).then(() => true).catch(() => false);
+
+      if (fileExists) {
+        vorterx.sendMessage(m.from, { audio: audioStream }, { quoted: m });
+      } else {
+        audioStream.pipe(fs.createWriteStream(filePath));
+
+        audioStream.on('end', async () => {
+          await vorterx.sendMessage(m.from, { audio: filePath }, { quoted: m });
+          fs.unlink(filePath);
+        });
+      }
     } catch (error) {
       console.error('Error in song download:', error);
-      
       return m.reply('An error occurred while fetching the song.');
     }
   }
 };
-    
+        
