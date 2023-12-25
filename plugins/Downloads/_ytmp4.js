0f3a1234307b6139e9c25f1f20ddc9c7bfcca298
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-25 11:47:19 +0200

    vorterx

--- plugins/Downloads/_ytmp4.js
+++ plugins/Downloads/_ytmp4.js
@@ -1,5 +1,3 @@
-const config = require('../../config.js');
-const { tiny } = require('@viper-x/fancytext');
 const fetch = async (url) => import('node-fetch').then(module => module.default(url));
 
 module.exports = {
@@ -7,70 +5,22 @@ module.exports = {
   alias: ['vid', 'mp4'],
   category: 'Downloads',
   async client(vorterx, m, { args, connect }) {
-
-    if (!args[0]) {
+    if (!args) {
       await connect('❌');
       return m.reply('Please provide a YouTube link for me to download');
     }
 
-    try {
-      const v_api = `https://vihangayt.me/download/ytmp4?url=${encodeURIComponent(args[0])}`;
-      const res = await fetch(v_api);
+    const getMP4 = `https://vihangayt.me/download/ytmp4?url=${encodeURIComponent(args)}`;
+    const response = await fetch(getMP4);
 
-      if (!res.ok) {
-        await connect('❌');
-        return m.reply(`\`\`\`Error while downloading the video...\`\`\``);
-      }
-
-      const vid = await res.json();
-
-      console.log('API Response:', vid);
-
-      // Check if the YouTube URL is incorrect
-      if (!vid.status) {
-        await connect('❌');
-        return m.reply(`\`\`\`Error: ${vid.err}\`\`\``);
-      }
-
-      await connect('📤');
-      m.reply(`\`\`\`Downloading your video, please wait...⏳\`\`\``);
-      const v_qualty = ['vid_360p', 'vid_720p'];
-      let videoURL = '';
-      let i = 0;
-
-      while (i < v_qualty.length && !videoURL) {
-        const quality = v_qualty[i];
-        if (vid.data && vid.data[quality]) {
-          videoURL = vid.data[quality];
-        }
-        i++;
-      }
-
-      console.log('Selected Video URL:', videoURL);
-
-      if (!videoURL) {
-        await connect('❌');
-        return m.reply('Failed to obtain a valid video URL.');
-      }
-
-      const selectedQuality = v_qualty[i - 1] || 'Unknown Quality';
-      const videoTitle = vid.data && vid.data.title ? vid.data.title : 'Unknown Title';
-
-      // Check if videoURL is a valid URL before sending
-      try {
-        new URL(videoURL);
-      } catch (urlError) {
-        console.error('Invalid URL:', urlError);
-        await connect('❌');
-        return m.reply('Failed to obtain a valid video URL.');
-      }
-
-      await vorterx.sendMessage(m.from, { video: videoURL, caption: tiny(`*Title*: ${videoTitle}\n*Quality*: ${selectedQuality}\n\n*${config.CAPTION}*`) });
-    } catch (error) {
-      console.error('Error:', error);
+    if (response.ok) {
+      const videoBuffer = await response.buffer();
+      await connect('✅');
+     await vorterx.sendMessage(m.from, videoBuffer, 'video', { caption: 'Downloaded video' });
+    } else {
       await connect('❌');
-      return m.reply('An unexpected error occurred, sorry...');
+      return m.reply('Failed to download the video. Please check the URL and try again.');
     }
-  }
+  },
 };
-        
+      
