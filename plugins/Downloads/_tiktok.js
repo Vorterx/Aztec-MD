3485e42dbfb10a3b0b65a1cdcd91bc3fd29b3b0a
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-26 16:04:13 +0200

    vorterx

--- plugins/Downloads/_tiktok.js
+++ plugins/Downloads/_tiktok.js
@@ -8,7 +8,6 @@ module.exports = {
   category: 'Downloads',
   description: 'To download TikTok videos',
   async client(vorterx, m, { args, connect }) {
-   
     if (!args) {
       await connect('❌');
       return m.reply('Provide a valid TikTok video URL...');
@@ -16,13 +15,13 @@ module.exports = {
 
     await connect('📥');
     m.reply(`\`\`\`Downloading TikTok video, please wait...⏳\`\`\``);
-   const result = await ttdl(args);
+    const result = await ttdl(args);
 
     if (result && result.video && result.video.length > 0) {
       const get_vid = result.video[0];
       const caption = `*Title:* ${result.title}\n*Size:* ${result.size}\n*Audio:* ${result.title_audio}\n\n*${config.CAPTION}*`;
 
-      await vorterx.sendMessage(m.from, { video: { url: get_vid }, tiny(caption) });
+      await vorterx.sendMessage(m.from, { video: { url: get_vid }, caption: tiny(caption) });
     } else {
       await connect('❌');
       return m.reply('Failed to download the TikTok video...');
