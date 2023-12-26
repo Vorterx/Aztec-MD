9f30c4ccaeefe2cd203984e85fd0632138c445f8
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-26 12:27:10 +0200

    vorterx

--- plugins/Downloads/_tiktok.js
+++ plugins/Downloads/_tiktok.js
@@ -1,4 +1,9 @@
-const fetch = require('node-fetch');
+/*
+* @Author: DiegosonTech
+* @BotName: Aztec-MD
+*/
+
+const fetch = async (url) => (await import('node-fetch')).default(url);
 
 module.exports = {
   name: 'tiktok',
