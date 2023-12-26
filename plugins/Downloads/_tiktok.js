996f9759631c9dd066ff593ae4fccd377cac6262
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-26 12:36:51 +0200

    vorterx

--- plugins/Downloads/_tiktok.js
+++ plugins/Downloads/_tiktok.js
@@ -13,23 +13,22 @@ module.exports = {
     const tikLink = `https://api.caliph.biz.id/api/tiktok?url=${args}&apikey=lykoUzNh`;
 
     try {
-      const response = await axios.get(tikLink);
-      const data = response.data;
+  const response = await axios.get(tikLink);
+  const data = response.data;
 
-      const getVid = data.download_link;
+  const getVid = data.download_link;
 
-      if (getVid) {
-        await vorterx.sendMessage(m.from, { video: { url: getVid } });
-        return m.reply('Video sent!');
-      } else {
-        await connect('❌');
-        return m.reply('Unable to fetch download link. Please check the TikTok URL.');
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
-      await connect('❌');
-      return m.reply('An error occurred while processing the request.');
-    }
+  if (getVid) {
+    await vorterx.sendMessage(m.from, { video: { url: getVid } });
+    return m.reply('Video sent!');
+  } else {
+    await connect('❌');
+    return m.reply('Unable to fetch download link. Please check the TikTok URL.');
   }
-};
-    
+} catch (error) {
+  console.error('Error:', error.message);
+  console.error('API Response:', error.response.data);
+  await connect('❌');
+  return m.reply('An error occurred while processing the request.');
+                              }
+                   
