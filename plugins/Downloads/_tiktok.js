aadeb97fe0b989ab1d96f0c4085c5dac181797e8
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-26 15:57:03 +0200

    vorterx

--- plugins/Downloads/_tiktok.js
+++ plugins/Downloads/_tiktok.js
@@ -1,43 +1,31 @@
 const { ttdl } = require('btch-downloader');
+const { tiny } = require('@viper-x/fancytext');
+const config = require('../../config.js');
 
 module.exports = {
   name: 'tiktok',
   alias: ['tik', 'tiktokdl', 'ttdl'],
+  category: 'Downloads',
   description: 'To download TikTok videos',
   async client(vorterx, m, { args, connect }) {
-    try {
-      if (!args) {
-        await connect('❌');
-        return m.reply('Provide a valid TikTok video URL...');
-      }
-
-      await connect('📥');
-      m.reply(`\`\`\`Downloading TikTok video, please wait...⏳\`\`\``);
-
-      // Use the TikTok downloader library
-      const result = await ttdl(args);
-
-      if (result && result.video && result.video.length > 0) {
-        const videoUrl = result.video[0];
-
-        // You can customize the caption using the retrieved information
-        const caption = `
-╭──*『 TIKTOK DOWNLOAD 』*
-│ *Title:* ${result.title}
-│ *Creator:* ${result.creator}
-│ *Audio:* ${result.title_audio}
-╰───────────────────༓\n\n`;
-
-        await vorterx.sendMessage(m.from, { video: { url: videoUrl }, caption });
-      } else {
-        await connect('❌');
-        return m.reply('Failed to download the TikTok video. Please check the URL.');
-      }
-    } catch (error) {
-      console.error(error);
+   
+    if (!args) {
       await connect('❌');
-      return m.reply('An unexpected error occurred while processing the request.');
+      return m.reply('Provide a valid TikTok video URL...');
+    }
+
+    await connect('📥');
+    m.reply(`\`\`\`Downloading TikTok video, please wait...⏳\`\`\``);
+   const result = await ttdl(args);
+
+    if (result && result.video && result.video.length > 0) {
+      const get_vid = result.video[0];
+      const caption = `*Title:* ${result.title}\n*Size:* ${result.size}\n*Audio:* ${result.title_audio}\n\n*${config.CAPTION}*`;
+
+      await vorterx.sendMessage(m.from, { video: { url: get_vid }, tiny(caption) });
+    } else {
+      await connect('❌');
+      return m.reply('Failed to download the TikTok video...');
     }
   }
 };
-          
