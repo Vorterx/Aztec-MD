2528fdd8a8ecce3ac43acbd3ae3847b615dadabe
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-27 17:12:39 +0200

    vorterx

--- plugins/GPT AI/_gmini.js
+++ plugins/GPT AI/_gmini.js
@@ -1,35 +1,37 @@
+
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const apiKey = "AIzaSyAlvaQ_Jv86iNnQlcyHYH0S3XXoqBw0HKs";
+const apiKey = "AIzaSyBPv4OM4uBGbSJcX5tEb6SobkNc6qwMdec";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 module.exports = {
   name: 'gemini',
   category: 'GPT AI',
   async client(vorterx, m, { args, quoted, connect }) {
-    if (!quoted) {
-      await connect('❌');
-      return m.reply('Please Reply to an image or video...');
-    }
-
+    if (!quoted) return m.reply(`Where is the picture?`);
+    if (!/image/.test(mime)) return m.reply(`Send/Reply Photo`);
+    
     try {
-      const media = await quoted.download();
-      const mediaType = quoted.mimetype || '';
-
-      if (mediaType.includes('image') || mediaType.includes('video')) {
+        const prompt = `${args}`;
         const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-        const result = await model.generateContent([`${args}`, { inlineData: { data: Buffer.from(media).toString("base64"), mimeType: mediaType } }]);
-        const getGeni = (await result.response).args();
+        const media = await quoted.download();
 
-        await vorterx.sendMessage(m.from, getGeni, { quoted: m });
-      } else {
-        await connect('❌');
-        m.reply('Reply to the video or an image...');
-      }
+        const imagePart = {
+            inlineData: {
+                data: Buffer.from(media).toString("base64"),
+                mimeType: mime
+            },
+        };
+
+        const result = await model.generateContent([prompt, imagePart]);
+        const response = await result.response;
+        const textt = response.args(); 
+
+     
+        m.reply(`${textt}`);
     } catch (error) {
-      console.error(error);
-      await connect('❌');
-      m.reply(`${error.message}`);
+        console.error('Error in Gemini Pro Vision:', error);
+        m.reply(`An error occurred: ${error.message}`);
     }
   },
 };
-        
+          
