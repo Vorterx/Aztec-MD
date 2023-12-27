36e0113ca0072989301eaa7939411b26fe59d7d8
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-27 17:25:25 +0200

    vorterx

--- plugins/GPT AI/_gmini.js
+++ plugins/GPT AI/_gmini.js
@@ -7,31 +7,35 @@ module.exports = {
   name: 'gemini',
   category: 'GPT AI',
   async client(vorterx, m, { args, quoted, connect }) {
-    if (!quoted) return m.reply(`Where is the picture?`);
-    if (!/image/.test(mime)) return m.reply(`Send/Reply Photo`);
+    if (!quoted) {
+      return m.reply(`Where is the picture?`);
+    }
+    
+    if (!/image/.test(mime)) {
+      return m.reply(`Send/Reply Photo`);
+    }
     
     try {
-        const prompt = `${args}`;
-        const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-        const media = await quoted.download();
+      const prompt = `${args}`;
+      const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+      const media = await quoted.download();
 
-        const imagePart = {
-            inlineData: {
-                data: Buffer.from(media).toString("base64"),
-                mimeType: mime
-            },
-        };
+      const imagePart = {
+        inlineData: {
+          data: Buffer.from(media).toString("base64"),
+          mimeType: mime
+        },
+      };
 
-        const result = await model.generateContent([prompt, imagePart]);
-        const response = await result.response;
-        const textt = response.args(); 
+      const result = await model.generateContent([prompt, imagePart]);
+      const response = await result.response;
+      const textt = response.args(); 
 
-     
-        m.reply(`${textt}`);
+      m.reply(`${textt}`);
     } catch (error) {
-        console.error('Error in Gemini Pro Vision:', error);
-        m.reply(`An error occurred: ${error.message}`);
+      console.error('Error in Gemini Pro Vision:', error);
+      m.reply(`An error occurred: ${error.message}`);
     }
   },
 };
-          
+        
