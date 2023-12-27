8e4f8c71ff04eb8b25bf400281fa3ef7d9464b7d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-27 17:38:07 +0200

    Update _gmini.js

--- plugins/GPT AI/_gmini.js
+++ plugins/GPT AI/_gmini.js
@@ -1,4 +1,5 @@
-
+        
+//
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const apiKey = "AIzaSyBPv4OM4uBGbSJcX5tEb6SobkNc6qwMdec";
 const genAI = new GoogleGenerativeAI(apiKey);
@@ -6,15 +7,15 @@ const genAI = new GoogleGenerativeAI(apiKey);
 module.exports = {
   name: 'gemini',
   category: 'GPT AI',
-  async client(vorterx, m, { args, quoted, mime,connect }) {
+  async client(vorterx, m, { args, quoted, mime, connect }) {
     if (!quoted) {
       return m.reply(`Where is the picture?`);
     }
-    
+
     if (!/image/.test(mime)) {
       return m.reply(`Send/Reply Photo`);
     }
-    
+
     try {
       const prompt = `${args}`;
       const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
@@ -28,14 +29,19 @@ module.exports = {
       };
 
       const result = await model.generateContent([prompt, imagePart]);
-      const response = await result.response;
-      const textt = response.args(); 
+      const rawResponse = await result.response.text();
 
-      m.reply(`${textt}`);
+      try {
+        const response = JSON.parse(rawResponse);
+        const textt = response.args();
+        m.reply(`${textt}`);
+      } catch (jsonError) {
+        console.error('Error parsing JSON:', jsonError);
+        m.reply('An error occurred while processing the response.');
+      }
     } catch (error) {
       console.error('Error in Gemini Pro Vision:', error);
       m.reply(`An error occurred: ${error.message}`);
     }
   },
 };
-        
