a76541df43e15388ec50098862fe788f63fafced
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-27 17:57:55 +0200

    vorterx

--- plugins/GPT AI/_gmini.js
+++ plugins/GPT AI/_gmini.js
@@ -1,47 +1,40 @@
-        
-//
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const apiKey = "AIzaSyBPv4OM4uBGbSJcX5tEb6SobkNc6qwMdec";
+const apiKey = "AIzaSyAlvaQ_Jv86iNnQlcyHYH0S3XXoqBw0HKs";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 module.exports = {
   name: 'gemini',
   category: 'GPT AI',
   async client(vorterx, m, { args, quoted, mime, connect }) {
-    if (!quoted) {
-      return m.reply(`Where is the picture?`);
-    }
-
-    if (!/image/.test(mime)) {
-      return m.reply(`Send/Reply Photo`);
-    }
+    if (!quoted) return m.reply(`Where is the picture?`);
+    if (!/image/.test(mime)) return m.reply(`Send/Reply Photos`);
 
     try {
-      const prompt = `${args}`;
-      const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-      const media = await quoted.download();
+        const prompt = `${args}`;
+        const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+        const media = await quoted.download();
 
-      const imagePart = {
-        inlineData: {
-          data: Buffer.from(media).toString("base64"),
-          mimeType: mime
-        },
-      };
+        const imagePart = {
+            inlineData: {
+                data: Buffer.from(media).toString("base64"),
+                mimeType: mime
+            },
+        };
 
-      const result = await model.generateContent([prompt, imagePart]);
-      const rawResponse = await result.response.text();
+        const result = await model.generateContent([prompt, imagePart]);
+        const response = await result.response;
 
-      try {
-        const response = JSON.parse(rawResponse);
-        const textt = response.args();
-        m.reply(`${textt}`);
-      } catch (jsonError) {
-        console.error('Error parsing JSON:', jsonError);
-        m.reply('An error occurred while processing the response.');
-      }
+        console.log('API Response:', response);
+
+        const textt = await response.args();
+
+                m.reply(`${textt}`);
     } catch (error) {
-      console.error('Error in Gemini Pro Vision:', error);
-      m.reply(`An error occurred: ${error.message}`);
+        console.error('Error in Gemini Pro Vision:', error);
+
+                console.error(error);
+
+        m.reply(`An error occurred: ${error.message}`);
     }
   },
 };
