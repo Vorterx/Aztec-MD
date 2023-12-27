2b98043fd6530ec565b3e8165c3529f02aab9fec
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-27 15:54:31 +0200

    vorterx

--- plugins/Search/_lyrics.js
+++ plugins/Search/_lyrics.js
@@ -1,17 +1,19 @@
-const axios = require('axios');
 const config = require('../../config.js');
+const axios = require('axios');
+const { Buffer } = require('buffer');
 
 module.exports = {
   name: 'lyrics',
   category: 'Search',
   async client(vorterx, m, { args, connect }) {
+  
     try {
       if (!args || typeof args !== 'string') {
         await connect('❌');
         return m.reply(`Please provide a song name or artist. For example, "Dior by Pop Smoke"`);
       }
 
-      const search = encodeURIComponent(args.trim());
+      const search = encodeURIComponent(text.trim());
       const { data } = await axios.get(`https://weeb-api.vercel.app/genius?query=${search}`);
 
       if (!data || data.length === 0) {
@@ -23,21 +25,31 @@ module.exports = {
 
       const title = data[0].title;
       const artist = data[0].artist;
-      const thumbnail = data[0].genius;
       const lyricsRes = await axios.get(`https://weeb-api.vercel.app/lyrics?url=${data[0].url}`);
       const lyrics = lyricsRes.data || 'Lyrics not found.';
+      const thumbnail = data[0].url;
+      const img = thumbnail ? Buffer.from(thumbnail).toString('base64') : '';
 
-      await vorterx.sendMessage(m.from, {
-  image: { url: thumbnail },
-  caption: `*🌷TITLE*: ${title}\n*👤ARTIST*: ${artist}\n\n${lyrics}\n\n*${config.CAPTION}*`
-});
+      const res = `*🌷TITLE*: ${title}\n\n*👤ARTIST*: ${artist}\n\n${lyrics}\n\n*${config.CAPTION}*`;
 
-      await connect('🤖');
+      const msgData = {
+        text: res,
+        contextInfo: {
+          externalAdReply: {
+            title: '',
+            body: res,
+            mediaType: 2,
+            mediaUrl: img,
+            thumbnail: img
+          }
+        },
+        data: [0, ...(data[0].data || [])].map(JSON.stringify)
+      };
+
+      return vorterx.sendMessage(m.from, msgData);
     } catch (error) {
       console.error(error);
-      await connect('❌');
-      return m.reply('An error occurred while fetching the lyrics.');
+      return m.reply('An error occurred while processing...');
     }
   }
 };
-        
