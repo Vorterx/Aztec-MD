70525d186cf49620cb0ebb108ae06f56fab2b1c0
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-28 19:32:51 +0200

    Update _mods.js

--- plugins/Mics/_mods.js
+++ plugins/Mics/_mods.js
@@ -3,7 +3,7 @@
 * @BotName: Aztec-MD 
 */
 
-const { fancytext } = require('@viper-x/fancytext'); // Changed 'tiny' to 'fancytext'
+const { fancytext } = require('@viper-x/fancytext');
 const config = require('../../config.js');
 
 module.exports = {
@@ -12,20 +12,23 @@ module.exports = {
   description: 'To check mods admins to the bot',
   category: 'Owner',
   async client(vorterx, m, { text, args, quoted, connect }) {
-   await connect ('✔️');
-    
+    await connect('✔️');
+
     let mods = process.env.MODS ? process.env.MODS.split(',').map(mod => mod.replace('@net.whatsapp', '').trim()) : [];
     let me = mods.map((mod, index) => `  ${index + 1} 〄 @${mod}\n\n`).join('');
     let mention = [m.sender, ...mods.map(mod => `${mod}@s.whatsapp.net`)];
-    if (!me || !mods || !mods[0]) return await m.reply("*There's no mods added yet...*");
-    
+
+    if (!me || !mods || !mods[0]) return await vorterx.sendMessage(m.from, "*There's no mods added yet...*", 'text', { quoted: m });
+
     let str = `
 👤 *${config.botName.toUpperCase().trim()} MODS* 👤\n\n${me}`.trim();
 
-return await m.reply({
-  url: "https://telegra.ph/file/5fd51597b0270b8cff15b.png",
-  caption: str,
-  mentions: mention
-}, "image", m);
-    }
-}
+    await vorterx.sendMessage(m.from, {
+      image: { url: "https://telegra.ph/file/5fd51597b0270b8cff15b.png" },
+      caption: str,
+      mentions: mention,
+      quoted: m
+    });
+  }
+};
+      
