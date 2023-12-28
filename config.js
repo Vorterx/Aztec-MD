5b84286e1b214a84673eb2bb219db9eb9392ef4d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-28 14:49:21 +0200

    vorterx

--- config.js
+++ config.js
@@ -20,6 +20,7 @@ let config = {
   LOGOS: [
     'https://i.ibb.co/frX9YvD/OIG.jpg',
     'https://i.ibb.co/grM9VLh/091e4657090fdaa14cb3fb9f69cfa7e6.jpg',
+    'https://i.imgur.com/hpH9wbL.jpg',
   ],
 };
 
