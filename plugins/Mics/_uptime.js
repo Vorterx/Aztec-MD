5b873a518d1f286432cd3a593bcc2462c51cdf46
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 22:15:02 +0200

    _vorterx

---
+++ plugins/Mics/_uptime.js
@@ -0,0 +1,20 @@
+Zenith(
+  {
+  usage: 'uptime',
+  category: 'Mics',
+  filename: __filename
+}, async (vorterx, m, react) => {
+
+  await react('⏲️');
+  const uptime = process.uptime();
+  const uptimeString = formatUptime(uptime);
+  coax.reply(`*Uptime*: ${uptimeString}`);
+});
+
+function formatUptime(seconds) {
+  const days = Math.floor(seconds / 86400);
+  const hours = Math.floor((seconds % 86400) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const formattedUptime = `${days}d ${hours}h ${minutes}m`;
+  return formattedUptime;
+ }
