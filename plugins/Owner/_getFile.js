515bf679db9d7aafcc60aab14df8e63423dbc62d
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-30 01:47:24 +0200

    vorterx

--- plugins/Owner/_getFile.js
+++ plugins/Owner/_getFile.js
@@ -1,30 +1,35 @@
-const { Zenith } = require ('../../lib/_cmd_sxntax.js');
+const { Zenith } = require('../../lib/_cmd_sxntax.js');
 
 Zenith(
   {
     usage: "file",
-    desc: "to get extact file",
+    desc: "to get exact file",
     category: "Owner",
     filename: __filename
-}, async(vorterx, coax, args, text, isDev) => {
- const { commands } = require('../../lib/_cmd_sxntax.js');
-if(!isDev) {
-  await react('❌');
-  return coax.reply('__Sorry this command is for my owner__');
-}
-    let aztec = [];
-        const cmd = commands.find((cmd) => cmd.usage === (text.split(" ")[0].toLowerCase()))
-        if (!cmd) {
-          await react('❌')
-            return await coax.reply("_No kinda file available sorry_");
-        }
-      await react('📇');
-        else aztec.push(`*📍Usage:* ${cmd.usage}`);
-       
-    if (cmd.category) aztec.push(`*📇Category:* ${cmd.category}`);
-      
-    if(cmd.filename) aztec.push(`📤FileName: ${cmd.filename}`)
-        
-    return coax.reply(aztec.join('\n'));
+  },
+  async (vorterx, coax, args, text, isDev) => {
+    const { commands } = require('../../lib/_cmd_sxntax.js');
 
-})
+    if (!isDev) {
+      await react('❌');
+      return coax.reply('__Sorry, this command is for my owner__');
+    }
+
+    let aztec = [];
+    const cmd = commands.find((cmd) => cmd.usage === text.split(" ")[0].toLowerCase());
+
+    if (!cmd) {
+      await react('❌');
+      return await coax.reply("_No kind of file available, sorry_");
+    }
+
+    await react('📇');
+    aztec.push(`*📍Usage:* ${cmd.usage}`);
+
+    if (cmd.category) aztec.push(`*📇Category:* ${cmd.category}`);
+
+    if (cmd.filename) aztec.push(`📤FileName: ${cmd.filename}`);
+
+    return coax.reply(aztec.join('\n'));
+  }
+);      
