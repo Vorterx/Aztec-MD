49193938c4b31cecd5959e7ce8e7fd6e88f06f1e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 18:33:46 +0200

    vorterx::

--- plugins/Mics/_translate.js
+++ plugins/Mics/_translate.js
@@ -6,40 +6,42 @@
 const { translate } = require('@vitalets/google-translate-api');
 const config = require('../../config.js');
 
-module.exports = {
-  name: 'tr',
+Zenith(
+  {
+  usage: 'tr',
   alias: ['translate','trt'],
-  description: 'Translate any usable language',
+  desc: 'Translate any usable language',
   category: 'Mics',
-  async client(vorterx, m, {  args, connect }) {
+  filename: __filename
+  }, async (vorterx, coax, args, react) => {
     
     if (!args) {
-      await connect('❌');
-      return m.reply('Example: tr zu Damn l love this Diegoson eish');
+      await react('❌');
+      return coax.reply('Example: tr zu Damn l love this Diegoson eish');
     }
 
     let lang;
     let data;
 
-    if (args && m.quoted && m.quoted.args) {
+    if (args && coax.quoted && coax.quoted.args) {
       lang = args.slice(0, 2);
-      data = m.quoted.args;
+      data = coax.quoted.args;
     } else if (args) {
       lang = args.slice(0, 2);
       data = args.substring(2).trim();
     }
 
     try {
-      await connect('📝');
+      await react('📝');
       const result = await translate(data, { to: lang });
       const caption = `*📝TEXT*: ${data}\n\n*🧘TRANSLATED*: ${result.text}\n\n*${config.CAPTION}*`;
-      await m.reply(caption);
+      await coax.reply(caption);
     } catch (error) {
       let errorMessage = 'An error occurred while translating';
       if (error.code === 'NOT_SUPPORTED') {
         errorMessage = 'The language is not supported';
       }
-      await m.reply(errorMessage);
+      await coax.reply(errorMessage);
     }
   },
 };
