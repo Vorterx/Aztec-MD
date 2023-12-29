33574df02d99a6b7cf3ff71ee52ae76ae8519923
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 21:50:47 +0200

    vorterx

---
+++ plugins/Owner/_getFile.js
@@ -0,0 +1,28 @@
+Zenith(
+  {
+    usage: "file",
+    desc: "to get extact file",
+    category: "Owner",
+    filename: __filename
+}, async(vorterx, coax, args, text, isDev) => {
+ const { commands } = require('../../lib/_cmd_sxntax.js');
+if(!isDev) {
+  await react('❌');
+  return coax.reply('__Sorry this command is for my owner__');
+}
+    let aztec = [];
+        const cmd = commands.find((cmd) => cmd.usage === (text.split(" ")[0].toLowerCase()))
+        if (!cmd) {
+          await react('❌')
+            return await coax.reply("_No kinda file available sorry_");
+        }
+      await react('📇');
+        else aztec.push(`*📍Usage:* ${cmd.usage}`);
+       
+    if (cmd.category) aztec.push(`*📇Category:* ${cmd.category}`);
+      
+    if(cmd.filename) aztec.push(`📤FileName: ${cmd.filename}`)
+        
+    return coax.reply(aztec.join('\n'));
+
+})
