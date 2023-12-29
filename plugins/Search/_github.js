709309c651741ebafbc4a1d89e508e40f9b0731c
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 20:28:20 +0200

    vorterx

--- plugins/Search/_github.js
+++ plugins/Search/_github.js
@@ -1,20 +1,22 @@
 const axios = require('axios');
 const config = require('../../config.js');
 
-module.exports = {
-  name: 'github',
+Zenith(
+  {
+  usage: 'github',
   alias: ['git'],
   category: 'Search',
-  async client(vorterx, m, { args, connect }) {
+  filename: __filename
+  }, async (vorterx, coax, args, react) => {
+   
     if (!args) {
-      await connect('❌');
-      return m.reply(`*Please provide a git user name e.g github DiegosonTech*`);
+      await react('❌');
+      return coax.reply(`*Please provide a git user name e.g github DiegosonTech*`);
     }
-
-    await connect('📊');
+    await react('📊');
     try {
-      const response = await axios.get(`https://api.github.com/users/${args}`);
-      const { login, name, bio, followers, public_repos, following, blog, avatar_url } = response.data;
+      const res = await axios.get(`https://api.github.com/users/${args}`);
+      const { login, name, bio, followers, public_repos, following, blog, avatar_url } = res.data;
 
       const rezText8 = `
 *乂 USER GITHUB INFO 乂*
@@ -31,10 +33,9 @@ module.exports = {
       const repoNames = userRepos.data.map(repo => repo.name);
       const repoList = repoNames.join('\n');
 
-      vorterx.sendMessage(m.from, { image: { url: avatar_url, mimetype: 'image/jpeg' }, caption: rezText8 + '\n\n*📚 Repositories:*\n' + repoList }, { quoted: m });
+      vorterx.sendMessage(coax.from, { image: { url: avatar_url, mimetype: 'image/jpeg' }, caption: rezText8 + '\n\n*📚 Repositories:*\n' + repoList }, { quoted: coax });
     } catch (error) {
       console.error(error);
-      m.reply('An error occurred');
+      coax.reply('An error occurred');
     }
-  }
-};
+  });
