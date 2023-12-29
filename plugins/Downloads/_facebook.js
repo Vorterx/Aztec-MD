787535a1c479c8d51f46e7a3e91ba26d43ef8350
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 19:47:43 +0200

    vorterx

--- plugins/Downloads/_facebook.js
+++ plugins/Downloads/_facebook.js
@@ -2,38 +2,37 @@ const bocil = require('@bochilteam/scraper');
 const { tiny } = require('@viper-x/fancytext');
 const config = require('../../config.js');
 
-module.exports = {
-    name: "fb",
-    description: "To download Facebook",
+Zenith(
+    {
+    usage: "fb",
+    desc: "To download Facebook",
     category: "Downloads",
-    async client(vorterx, m, {
-        connect, args
-    }) {
+    filename: __filename
+    }, async (vorterx, coax, react, args) => {
        
         try {
             if (!args) {
-                await connect("❌");
-                return m.reply(`*Please Provide a Valid Facebook Video Link*`);
+                await react("❌");
+                return coax.reply(`*Please Provide a Valid Facebook Video Link*`);
             } else {
-                await connect("📤");
+                await react("📤");
                 bocil.facebookdlv2(`${args}`).then(async (data) => {
 
                     const { filesizeH, quality } = data.result[0];
-                    let caption = `*Size* : ${filesizeH}\n*Quality* : ${quality}\n\n*${config.CAPTION}*`;
-                    vorterx.sendMessage(m.from, {
+                    let caption = `*Quality* : ${quality}\n\n*${config.CAPTION}*`;
+                    vorterx.sendMessage(coax.from, {
                         video: {
                             url: data.result[0].url
                         },
                         caption: tiny(caption)
                     }, {
-                        quoted: m
+                        quoted: coax
                     });
                 });
             }
         } catch (error) {
-            vorterx.sendMessage(m.from, {
+            vorterx.sendMessage(coax.from, {
                 text: "Error occurred while processing"
             });
         }
-    }
-};
+    });
