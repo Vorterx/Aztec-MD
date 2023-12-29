8cb8901edf21b04ab40f233dd9e29bcb7ebc8f5a
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 19:53:22 +0200

    vorterx

--- plugins/Downloads/_song.js
+++ plugins/Downloads/_song.js
@@ -2,30 +2,24 @@ const ytdl = require('ytdl-core');
 const fs = require('fs');
 const path = require('path');
 const search = require('yt-search');
-//const { Zenith } = require('../../lib/client.js');
 
-/*Zenith({
-  usage: `${prefix}song`,
-  alias: ['audio', 'mp3'],
+Zenith(
+  {
+  usage: 'song',
   category: 'Downloads',
-  desc: 'To download music',
-  isOwner: false,
-}).client = async (vorterx, m, { args, connect }) => {
-*/
-module.exports = {
-  name: 'song',
-  category: 'Downloads',
-  async client(vorterx,m, { args, connect }) {
+  desc: 'To Download the song',
+  filename: __filename
+}, async (vorterx, coax, args, react) => {
   
   if (!args) {
-    await connect('❌');
-    return m.reply('Provide a song name, please...');
+    await react('❌');
+    return coax.reply('Provide a song name, please...');
   }
   const vidS = await search(args);
 
   if (!vidS || !vidS.videos || vidS.videos.length === 0) {
-    await connect('❌');
-    return m.reply('__Sorry If youre GAY you cant get the song__...');
+    await react('❌');
+    return coax.reply('__Sorry If youre GAY you cant get the song__...');
   }
 
   const videoId = vidS.videos[0].videoId;
@@ -38,22 +32,19 @@ module.exports = {
   if (!fs.existsSync(downloadsPath)) {
     fs.mkdirSync(downloadsPath);
   }
-
+   await react('✔️');
   const audio = ytdl(videoId, { quality: 'highestaudio' });
   const filePath = path.join(downloadsPath, `${videoD.videoDetails.title}.mp3`);
   const writeStream = fs.createWriteStream(filePath);
   audio.pipe(writeStream);
 
   writeStream.on('finish', () => {
-    connect('✅');
     const msg = `Downloaded: ${videoD.videoDetails.title}\nViews: ${viewCount}\nUploaded Date: ${uploaded}`;
-    vorterx.sendMessage(m.from, { text: msg, thumbnail: thumbnail, audio: fs.readFileSync(filePath), mimetype: 'audio/mp3' });
+    vorterx.sendMessage(coax.from, { text: msg, thumbnail: thumbnail, audio: fs.readFileSync(filePath), mimetype: 'audio/mp3' });
     fs.unlinkSync(filePath);
   });
 
   writeStream.on('error', (err) => {
-    m.reply(`${err.message}`);
+    coax.reply(`${err.message}`);
   });
-}
-                               }
-  
+})
