d9248e2e96ed74c7b2582e84933606d60b293dcc
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 13:08:51 +0200

    vorterx

---
+++ plugins/Convert/_attps.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'atts',
+  category: 'Convert',
+  async client(vorterx,m, { args, connect, quoted }) {
+
+     if (!args) {
+       await connect('❌');
+      return m.reply('Please provide a text e.g attp Vorterx...');
+     }
+        await connect('✔️');
+          vorterx.sendMessage(m.from, {
+                    sticker: {
+                        url: `https://api.lolhuman.xyz/api/attp?apikey=GataDios&text=${args}`}
+                }, { quoted: m });
+
+     }
+  
+}
