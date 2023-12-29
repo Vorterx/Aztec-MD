402559cdf41b60e606fd897168b5c3afcef9c745
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 19:30:51 +0200

    vorterx

--- plugins/Downloads/_tiktok.js
+++ plugins/Downloads/_tiktok.js
@@ -7,29 +7,30 @@ const { ttdl } = require('btch-downloader');
 const { tiny } = require('@viper-x/fancytext');
 const config = require('../../config.js');
 
-module.exports = {
-  name: 'tiktok',
+Zenith (
+  {
+  usage: 'tiktok',
   alias: ['tik', 'tiktokdl', 'ttdl'],
   category: 'Downloads',
-  description: 'To download TikTok videos',
-  async client(vorterx, m, { args, connect }) {
+  desc: 'To download TikTok videos',
+  filename: __filename
+}, async (vorterx, coax, args, react) => {
     if (!args) {
-      await connect('❌');
-      return m.reply('Provide a valid TikTok video URL...');
+      await react('❌');
+      return coax.reply('Provide a valid TikTok video URL...');
     }
 
-    await connect('📥');
-    m.reply(`\`\`\`Downloading TikTok video, please wait...⏳\`\`\``);
+    await react('📥');
+    coax.reply(`\`\`\`Downloading TikTok video, please wait...⏳\`\`\``);
     const result = await ttdl(args);
 
     if (result && result.video && result.video.length > 0) {
       const get_vid = result.video[0];
       const caption = `*Title:* ${result.title}\n*Size:* ${result.title_size}\n*Audio:* ${result.title_audio}\n\n*${config.CAPTION}*`;
 
-      await vorterx.sendMessage(m.from, { video: { url: get_vid }, caption: tiny(caption) });
+      await vorterx.sendMessage(coax.from, { video: { url: get_vid }, caption: tiny(caption) });
     } else {
-      await connect('❌');
-      return m.reply('Failed to download the TikTok video...');
+      await react('❌');
+      return coax.reply('Failed to download the TikTok video...');
     }
-  }
-};
+});
