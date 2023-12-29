9b26d49ea99aece10ab491c51f3e85c6a9a12bf1
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 18:53:40 +0200

    vorterx

--- plugins/GPT AI/_dalliE.js
+++ plugins/GPT AI/_dalliE.js
@@ -6,18 +6,21 @@
 const axios = require('axios');
 const config = require('../../config.js');
 
-module.exports = {
-  name: 'dalli',
+Zenith(
+  {
+  usage: 'dalli',
   alias: ['imagine'],
   category: 'GPT AI',
-  async client(vorterx, m, { args, connect }) {
+  desc: 'To Download images using dalle E',
+  filename: __filename
+  }, async (vorterx, coax, args, react) => {
     try {
         if (!args) {
-        await connect('❌');
-        return m.reply('__Provide a name___...');
+        await react('❌');
+        return coax.reply('__Provide a name___...');
       }
 
-      await connect('✔️');
+      await react('✔️');
       const search = encodeURIComponent(args);
       const get = await axios.get(`https://v2-guru-indratensei.cloud.okteto.net/scrape?query=${search}`);
       const data = get.data;
@@ -25,16 +28,15 @@ module.exports = {
       const aztec = `*Name*: ${search}\n\n*${config.CAPTION}*`;
 
       const get_img = dalle_img[Math.floor(Math.random() * dalle_img.length)];
-      await vorterx.sendMessage(m.from, {
+      await vorterx.sendMessage(coax.from, {
         image: { url: get_img },
         caption: aztec
-      }, { quoted: m });
+      }, { quoted: coax });
 
     } catch (error) {
       console.error(error.message);
-      await connect('❌');
-      m.reply('An unexpected error occurred. Please try again later...');
+      await react('❌');
+      coax.reply('An unexpected error occurred. Please try again later...');
     }
-  }
-};
+  });
     
