bccea6613c303c887825744f638b9ce42ba16f4c
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-30 01:33:31 +0200

    vorterx

--- config.js
+++ config.js
@@ -7,14 +7,11 @@
 //                     |
 //                     -------------------
 let config = {
-  botName: process.env.BOTNAME || 'AZTEC-MD',
   prefix: process.env.PREFIX || '.',
   name: process.env.OWNER_NAME || '',
-  owner_number: process.env.OWNER_NUMBER || '27686881509',
   ANTILINK: process.env.ANTILINK || 'true',
   CAPTION: process.env.CAPTION || 'ᴾᴼᵂᴱᴿᴱᴰ ᴮʸ ⱽᴼᴿᵀᴱᴿˣ⁶⁶⁵™',
-  menu: process.env.MENU || '',
-  THUMB: process.env.THUMB || 'https://imageupload.io/69vJBZbn4iPqWTZ',
+  MENU: process.env.MENU || '',
   mods: process.env.MODS ? process.env.MODS.split(',').map(mod => mod.replace('@net.whatsapp', '')) : [],  
   MONGODB: process.env.MONGODB || '', 
   LOGOS: [
