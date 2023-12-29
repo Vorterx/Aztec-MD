673461b21b2e77aa88aa116fb591037e067026ad
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-29 18:20:35 +0200

    vorterx

--- plugins/Mics/_ebinary.js
+++ plugins/Mics/_ebinary.js
@@ -5,19 +5,18 @@
 
 const { eBinary } = require('../../lib/_getBinary.js');
 
-Zenith(
-  {
+Zenith({
   usage: 'ebinary',
   category: 'Mics',
   desc: 'For Funny',
   filename: __filename
-  }, async(vorterx, coax, args, react) => {
-   
-    if (!args) {
-      await react('❌');
-      return coax.reply('Provide a text to encode in binary...');
-    }
+}, async (vorterx, coax, args, react) => {
+  if (!args) {
+    await react('❌');
+    return coax.reply('Provide a text to encode in binary...');
+  }
 
-    await react('🔄');
-    coax.reply(await eBinary(args));
-  });
+  await react('🔄');
+  coax.reply(await eBinary(args));
+});
+       
