542f23359d00b3ad72bdfa579b77e32896b2ac06
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-31 16:17:23 +0200

    vorterx

--- plugins/Downloads/_download.js
+++ plugins/Downloads/_download.js
@@ -4,6 +4,7 @@ const { tiny } = require('@viper-x/fancytext');
 const config = require('../../config.js');
 const gis = require('g-i-s');
 const { ttdl, igdl } = require('btch-downloader');
+const axios = require('axios');
 
 
 function gisPromise(args) {
@@ -131,6 +132,36 @@ Zenith(
   });
                                                            
 //---------------------------------------------------------------------------                                
+
+Zenith (
+  {
+  usage: 'tiktok',
+  alias: ['tik', 'tiktokdl', 'ttdl'],
+  category: 'Downloads',
+  desc: 'To download TikTok videos',
+  filename: __filename
+}, async (vorterx, coax, react, {args}) => {
+    if (!args) {
+      await react('❌');
+      return coax.reply('Provide a valid TikTok video URL...');
+    }
+
+    await react('📥');
+    coax.reply(`\`\`\`Downloading TikTok video, please wait...⏳\`\`\``);
+    const result = await ttdl(args);
+
+    if (result && result.video && result.video.length > 0) {
+      const get_vid = result.video[0];
+      const caption = `*Title:* ${result.title}\n*Audio:* ${result.title_audio}\n\n*${config.CAPTION}*`;
+
+      await vorterx.sendMessage(coax.from, { video: { url: get_vid }, caption: tiny(caption) });
+    } else {
+      await react('❌');
+      return coax.reply('Failed to download the TikTok video...');
+    }
+});
+
+  //---------------------------------------------------------------------------                                
 
 
 
