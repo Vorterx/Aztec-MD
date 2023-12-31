9c987f07def1035cca6a1c1d44fd177b5970d030
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-01 01:48:09 +0200

    vorterx

--- plugins/Search/_xnxxsrch.js
+++ plugins/Search/_xnxxsrch.js
@@ -1,51 +1,43 @@
 const axios = require("axios");
 const config = require('../../config.js');
-const fs = require("fs");
-const { Zenith } = require ('../../lib/_cmd_sxntax.js');
+const { Zenith } = require('../../lib/_cmd_sxntax.js');
 
-Zenith( 
-  {  
-  usage: "xnxxsh",
-  desc: "18 videos only",
-  category: "Downloads",
-  filename: __filename
-  }, async (vorterx, coax, react, {args}) => {
- 
+Zenith(
+  {
+    usage: "xnxxsh",
+    desc: "18+ videos only",
+    category: "Downloads",
+    filename: __filename,
+  },
+  async (vorterx, coax, react, { args }) => {
     if (!args) {
       await react("❌");
       return coax.reply("Please provide a search term.");
     }
+
     await react("🍑");
 
-    const res = (await axios(
-      `https://raganork-network.vercel.app/api/xvideos/search?query=${args}`
-    )).data;
-    let textt = `🔎 *XNXXV SEARCH RESULTS* 🔎\n\n🔍 Search Term: ${args}\n\n`;
-    for (const [index, video] of res.result.entries()) {
-      const videoInfo = `
+    try {
+      const res = await axios.get(`https://raganork-network.vercel.app/api/xvideos/search?query=${args}`);
+      const result = res.data.result;
+
+      let textt = `🔎 *XNXXV SEARCH RESULTS* 🔎\n\n🔍 Search Term: ${args}\n\n`;
+      
+      result.forEach((video, index) => {
+        const videoInfo = `
 📽️ *Video ${index + 1}*
 🎬 *Title: ${video.title}*
 ⏰ *Duration: ${video.duration}*
 🔗 *[Watch]*(${video.url})\n\n*${config.CAPTION}*`;
-      textt += `${videoInfo}\n\n`;
-    }
+        textt += `${videoInfo}\n\n`;
+      });
 
-    await vorterx.sendMessage(
-      coax.from,
-      {
-        caption: textt,
-        captionType: 1,
-        captionInfo: {
-          markdown: {
-            bold: [
-              [textt.indexOf("🔎 XNXXV SEARCH RESULTS 🔎"), textt.indexOf("\n\n")],
-            ],
-            italic: [
-              [textt.indexOf("🔍 Search Term"), textt.indexOf("\n\n")],
-            ],
-          },
-        },
-      },
-      { quoted: coax }
-    ); 
-  });              
+      await vorterx.sendMessage(coax.from, textt, { quoted: coax, markdown: true });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      await react("❌");
+      return coax.reply("An error occurred while fetching data.");
+    }
+  }
+);
+    
