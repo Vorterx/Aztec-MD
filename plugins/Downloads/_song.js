bd3dad446b25facb1ca611764452869638796490
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-31 13:33:38 +0200

    vorterx

--- plugins/Downloads/_song.js
+++ plugins/Downloads/_song.js
@@ -1,51 +1,39 @@
-const { Zenith } = require ('../../lib/_cmd_sxntax.js');
+const { YTM3 } = require('../../lib/YTM3.js');
+const { Zenith } = require('../../lib/');
 const ytdl = require('ytdl-core');
-const fs = require('fs');
-const path = require('path');
-const search = require('yt-search');
 
 Zenith(
   {
-  usage: 'song',
-  category: 'Downloads',
-  desc: 'To Download the song',
-  filename: __filename
-}, async (vorterx, coax, args, react) => {
-  
-  if (!args) {
-    await react('❌');
-    return coax.reply('Provide a song name, please...');
+    usage: 'song',
+    category: 'Downloads',
+    desc: 'Downloads and sends the requested song',
+    filename: __filename,
+  },
+ async (vorterx, coax, args, react) => {
+   
+    if (!args) {
+      await react('❌');
+      return coax.reply('__Please provide a song name__.');
+    }
+
+    const Query = args;
+    YTM3.searchMusic(Query, async (err, results) => {
+      if (err) {
+        console.error(err);
+        await react('❌');
+        return coax.reply('__Error searching for music...');
+      }
+
+      if (results.length === 0) {
+        await react('❌');
+        return coax.reply(`__No results found for '${Query}'.`);
+      }
+      const gotQuery = results[0];
+      const stream = ytdl(gotQuery.url, { filter: 'audioonly' });
+      vorterx.sendMessage(coax.from, { file: stream, name: `${gotQuery.title}.mp3` });
+      await react('✅');
+      return coax.reply(`_Downloading:'${gotQuery.title}'.`);
+    });
   }
-  const vidS = await search(args);
-
-  if (!vidS || !vidS.videos || vidS.videos.length === 0) {
-    await react('❌');
-    return coax.reply('__Sorry If youre GAY you cant get the song__...');
-  }
-
-  const videoId = vidS.videos[0].videoId;
-  const videoD = await ytdl.getInfo(videoId);
-  const thumbnail = videoD.videoDetails.thumbnails[0].url;
-  const viewCount = videoD.videoDetails.viewCount;
-  const uploaded = videoD.videoDetails.uploadDate;
-
-  const downloadsPath = path.join(__dirname, 'downloads');
-  if (!fs.existsSync(downloadsPath)) {
-    fs.mkdirSync(downloadsPath);
-  }
-   await react('✔️');
-  const audio = ytdl(videoId, { quality: 'highestaudio' });
-  const filePath = path.join(downloadsPath, `${videoD.videoDetails.title}.mp3`);
-  const writeStream = fs.createWriteStream(filePath);
-  audio.pipe(writeStream);
-
-  writeStream.on('finish', () => {
-    const msg = `Downloaded: ${videoD.videoDetails.title}\nViews: ${viewCount}\nUploaded Date: ${uploaded}`;
-    vorterx.sendMessage(coax.from, { text: msg, thumbnail: thumbnail, audio: fs.readFileSync(filePath), mimetype: 'audio/mp3' });
-    fs.unlinkSync(filePath);
-  });
-
-  writeStream.on('error', (err) => {
-    coax.reply(`${err.message}`);
-  });
-})
+);
+    
