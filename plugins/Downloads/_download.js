e59dd776e0ee2e6ecc7a18f8547ed335ef12ba53
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2023-12-31 16:14:26 +0200

    vorterx

--- plugins/Downloads/_download.js
+++ plugins/Downloads/_download.js
@@ -3,6 +3,7 @@ const bocil = require('@bochilteam/scraper');
 const { tiny } = require('@viper-x/fancytext');
 const config = require('../../config.js');
 const gis = require('g-i-s');
+const { ttdl, igdl } = require('btch-downloader');
 
 
 function gisPromise(args) {
@@ -92,10 +93,44 @@ Zenith(
 
   //---------------------------------------------------------------------------                                
 
+Zenith(
+  {
+  usage: 'insta',
+  alias: ['ig'],
+  category: 'Downloads',
+  decs: 'to download insta vids',
+  filename: __filename
+  }, async (vorterx, coax, react, {args}) => {
+   
+    if (!args) {
+      await react('❌');
+      return coax.reply('Please provide a valid Instagram URL.');
+    }
 
+    try {
+      const url = args;
+      const data = await igdl(url);
+      if (!data || data.length === 0) {
+        return coax.reply('Failed to download the video.');
+      }
 
-                                                   
-    //---------------------------------------------------------------------------                                
+      console.log(data);
+      await react('📤');
+      coax.reply(`\`\`\`Downloading your video, please wait...⏳\`\`\``);
+
+      for (let i of data) {
+        const { quality, size, url } = i;
+        const vidi = `*Quality* : 420p\n\n*${config.CAPTION}*`;
+
+        vorterx.sendMessage(coax.from, { video: { url }, caption: tiny(vidi)}, {quoted: coax });
+      }
+    } catch (error) {
+      console.error(error);
+      return coax.reply('Failed to download the video.');
+    }
+  });
+                                                           
+//---------------------------------------------------------------------------                                
 
 
 
