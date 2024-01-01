c73166dee451ec278514cdb8a434e898548724dc
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-01 19:57:26 +0200

    vorterx

--- plugins/Downloads/_ytmp4.js
+++ plugins/Downloads/_ytmp4.js
@@ -10,25 +10,27 @@ Zenith(
     filename: __filename
   },
   async (vorterx, coax, react, { args }) => {
-  try {
+  
+    try {
     if (args.length < 1 || !isUrl(args) || !YTM3.isYTUrl(args)) {
       await react('❌');
       return coax.reply('Please provide a valid YouTube video link.');
     }
 
     const video = await YTM3.mp4(args);
-    const { title, duration, quality, videoUrl } = video;
-
+    const { duration, quality, videoUrl } = video;
+   
+    await react('📤');
     const caption = `
-      *Title:* ${title}
-      *Duration:* ${duration}
-      *Quality:* ${quality}\n\n*${config.CAPTION}*`;
+*Duration:* ${duration}
+*Quality:* ${quality}\n\n*${config.CAPTION}*`;
 
     await vorterx.sendMessage(coax.from, { video: { url: videoUrl }, caption }, { quoted: coax });
   } catch (error) {
     console.error('Error:', error); 
     await react('❌');
     return coax.reply(`${error.message}`);
-  }
-      })
-                                                   
+      }
+    }
+ )
+        
