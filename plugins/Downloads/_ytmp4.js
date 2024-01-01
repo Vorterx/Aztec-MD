ca986a3011d16bf3f2f40e6ab1f5c6a275c94979
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-01 19:47:36 +0200

    vorterx

--- plugins/Downloads/_ytmp4.js
+++ plugins/Downloads/_ytmp4.js
@@ -10,26 +10,25 @@ Zenith(
     filename: __filename
   },
   async (vorterx, coax, react, { args }) => {
-   
-    try {
-      if (args.length < 1 || !isUrl(args) || !YTM3.isYTUrl(args)) {
-        await react('❌');
-        return coax.reply('__Please provide a valid YouTube video link.__');
-      }
-
-      await react('📤');
-      const video = await YTM3.mp4(args);
-      const { title, quality, videoUrl } = video;
-      
-      const caption = `
-        *Title:* ${title}
-        *Quality:* ${quality}\n\n*${config.CAPTION}*`;
-
-      await vorterx.sendMessage(coax.from, { video: { url: videoUrl }, caption }, { quoted: coax });
-    } catch (error) {
-      console.error(error);
+  try {
+    if (args.length < 1 || !isUrl(args) || !YTM3.isYTUrl(args)) {
       await react('❌');
-      return coax.reply('An error occurred while processing your request...');
+      return coax.reply('Please provide a valid YouTube video link.');
     }
+
+    const video = await YTM3.mp4(args);
+    const { title, duration, quality, videoUrl } = video;
+
+    const caption = `
+      *Title:* ${title}
+      *Duration:* ${duration}
+      *Quality:* ${quality}\n\n*${config.CAPTION}*`;
+
+    await vorterx.sendMessage(coax.from, { video: { url: videoUrl }, caption }, { quoted: coax });
+  } catch (error) {
+    console.error('Error:', error); 
+    await react('❌');
+    return coax.reply(`${error.message}`);
   }
-);
+      }
+                                                   
