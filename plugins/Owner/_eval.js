142c186fc4182b3797cfe03b4ba24cea4e00bc9b
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-03 14:11:19 +0200

    vorterx

---
+++ plugins/Owner/_eval.js
@@ -0,0 +1,24 @@
+const { Zenith } = require('../../lib/functions.js');
+
+Zenith({
+    usage: "eval", isOwner: true, category: "Owner", desc: "Runs a server code"
+}, async ({}) => {})
+Zenith(
+    {
+        on: "text",
+        isOwner: true,
+    },
+    async ({
+        vorterx, coax, args, react
+    }) => {
+        if (args.startsWith(">")) {
+            try {
+              await react('🙂');
+                let evaled = await eval(`(async () => { ${args.replace(">", "")} })()`);
+                if (typeof evaled !== "string") evaled = util.inspect(evaled);
+                await coax.reply(`${'```'}${evaled}${'```'}`)
+            } catch (err) {
+                await coax.reply(`_${util.format(err)}_`);
+            }
+        }
+    });
