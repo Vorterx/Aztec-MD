02bd601b1c3314011ffa8abaf4b08e9599ade966
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-03 14:29:15 +0200

    vorterx

--- plugins/Owner/_eval.js
+++ plugins/Owner/_eval.js
@@ -1,24 +1,33 @@
+const util = require('util');
 const { Zenith } = require('../../lib/functions.js');
 
 Zenith({
-    usage: "eval", isOwner: true, category: "Owner", desc: "Runs a server code"
-}, async ({}) => {})
+    usage: "eval",
+    isOwner: true,
+    category: "Owner",
+    desc: "Runs server code"
+}, async ({}) => {});
+
 Zenith(
     {
-        on: "text",
+        on: "args",
         isOwner: true,
     },
     async (
-        vorterx, coax, react, { args
-    }) => {
+        vorterx, coax, react, { args }
+    ) => {
         if (args.startsWith(">")) {
             try {
-              await react('🙂');
-                let evaled = await eval(`(async () => { ${args.replace(">", "")} })()`);
-                if (typeof evaled !== "string") evaled = util.inspect(evaled);
-                await coax.reply(`${'```'}${evaled}${'```'}`)
-            } catch (err) {
-                await coax.reply(`_${util.format(err)}_`);
-            }
-        }
-    });
+                        await react('🙂');
+                            let evaled = await eval(`(async () => { ${args.replace(">", "")} })()`);
+                                if (typeof evaled !== "string") evaled = util.inspect(evaled);
+                                    await coax.reply(`${'```'}${evaled}${'```'}`);
+                                     } catch (err) {
+                                      await coax.reply(`_${util.format(err)}_`);   
+                             }
+                     }
+         
+          }
+
+   );
+                                
