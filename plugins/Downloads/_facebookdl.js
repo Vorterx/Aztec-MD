73d82e416ce9420d2e774d3c1c1e532ae6b507c5
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 23:23:38 +0200

    vorterx:

---
+++ plugins/Downloads/_facebookdl.js
@@ -0,0 +1,37 @@
+const { Zenith } = require('../../lib/functions.js');
+
+
+Zenith(
+    {
+    usage: "fb",
+    desc: "To download Facebook",
+    category: "Downloads",
+    filename: __filename
+    }, async (vorterx, m, react, {args}) => {
+       
+        try {
+            if (!args) {
+                await react("❌");
+                return m.reply(`*Please Provide a Valid Facebook Video Link*`);
+            } else {
+                await react("📤");
+                bocil.facebookdlv2(`${args}`).then(async (data) => {
+
+                    const { filesizeH, quality } = data.result[0];
+                    let caption = `*Quality* : ${quality}\n\n*${config.CAPTION}*`;
+                    vorterx.sendMessage(m.chat, {
+                        video: {
+                            url: data.result[0].url
+                        },
+                        caption: tiny(caption)
+                    }, {
+                        quoted: m
+                    });
+                });
+            }
+        } catch (error) {
+            vorterx.sendMessage(m.chat, {
+                text: "Error occurred while processing"
+            });
+        }
+    });
