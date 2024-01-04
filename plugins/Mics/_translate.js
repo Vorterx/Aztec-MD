b0c765873cb730cbe13b9e8cd54f682b9cd4f097
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 21:11:31 +0200

    vorterx

--- plugins/Mics/_translate.js
+++ plugins/Mics/_translate.js
@@ -15,19 +15,19 @@ Zenith(
   desc: 'Translate any usable language',
   category: 'Mics',
   filename: __filename
-  }, async (vorterx, coax, react, {args}) => {
+  }, async (vorterx, m, react, {args}) => {
     
     if (!args) {
       await react('❌');
-      return coax.reply('Example: tr zu Damn l love this Diegoson eish');
+      return m.reply('Example: tr zu Damn l love this Diegoson eish');
     }
 
     let lang;
     let data;
 
-    if (args && coax.quoted && coax.quoted.args) {
+    if (args && m.quoted && m.quoted.args) {
       lang = args.slice(0, 2);
-      data = coax.quoted.args;
+      data = m.quoted.args;
     } else if (args) {
       lang = args.slice(0, 2);
       data = args.substring(2).trim();
@@ -37,12 +37,12 @@ Zenith(
       await react('📝');
       const result = await translate(data, { to: lang });
       const caption = `*📝TEXT*: ${data}\n\n*🧘TRANSLATED*: ${result.text}\n\n*${config.CAPTION}*`;
-      await coax.reply(caption);
+      await m.reply(caption);
     } catch (error) {
       let errorMessage = 'An error occurred while translating';
       if (error.code === 'NOT_SUPPORTED') {
         errorMessage = 'The language is not supported';
       }
-      await coax.reply(errorMessage);
+      await m.reply(errorMessage);
     }
   });
