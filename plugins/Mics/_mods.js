2da6aa92e1ff5bbbf2784c075b20defe847d492b
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 21:02:55 +0200

    vorterx

--- plugins/Mics/_mods.js
+++ plugins/Mics/_mods.js
@@ -14,22 +14,22 @@ Zenith(
   desc: 'To check mods admins to the bot',
   category: 'Owner',
   filename: __filename
-  }, async (vorterx, coax, react, {args, quoted}) => {
+  }, async (vorterx,m, react, {args, quoted}) => {
     
     await react('✔️');
     let mods = process.env.MODS ? process.env.MODS.split(',').map(mod => mod.replace('@net.whatsapp', '').trim()) : [];
     let me = mods.map((mod, index) => `  ${index + 1} 〄 @${mod}\n\n`).join('');
     let mention = [m.sender, ...mods.map(mod => `${mod}@s.whatsapp.net`)];
 
-    if (!me || !mods || !mods[0]) return await vorterx.sendMessage(coax.from, "*There's no mods added yet...*", 'text', { quoted: coax });
+    if (!me || !mods || !mods[0]) return await vorterx.sendMessage(m.chat, "*There's no mods added yet...*", 'text', { quoted: m });
 
     let str = `
 ${tiny('👤 *AZTEC-MD MODS* 👤')}\n\n${me}`.trim();
-    await vorterx.sendMessage(coax.from, {
+    await vorterx.sendMessage(m.chat, {
       image: { url: "https://telegra.ph/file/5fd51597b0270b8cff15b.png" },
       caption: str,
       mentions: mention,
-      quoted: coax
+      quoted: m
     });
   })
       
