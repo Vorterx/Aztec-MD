e5283dc26d0af4baa9ea487fd01d6b10a4d7692c
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 20:38:04 +0200

    vorterx

--- plugins/Search/_lyrics.js
+++ plugins/Search/_lyrics.js
@@ -8,18 +8,18 @@ Zenith(
   usage: 'lyrics',
   category: 'Search',
   filename: __filename
-  }, async (vorterx, coax, react, {args}) => {
+  }, async (vorterx,m, react, {args}) => {
   
     try {
       if (!args || typeof args !== 'string') {
         await react('❌');
-        return coax.reply(`Please provide a song name or artist. For example, "Dior by Pop Smoke"`);
+        return m.reply(`Please provide a song name or artist. For example, "Dior by Pop Smoke"`);
       }
       const search = encodeURIComponent(args.trim());
       const { data } = await axios.get(`https://weeb-api.vercel.app/genius?query=${search}`);
 
       if (!data || data.length === 0) {
-        return coax.reply('Lyrics not found for the given song or artist.');
+        return m.reply('Lyrics not found for the given song or artist.');
       }
 
       console.log(data);
@@ -31,7 +31,6 @@ Zenith(
       const lyrics = lyricsRes.data || 'Lyrics not found.';
       const thumbnail = data[0].url;
       const img = thumbnail ? Buffer.from(thumbnail).toString('base64') : '';
-
       const res = `*🌷TITLE*: ${title}\n\n*👤ARTIST*: ${artist}\n\n${lyrics}\n\n*${config.CAPTION}*`;
 
       const msgData = {
@@ -48,9 +47,9 @@ Zenith(
         data: [0, ...(data[0].data || [])].map(JSON.stringify)
       };
 
-      return vorterx.sendMessage(coax.from, msgData);
+      return vorterx.sendMessage(m.chat, msgData);
     } catch (error) {
       console.error(error);
-      return coax.reply('An error occurred while processing...');
+      return m.reply('An error occurred while processing...');
     }
   });
