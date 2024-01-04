83b8abd069e276da56217e4cbb5fbba651ef8d42
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 21:20:37 +0200

    vorterx

--- plugins/Groups/_gcKick.js
+++ plugins/Groups/_gcKick.js
@@ -7,16 +7,16 @@ Zenith(
 	desc: "remove Member from group",
 	category: "Group",
         filename: __filename
-	}, async (vorterx, coax, react, {isBotAdmin, isAdmin, mentionByTag}) => {
+	}, async (vorterx, m, react, {isBotAdmin, isAdmin, mentionByTag}) => {
 		
-    if(!isAdmin) { await react("❌"); return coax.reply(`*🔌This is admin command*`);
+    if(!isAdmin) { await react("❌"); return m.reply(`*🔌This is admin command*`);
         }
-		if(!isBotAdmin) { await react("😭"); return coax.reply(`*🔌I need to be an admin in order to use this command*`);
+		if(!isBotAdmin) { await react("😭"); return m.reply(`*🔌I need to be an admin in order to use this command*`);
         }
 		const mention = await mentionByTag
-		if(!mention[0]) { await react("❌"); return coax.reply(`*🤔No user found*`);
+		if(!mention[0]) { await react("❌"); return m.reply(`*🤔No user found*`);
         }
 		await react("🙄");
-		await vorterx.groupParticipantsUpdate(coax.from, [mention[0]], "remove")
-		await vorterx.sendMessage(coax.from,{text:`*🎊User has been removed by ${coax.pushName}*`},{quoted: coax})
+		await vorterx.groupParticipantsUpdate(m.chat, [mention[0]], "remove")
+		await vorterx.sendMessage(m.chat,{text:`*🎊User has been removed by ${m.pushName}*`},{quoted: m})
 	});
