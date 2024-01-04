812a86240e3932c10945c70c51eb896222a219b8
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 21:23:44 +0200

    vorterx

--- plugins/Groups/_gcOpen.js
+++ plugins/Groups/_gcOpen.js
@@ -8,12 +8,12 @@ alias: ['unmute'],
 category: "Group",
 desc: "To Open the group",
 filename: __filename
-  }, async (vorterx, coax, react, {isAdmin, isBotAdmin, isGroup}) => {
+  }, async (vorterx,m, react, {isAdmin, isBotAdmin, isGroup}) => {
 
-if(!coax.isGroup) { 
-await react ("❌"); return coax.reply("*👋 Sorry this command is for admins only*");
+if(!m.isGroup) { 
+await react ("❌"); return m.reply("*👋 Sorry this command is for admins only*");
 }
 await react("🔉");
-  coax.reply(`『 GROUP SETTINGS 』\n\nGroup has been opened by admin,All participants can now send messagez`);
-return await vorterx.groupSettingUpdate(coax.from, "not_announcement");
+  m.reply(`『 GROUP SETTINGS 』\n\nGroup has been opened by admin,All participants can now send messagez`);
+return await vorterx.groupSettingUpdate(m.chat "not_announcement");
   });
