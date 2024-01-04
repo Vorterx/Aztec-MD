ecb8536729c97d32d56817e0372f01310cc7d4c4
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 20:28:40 +0200

    vorterx

--- plugins/Search/_youtube-search.js
+++ plugins/Search/_youtube-search.js
@@ -9,39 +9,39 @@ Zenith (
   category: 'Downloads',
   desc: 'To search anything',
   filename: __filename
-  }, async (vorterx, coax, react, { args}) => {
+  }, async (vorterx, m, react, { args }) => {
    
     if (!args) {
       await react('❌');
-      return coax.reply('*Provide a query example how to create Aztec*');
+      return m.reply('*Provide a query example how to create Aztec*');
     }
     await react('🔍');
     try {
       const results = await yts(args);
-      const videos = results.videos.slice(0, 15);
+      const videos = results.videos.slice(0, 25);
       if (videos.length === 0) {
         await react('❌');
-        return coax.reply('No YouTube videos found for the given query.');
+        return m.reply('No YouTube videos found for the given query.');
       }
 
       let res = '';
       if (videos.length > 0) {
         res = videos.map((video, index) => {
           const searchIndex = index + 1;
-          return `🔎 Search: ${searchIndex}\n\n` +
-            `📽️ TITLE: ${video.title}\n` +
-            `👁️ VIEWS: ${video.views}\n` +
-            `⌛ DURATION: ${video.timestamp}\n` +
-            `📅 UPLOADED: ${video.ago}\n` +
-            `🔗 LINK: ${video.url}\n\n`;
+          return `*🔎 Search*: ${searchIndex}\n\n` +
+            `*📽️ TITLE*: ${video.title}\n` +
+            `*👁️ VIEWS*: ${video.views}\n` +
+            `*⌛ DURATION*: ${video.timestamp}\n` +
+            `*📅 UPLOADED*: ${video.ago}\n` +
+            `*🔗 LINK*: ${video.url}\n\n`;
         }).join('');
       }
 
       const img = videos[0].thumbnail;
-      vorterx.sendMessage(coax.from, { image: { url: img }, caption: res }, { quoted: coax });
+      vorterx.sendMessage(m.chat, { image: { url: img }, caption: res }, { quoted: m });
     } catch (error) {
       console.error(error);
       await react('❌');
-      coax.reply('An error occurred while performing the YouTube search.');
+      m.reply('An error occurred while performing the YouTube search.');
     }
   });
