7c05109ab6a060beded9fefb1ff8d68a744576e6
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 23:36:24 +0200

    vorterx

--- plugins/Downloads/_ytmp4.js
+++ plugins/Downloads/_ytmp4.js
@@ -11,27 +11,26 @@ Zenith(
     filename: __filename
   },
   async (vorterx, m, react, { args }) => {
-  
     try {
-    if (args.length < 1 || !isUrl(args) || !YTM3.isYTUrl(args)) {
-      await react('❌');
-      return m.reply('Please provide a valid YouTube video link.');
-    }
+      if (args.length < 1 || !isUrl(args) || !YTM3.isYTUrl(args)) {
+        await react('❌');
+        return m.reply('Please provide a valid YouTube video link.');
+      }
 
-    const video = await YTM3.mp4(args);
-    const { duration, quality, videoUrl } = video;
-   
-    await react('📤');
-    const caption = `
+      const video = await YTM3.mp4(args);
+      const { duration, quality, videoUrl } = video;
+
+      await react('📤');
+      const ca_ption = `
 *Duration:* ${duration}
 *Quality:* ${quality}\n\n*${config.CAPTION}*`;
 
-    await vorterx.sendMessage(m.chat, { video: { url: videoUrl }, tiny(caption) }, { quoted: m });
-  } catch (error) {
-    console.error('Error:', error); 
-    await react('❌');
-    return m.reply(`${error.message}`);
-      }
+      await vorterx.sendMessage(m.chat, { video: { url: videoUrl }, caption: tiny(ca_ption) }, { quoted: m });
+    } catch (error) {
+      console.error('Error:', error); 
+      await react('❌');
+      return m.reply(`${error.message}`);
     }
- )
+  }
+);
         
