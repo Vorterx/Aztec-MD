eebad2ff885122379e78fbe3821da63d253ee48e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-04 21:50:35 +0200

    vorterx ::

--- plugins/Downloads/_mediafire.js
+++ plugins/Downloads/_mediafire.js
@@ -19,15 +19,15 @@ Zenith(
   category: 'Downloads',
   desc: 'Download files from MediaFire links',
   filename: __filename
-}, async (vorterx, coax, react,{args}) => {
+}, async (vorterx, m, react,{args}) => {
     try {
       if (args.length === 0 || !isValidUrl(args[0])) {
         await react('❌');
-        return coax.reply('Please provide a valid MediaFire link.');
+        return m.reply('Please provide a valid MediaFire link.');
       }
 
       await react('📤');
-      await coax.reply(`\`\`\`Downloading your media, wait...⏳\`\`\``);
+      await m.reply(`\`\`\`Downloading your media, wait...⏳\`\`\``);
 
       const result = await mediafiredl(args[0]);
 
@@ -41,20 +41,20 @@ Zenith(
 │ *Type:* ${filetype}
 ╰───────────────────༓\n\n*${config.CAPTION}*`;
 
-        await vorterx.sendMessage(coax.from, {
+        await vorterx.sendMessage(m.chat, {
           url,
           caption: tiny(v_cap),
           document: { url: filename, mimetype: ext },
-          quoted: coax,
+          quoted: m,
         });
       } else {
         await react('❌');
-        return coax.reply('Failed to download the file. Please check the MediaFire link.');
+        return m.reply('Failed to download the file. Please check the MediaFire link.');
       }
     } catch (error) {
       console.error(error);
       await react('❌');
-      return coax.reply('An unexpected error occurred during the download process.');
+      return m.reply('An unexpected error occurred during the download process.');
     }
   });
           
