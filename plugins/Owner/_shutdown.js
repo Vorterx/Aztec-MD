adfad12aff7beca95d062d109ad936ebcc98a14a
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-05 18:41:32 +0200

    vorterx::

--- plugins/Owner/_shutdown.js
+++ plugins/Owner/_shutdown.js
@@ -5,23 +5,28 @@
 
    const { Zenith } = require('../../lib/functions.js');
 
-    Zenith(
-                    {
-                          usage: 'shutdown',
-                              desc: 'To shut down the bot',
-                                  category: 'Owner',
-                                     filename: __filename
-                                        },  
-                                               async (vorterx,m,react, { isDev}) => {
-                                                      
-                                                         if(!isDev) {
-                                                                   await react('❌');
-                                                                        return m.reply('This command is for my Dev only');
-                                                                            }
-                                                                                await react('🌀');
-                                                                                       m.reply('_Shutting down_');
-                                                                                               await sleep(3000)
-                                                                                                    process.exit()
-                                                                                           }
-                                                                                     );
-            
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+Zenith(
+  {
+    usage: 'shutdown',
+    desc: 'To shut down the bot',
+    category: 'Owner',
+    filename: __filename
+  },
+  async (vorterx, m, react, { isDev }) => {
+    
+            if (!isDev) {
+                await react('❌');
+                   return m.reply('This command is for my Dev only');
+              }
+
+                  await react('🌀');
+                      m.reply('_Shutting down_');
+                        await sleep(3000);
+                            process.exit();
+             }
+);
+         
