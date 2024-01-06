b323b4ad200c7891738a25a99d753591622990fd
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-07 00:31:41 +0200

    Update _menuu.js

--- plugins/_menuu.js
+++ plugins/_menuu.js
@@ -31,44 +31,39 @@ Zenith(
         },
       };
 
-      const categoryFolders = fs.readdirSync(pluginsPath, { withFileTypes: true })
-        .filter(file => file.isDirectory())
-        .map(folder => path.join(pluginsPath, folder.name));
+for (const categoryPath of categoryFolders) {
+  console.log('Checking category folder:', categoryPath);
+  
+  const jsFiles = fs.readdirSync(categoryPath)
+    .filter(file => file.endsWith('.js'))
+    .map(file => path.join(process.cwd(), categoryPath, file)); 
 
-      for (const categoryPath of categoryFolders) {
-        console.log('Checking category folder:', categoryPath);
-        
-        const jsFiles = fs.readdirSync(categoryPath)
-          .filter(file => file.endsWith('.js'))
-          .map(file => path.join(categoryPath, file));
+  for (const filePath of jsFiles) {
+    console.log('Checking command file:', filePath);
 
-        for (const filePath of jsFiles) {
-          console.log('Checking command file:', filePath);
+    const commandModule = require(filePath);
 
-          const commandModule = require(filePath);
+    if (commandModule && commandModule.Zenith) {
+      console.log('Command Module:', commandModule);
 
-          if (commandModule && commandModule.Zenith) {
-            console.log('Command Module:', commandModule);
+      const commandInfo = commandModule.Zenith;
 
-            const commandInfo = commandModule.Zenith;
-
-            if (commandInfo.usage) {
-              if (messageToSend === '') {
-                messageToSend += `
+      if (commandInfo.usage) {
+        if (messageToSend === '') {
+          messageToSend += `
 ${menuDesign.header.left}${menuDesign.header.right}
 *NAME*: ${m.pushName}
 *PREFIX*: ${prefix}
 ${menuDesign.header.down}`;
-              }
+        }
 
-              messageToSend += `
+        messageToSend += `
 ${menuDesign.body.left}${menuDesign.body.up}『${commandInfo.category || 'Uncategorized'}』${menuDesign.body.right}
 ${menuDesign.body.down} ${commandInfo.usage}`;
-            }
-          }
-        }
       }
-
+    }
+  }
+}
       if (messageToSend !== '') {
         vorterx.sendMessage(m.chat, messageToSend, { quoted: m });
       } else {
