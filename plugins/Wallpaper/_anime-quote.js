c71168cb040605a9b80b566b14b3c1435c916cb1
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-08 16:00:30 +0200

    vorterx

---
+++ plugins/Wallpaper/_anime-quote.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const { Zenith } = require('../../lib/functions');
+
+Zenith({
+  usage: 'anime-quote',
+  desc: 'Anime Quotes',
+  category: 'Anime',
+}, async (vorterx, m, react) => {
+  await react('✔️');
+
+  try {
+    const res = await axios.get('https://api.lolhuman.xyz/api/random/quotesnime?apikey=5c250069e8936d6bf70295b8');
+    const { quote, character, anime, episode } = res.data.result;
+
+    const forQuote = `*Anime Quote:*\n${quote}\n\n*Character:* ${character}\n*Anime:* ${anime}\n*Episode:* ${episode}`;
+
+    await vorterx.sendMessage(m.chat, forQuote);
+  } catch (error) {
+    console.error(error.message);
+
+    m.reply('Failed to fetch anime quote. Please try again later');
+  }
+});
+      
