b70f967bbca3eb28a94fa03c48e97cdd19210450
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-08 17:13:37 +0200

    vorterx

--- plugins/Wallpaper/_anime-quote.js
+++ plugins/Wallpaper/_anime-quote.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { Zenith } = require('../../lib/functions');
+const config = require('../../config.js');
 
 Zenith({
   usage: 'anime-quote',
@@ -8,15 +9,17 @@ Zenith({
 }, async (vorterx, m, react) => {
   await react('✔️');
 
-  try {
-    const res = await axios.get('https://api.lolhuman.xyz/api/random/quotesnime?apikey=5c250069e8936d6bf70295b8&language=en');
+  let cmd = "https://i.imgur.com/oGX8YvH.jpg";
+  const res = await axios.get('https://api.lolhuman.xyz/api/random/quotesnime?apikey=5c250069e8936d6bf70295b8');
+
+  if (res && res.data && res.data.result) {
     const { quote, character, anime, episode } = res.data.result;
+    const forQuote = `*ANIME QUOTE:*\n${quote}\n\n*_Character_:* ${character}\n\n*_Anime_:* ${anime}\n\n*_Episode_:* ${episode}\n\n*${config.CAPTION}*`;
 
-    const forQuote = `*Anime Quote:*\n${quote}\n\n*_Character_:* ${character}\n\n*_Anime_:* ${anime}\n\n*_Episode_:* ${episode}`;
-
-    await vorterx.sendMessage(m.chat, forQuote);
-  } catch (error) {
-    console.error(error.message);
-    m.reply('Please try again later');
+    await vorterx.sendMessage(m.chat, { image: { url: cmd }, text: forQuote });
+  } else {
+    console.error(res && res.data && res.data.message);
+    m.reply('Please try again later.');
   }
-});      
+});
+  
