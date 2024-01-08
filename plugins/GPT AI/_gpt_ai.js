a5485d2b6c6a3b7aa54f4d9bf122621c0ba26e9e
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-08 23:08:52 +0200

    vorterx

--- plugins/GPT AI/_gpt_ai.js
+++ plugins/GPT AI/_gpt_ai.js
@@ -1,50 +1,45 @@
-/*
-* @Author: DiegosonTech
-* @BotName: Aztec-MD
-*/
-
 const fetch = async (url) => import('node-fetch').then(module => module.default(url));
 const { getBuffer } = require('../../lib/_getBuffer.js');
 const { Zenith } = require('../../lib/_cmd_sxntax.js');
 
-Zenith(
-  {
-    usage: "gpt",
-    alias: ["ai", "openai", "chatgpt"],
-    category: "GPT AI",
-    desc: "Randomly search",
-    filename: __filename
-  }, async (vorterx, m, react, { args }) => {
+Zenith({
+  usage: "gpt",
+  alias: ["ai", "openai", "chatgpt"],
+  category: "GPT AI",
+  desc: "Randomly search",
+  filename: __filename
+}, async (vorterx, m, react, { args }) => {
   
-    if (!args) {
-      await react("❌");
-      return m.reply(`*Provide me a query, e.g., "Who made Aztec?"`);
-    }
+  if (!args) {
+    await react("❌");
+    return m.reply(`*Provide me a query, e.g., "Who made Aztec?"`);
+  }
 
-    const encodedArgs = encodeURIComponent(args);
-    const gpt_api = `https://api.lolhuman.xyz/api/openai-turbo?apikey=GataDios&text=${encodedArgs}`;                                                                                    
-    const res = await fetch(gpt_api);
-    const result = await res.json();
-    console.log(result);
+  const encodedArgs = encodeURIComponent(args);
+  const gpt_api = `https://api.lolhuman.xyz/api/openai-turbo?apikey=GataDios&text=${encodedArgs}`;                                                                                    
+  const res = await fetch(gpt_api);
+  const result = await res.json();
+  console.log(result);
 
-    if (result.status === true) {
-      await react("💡");
-      await vorterx.sendMessage(m.chat, {
-        text: result.result,
-        image: { url: "https://i.ibb.co/9bfjPyH/1-t-Y7-MK1-O-S4eq-YJ0-Ub4irg.png" },
-        contextInfo: {
-          externalAdReply: {
-            title: "GPT TURBO 3.5K",
-            body: "",
-            mediaType: 1,
-            thumbnail: await getBuffer("https://i.ibb.co/9bfjPyH/1-t-Y7-MK1-O-S4eq-YJ0-Ub4irg.png"),
-            mediaUrl: "",
-            sourceUrl: "",            
-          },
+  if (result.status === true) {
+    await react("💡");
+    await vorterx.sendMessage(m.chat, {
+      text: result.result,
+      image: { url: "https://i.ibb.co/9bfjPyH/1-t-Y7-MK1-O-S4eq-YJ0-Ub4irg.png" },
+      contextInfo: {
+        externalAdReply: {
+          title: "GPT TURBO 3.5K",
+          body: "",
+          mediaType: 1,
+          thumbnail: await getBuffer("https://i.ibb.co/9bfjPyH/1-t-Y7-MK1-O-S4eq-YJ0-Ub4irg.png"),
+          mediaUrl: "",
+          sourceUrl: "",            
         },
-      });
-    } else {
-
-    }
-  });
-  
+      },
+    });
+  } else {
+    await react("❌");
+    await m.reply('_Error occurred, sorry._');
+  }
+});
+          
