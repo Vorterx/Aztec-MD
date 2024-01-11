ce3a03ffd66e1e290c3b75d18166145385b7e9bb
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-11 23:59:41 +0200

    vorterx

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -8,27 +8,27 @@ const path = require('path');
 
 const pluginDir = path.join(__dirname);
 const commandsByCategory = {};
-
+const { commands } = require('../lib/_cmd_sxntax');
 function readCommandsFromDirectory(directory) {
-  const files = fs.readdirSync(directory);
+    const files = fs.readdirSync(directory);
 
-  for (const file of files) {
-    const filePath = path.join(directory, file);
-    const stat = fs.statSync(filePath);
+    for (const file of files) {
+        const filePath = path.join(directory, file);
+        const stat = fs.statSync(filePath);
 
-    if (stat.isDirectory()) {
-      readCommandsFromDirectory(filePath);
-    } else if (file.endsWith('.js') && file !== 'menu.js') {
-      const plugin = require(filePath);
-      const category = plugin.category || 'Uncategorized';
+        if (stat.isDirectory()) {
+            readCommandsFromDirectory(filePath);
+        } else if (file.endsWith('.js') && file !== 'menu.js') {
+            const plugin = require(filePath);
+            const category = plugin.category || 'Uncategorized';
 
-      if (!commandsByCategory[category]) {
-        commandsByCategory[category] = [];
-      }
+            if (!commandsByCategory[category]) {
+                commandsByCategory[category] = [];
+            }
 
-      commandsByCategory[category].push(plugin);
+            commandsByCategory[category].push(plugin);
+        }
     }
-  }
 }
 
 readCommandsFromDirectory(pluginDir);
@@ -38,49 +38,54 @@ const configData = fs.readFileSync(configFile);
 const configJson = JSON.parse(configData);
 
 Zenith(
-  {
-    usage: 'menu',
-    alias: ['h', 'help'],
-    desc: 'Reveals menu categories commands',
-    filename: __filename,
-  },
-  async (vorterx, m, react, { args  } ) => {
-    await react('Ⓜ️');
+    {
+        usage: 'menu',
+        alias: ['h', 'help'],
+        desc: 'Reveals menu categories commands',
+        filename: __filename,
+    },
+    async (vorterx, m, react, { args }) => {
+        
+        await react('Ⓜ️');
+        const cmd = commands.find((cmd) => cmd.usage === (args.split(" ")[0].toLowerCase()))
+        if (cmd && cmd.dontAddCommandList === false && cmd.usage !== undefined) {
+            if (!cmds[cmd.category]) cmds[cmd.category] = []
+            cmds[cmd.category].push(cmd.usage)
+        }
+        const allLogos = [...(config.LOGOS || []), ...(process.env.LOGOS ? process.env.LOGOS.split(',') : [])];
+        const doIndex = Math.floor(Math.random() * allLogos.length);
+        const getLogo = allLogos[doIndex];
 
-    const allLogos = [...(config.LOGOS || []), ...(process.env.LOGOS ? process.env.LOGOS.split(',') : [])];
-    const doIndex = Math.floor(Math.random() * allLogos.length);
-    const getLogo = allLogos[doIndex];
+        const isImage = /\.(jpg|jpeg|png|gif)$/i.test(getLogo);
+        const mediaType = isImage ? 1 : 2;
+        let headerTop, midSection, bottomSection, categoryLeft, categoryRight, commandLine, categoryEnd;
+        let randomMenu = 0;
 
-    const isImage = /\.(jpg|jpeg|png|gif)$/i.test(getLogo);
-    const mediaType = isImage ? 1 : 2;
-    let headerTop, midSection, bottomSection, categoryLeft, categoryRight, commandLine, categoryEnd;
-    let randomMenu = 0;
+        if (!process.env.MENU) {
+            randomMenu = Math.floor(Math.random() * 2) + 1;
+        }
 
-    if (!process.env.MENU) {
-      randomMenu = Math.floor(Math.random() * 2) + 1;
-    }
+        if (randomMenu == 1 || process.env.MENU.trim().startsWith('1') || process.env.MENU.toLowerCase().includes('aztec-md')) {
+            headerTop = `┏╼╾╼╾『 *${configJson.Bots[0].BotName || ''}* 』╾╼⦿`;
+            midSection = `|`;
+            bottomSection = `┗╼╾╼╾╼╼╾╼╾╼╼╾╼╾╼⦿`;
+            categoryLeft = `┌─╾╼╼『`;
+            categoryRight = `』╾╼❖\n`;
+            commandLine = `│ ╰─ `;
+            categoryEnd = `\n╰─────────────◉\n`;
 
-    if (randomMenu == 1 || process.env.MENU.trim().startsWith('1') || process.env.MENU.toLowerCase().includes('aztec-md')) {
-      headerTop = `┏╼╾╼╾『 *${configJson.Bots[0].BotName || ''}* 』╾╼⦿`;
-      midSection = `|`;
-      bottomSection = `┗╼╾╼╾╼╼╾╼╾╼╼╾╼╾╼⦿`;
-      categoryLeft = `┌─╾╼╼『`;
-      categoryRight = `』╾╼❖\n`;
-      commandLine = `│ ╰─ `;
-      categoryEnd = `\n╰─────────────◉\n`;
-      
-    }
+        }
 
-    let getCommands = '';
+        let getCommands = '';
 
-for (const category in commandsByCategory) {
-  getCommands += `${categoryLeft} *${category}* ${categoryRight}\n`;
-  getCommands += commandsByCategory[category].map((plugin) => ` ${commandLine} ${plugin.usage || 'Undefined'}`).join('\n');
-  getCommands += `\n${categoryEnd}\n\n`;
-}
-  
-    const country = 'South Africa';
-    const amarok = `${headerTop}
+        for (const category in commandsByCategory) {
+            getCommands += `${categoryLeft} *${category}* ${categoryRight}\n`;
+            getCommands += commandsByCategory[category].map((plugin) => ` ${commandLine} ${plugin.usage || 'Undefined'}`).join('\n');
+            getCommands += `\n${categoryEnd}\n\n`;
+        }
+
+        const country = 'South Africa';
+        const amarok = `${headerTop}
 ${midSection} User: ${m.pushName || ''}
 ${midSection} Botname: ${configJson.Bots[0].BotName || ''}
 ${midSection} Owner: ${configJson.Bots[0].Owner || ''}
@@ -91,29 +96,29 @@ ${midSection} Date: ${new Date().toLocaleDateString()}
 ${midSection} From: ${country || ''}
 ${bottomSection}\n\n${getCommands}*${config.CAPTION || ''}*`;
 
-    const chatBot = {
-      [isImage ? 'image' : 'video']: {
-        url: getLogo,
-      },
-      caption: tiny(amarok),
-      headerType: 2,
-      contextInfo: {
-        forwardingScore: 6,
-        isForwarded: true,
-        discountCode: 'CHATGPT20',
-        externalAdReply: {
-          title: `${config.CAPTION || ''}`,
-          body: 'ʙᴇsᴛ ᴛᴏ ᴜsᴇ',
-          mediaType,
-          thumbnail: await getBuffer(getLogo),
-          mediaUrl: '',
-          sourceUrl: `${process.env.MODS || ''}`,
-        },
-      },
-    };
+        const chatBot = {
+            [isImage ? 'image' : 'video']: {
+                url: getLogo,
+            },
+            caption: tiny(amarok),
+            headerType: 2,
+            contextInfo: {
+                forwardingScore: 6,
+                isForwarded: true,
+                discountCode: 'CHATGPT20',
+                externalAdReply: {
+                    title: `${config.CAPTION || ''}`,
+                    body: 'ʙᴇsᴛ ᴛᴏ ᴜsᴇ',
+                    mediaType,
+                    thumbnail: await getBuffer(getLogo),
+                    mediaUrl: '',
+                    sourceUrl: `${process.env.MODS || ''}`,
+                },
+            },
+        };
 
-    await vorterx.sendMessage(m.chat, chatBot, { quoted: m });
-  }
+        await vorterx.sendMessage(m.chat, chatBot, { quoted: m });
+    }
 );
 
 //_____________________________________________________________________
