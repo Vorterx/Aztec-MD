b4ff6637119c5eca898425253e8f082cdb2cd9de
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-22 22:00:12 +0200

    vorterx

---
+++ plugins/Mics/_jids.js
@@ -0,0 +1,12 @@
+const { Zenith } = require('../../lib/functions');
+
+Zenith({
+  usage: 'jid',
+  desc: 'Mmh ldk but its useless though',
+  category: 'Mics',
+}, async (vorterx,m, react) => {
+
+       await react('😋');
+  await m.reply(m.chat);
+}
+       );
