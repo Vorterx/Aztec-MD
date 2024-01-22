ca434f5c13883f0d7638c075a65ecb8370dbbde4
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-22 17:23:28 +0200

    vorterx

---
+++ plugins/GPT AI/_Bin.js
@@ -0,0 +1,29 @@
+const { Zenith } = require('../../lib/functions');
+const fetch = require('node-fetch');
+
+Zenith( 
+  {
+    usage: 'bins',
+    desc: 'To convert to bin',
+    category: 'GPT AI',
+  }, async (vorterx, m, react, { args }) => {
+
+    if (!args) {
+      await react('❌');
+      return m.reply('Provide cc to convert');
+    }
+
+    var THIS_HEROKU = `https://vihangayt.me/tools/bingen?query=${args}`;
+    const res = await fetch(THIS_HEROKU);
+    const data = await res.json();
+
+    if (data.status) {
+      const _C_Info = data.data.map(card => {
+        return `CardNumber: ${card.CardNumber}\nExpirationDate: ${card.ExpirationDate}\nCVV: ${card.CVV}\n\n`;
+      }).join('');
+
+      return m.reply(`*BIN CONVERTER*\n\n${_C_Info}`);
+    } else {
+      m.reply('Im tired bruh');
+    }
+  });
