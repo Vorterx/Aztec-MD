f39266d254733e5cebccc4cc0a463ba70db0cffd
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-22 16:45:50 +0200

    vorterx

---
+++ plugins/Search/_Nassa.js
@@ -0,0 +1,31 @@
+const { Zenith } = require('../lib/functions');
+const  = require('node-fetch');
+
+Zenith(
+  {
+    usage: 'nassa',
+    desc: 'To check Nassa news',
+    category: 'UPDATES',
+  }, async (vorterx, m, react, { args }) => {
+
+  await react('✳️');
+  const db = 'https://vihangayt.me/details/nasa';
+  const THEN_RES = await fetch(db);
+
+  if (THEN_RES.ok) {
+    const { status, data } = await THEN_RES.json();
+
+    if (status && data) {
+      const { date, explanation, image, title } = data;
+      const DETA = `*NASSA NEWS UPDATES*\n*Date*: ${date}\n*Title*: ${title}\n*News*: ${explanation}\n\n*${config.CAPTION}*`;
+
+      vorterx.sendMessage(m.chat, { image: { url: image }, caption: DETA }, { quoted: m });
+    } else {
+      m.reply('*_E3RR NOT FOUND_*');
+    }
+  } else {
+    const VARTERX = `${THEN_RES.status}`;
+    console.error(VARTERX);
+    m.reply(VARTERX);
+  }
+});
