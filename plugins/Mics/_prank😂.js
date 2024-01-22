5280e33423b3a3b3693cde16c39fc8868d54f1f7
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-22 19:53:24 +0200

    aztec

--- plugins/Mics/_prank😂.js
+++ plugins/Mics/_prank😂.js
@@ -1,19 +1,19 @@
-const { Zenith, sleep } = require('../..lib/functions');
+const { Zenith, REST_BRUH } = require('../..lib/functions');
 
 Zenith({ usage: 'countdown', desc: 'Spooky countdown prank', category: 'Prank' }, async (vorterx, m, react) => {
  
   m.edit('👻 Initiating Spooky Countdown...');
    await react('🤐');
   for (let i = 10; i >= 0; i--) {
-    await sleep(2000); 
+    await REST_BRUH(2000); 
 
     if (i > 0) {
       m.edit(`⏳ ${i}...`);
     } else {
       m.edit('💀 Boo! Spooky countdown complete👻');
-      await sleep(1500); 
+      await REST_BRUH(1500); 
       m.edit('🕷️ Surprise! A spider crawls across your screen! 🕷️');
-      await sleep(2000); 
+      await REST_BRUH(2000); 
       m.edit('😱 Just kidding! Happy 2024 😂😂😂..omg');
     }
   }
